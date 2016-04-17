a8b545ba2b29b7ad7f53c6d88655ac442bde6732
Michael Forney <mforney@mforney.org>
2016-04-17 16:54:05 -0700

    file: Add config.h rather than using flags

---
+++ core/file/config.h
@@ -0,0 +1,32 @@
+#include <config-posix.h>
+
+/* features */
+#define BUILTIN_ELF 1
+#define ELFCORE 1
+#define HAVE_ZLIB_H 1
+
+/* headers */
+#define HAVE_ERR_H 1
+#define HAVE_GETOPT_H 1
+/* #undef HAVE_NBTOOL_CONFIG_H */
+/* #undef HAVE_XLOCALE_H */
+
+/* types */
+#define HAVE_SIG_T 1
+#define HAVE_STRUCT_OPTION 1
+#define HAVE_STRUCT_TM_TM_GMTOFF 1
+#define HAVE_STRUCT_TM_TM_ZONE 1
+
+/* functions */
+#define HAVE_ASPRINTF 1
+#define HAVE_GETOPT_LONG 1
+#define HAVE_STRCASESTR 1
+#define HAVE_STRLCAT 1
+#define HAVE_STRLCPY 1
+#define HAVE_VASPRINTF 1
+/* #undef HAVE_FMTCHECK */
+
+/* misc */
+#define HAVE_VISIBILITY 1
+#define MAJOR_IN_SYSMACROS 1
+/* #undef HAVE_DECL_OPTRESET */
