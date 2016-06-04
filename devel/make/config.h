8b52db755696b3a2b29a3da28d664b315483193f
Michael Forney <mforney@mforney.org>
2016-06-04 10:33:16 -0700

    Add make 4.2

---
+++ devel/make/config.h
@@ -0,0 +1,119 @@
+#include <config-posix.h>
+
+/* these cause make to use glibc symbols */
+#undef HAVE_WCHTYPE_H
+#undef HAVE_WCHAR_H
+
+/* paths */
+#define INCLUDEDIR "/include"
+#define LIBDIR "/lib"
+#define LOCALEDIR "/share/locale"
+
+/* #undef CLOSEDIR_VOID */
+/* #undef CRAY_STACKSEG_END */
+/* #undef C_ALLOCA */
+/* #undef C_GETLOADAVG */
+/* #undef DGUX */
+/* #undef ENABLE_NLS */
+#define FILE_TIMESTAMP_HI_RES 1
+/* #undef GETLOADAVG_PRIVILEGED */
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+/* #undef HAVE_CASE_INSENSITIVE_FS */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+/* #undef HAVE_DCGETTEXT */
+#define HAVE_DECL_BSD_SIGNAL 1
+#define HAVE_DECL_DLERROR 1
+#define HAVE_DECL_DLOPEN 1
+#define HAVE_DECL_DLSYM 1
+#define HAVE_DECL_SYS_SIGLIST 0
+#define HAVE_DECL__SYS_SIGLIST 0
+#define HAVE_DECL___SYS_SIGLIST 0
+/* #undef HAVE_DOS_PATHS */
+/* #undef HAVE_GETHOSTBYNAME */
+#define HAVE_GETLOADAVG 1
+/* #undef HAVE_GETTEXT */
+/* #undef HAVE_GUILE */
+/* #undef HAVE_LIBDGC */
+/* #undef HAVE_LIBKSTAT */
+/* #undef HAVE_MACH_MACH_H */
+#define HAVE_MEMORY_H 1
+#define HAVE_MKTEMP 1
+/* #undef HAVE_NDIR_H */
+/* #undef HAVE_NLIST_H */
+/* #undef HAVE_PSTAT_GETDYNAMIC */
+#define HAVE_SA_RESTART 1
+#define HAVE_SETLINEBUF 1
+/* #undef HAVE_SIGSETMASK */
+/* #undef HAVE_STRCMPI */
+/* #undef HAVE_STRICMP */
+/* #undef HAVE_STRNCMPI */
+/* #undef HAVE_STRNICMP */
+/* #undef HAVE_STRUCT_NLIST_N_UN_N_NAME */
+/* #undef HAVE_SYS_DIR_H */
+/* #undef HAVE_SYS_NDIR_H */
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_TIMEB_H 1
+/* #undef HAVE_UNION_WAIT */
+#define HAVE_UNSIGNED_LONG_LONG_INT 1
+/* #undef HAVE_VFORK_H */
+#define HAVE_WAIT3 1
+#define MAKE_HOST "x86_64-linux-musl"
+#define MAKE_JOBSERVER 1
+#define MAKE_LOAD 1
+#define MAKE_SYMLINKS 1
+/* #undef NLIST_NAME_UNION */
+/* #undef NLIST_STRUCT */
+#define PACKAGE "make"
+#define PACKAGE_BUGREPORT "bug-make@gnu.org"
+#define PACKAGE_NAME "GNU make"
+#define PACKAGE_STRING "GNU make 4.2"
+#define PACKAGE_TARNAME "make"
+#define PACKAGE_URL "http://www.gnu.org/software/make/"
+#define PACKAGE_VERSION "4.2"
+#define PATH_SEPARATOR_CHAR ':'
+#define SCCS_GET "get"
+/* #undef SCCS_GET_MINUS_G */
+/* #undef STACK_DIRECTION */
+/* #undef STAT_MACROS_BROKEN */
+#define ST_MTIM_NSEC st_mtim.tv_nsec
+/* #undef SVR4 */
+#define TIME_WITH_SYS_TIME 1
+/* #undef UMAX */
+/* #undef UMAX4_3 */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "4.2"
+/* #undef WINDOWS32 */
+/* #undef WITH_DMALLOC */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef const */
+/* #undef gid_t */
+/* #undef off_t */
+/* #undef pid_t */
+/* #undef size_t */
+/* #undef ssize_t */
+/* #undef uid_t */
+/* #undef uintmax_t */
+/* #undef vfork */
