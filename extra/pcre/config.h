e6b127e22a2b7d34e2bf46331c1b0bfa9848da1e
Michael Forney <mforney@mforney.org>
2016-06-04 21:14:07 -0700

    Add pcre 8.38

---
+++ extra/pcre/config.h
@@ -0,0 +1,64 @@
+#include <config-posix.h>
+
+/* #undef BSR_ANYCRLF */
+/* #undef EBCDIC */
+/* #undef EBCDIC_NL25 */
+#define HAVE_BCOPY 1
+/* #undef HAVE_BITS_TYPE_TRAITS_H */
+/* #undef HAVE_BZLIB_H */
+/* #undef HAVE_EDITLINE_READLINE_H */
+/* #undef HAVE_EDIT_READLINE_READLINE_H */
+#define HAVE_LONG_LONG 1
+#define HAVE_MEMORY_H 1
+#define HAVE_PTHREAD 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+/* #undef HAVE_READLINE_HISTORY_H */
+/* #undef HAVE_READLINE_READLINE_H */
+#define HAVE_STRING 1
+/* #undef HAVE_STRTOQ */
+/* #undef HAVE_TYPE_TRAITS_H */
+#define HAVE_UNSIGNED_LONG_LONG 1
+#define HAVE_VISIBILITY 1
+/* #undef HAVE_WINDOWS_H */
+/* #undef HAVE_ZLIB_H */
+/* #undef HAVE__STRTOI64 */
+#define LINK_SIZE 2
+#define LT_OBJDIR ".libs/"
+#define MATCH_LIMIT 10000000
+#define MATCH_LIMIT_RECURSION MATCH_LIMIT
+#define MAX_NAME_COUNT 10000
+#define MAX_NAME_SIZE 32
+#define NEWLINE 10
+/* #undef NO_RECURSE */
+#define PACKAGE "pcre"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "PCRE"
+#define PACKAGE_STRING "PCRE 8.38"
+#define PACKAGE_TARNAME "pcre"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "8.38"
+#define PARENS_NEST_LIMIT 250
+#define PCRECPP_EXP_DECL extern __attribute__ ((visibility ("default")))
+#define PCRECPP_EXP_DEFN __attribute__ ((visibility ("default")))
+#define PCREGREP_BUFSIZE 20480
+#define PCREPOSIX_EXP_DECL extern __attribute__ ((visibility ("default")))
+#define PCREPOSIX_EXP_DEFN extern __attribute__ ((visibility ("default")))
+#define PCRE_EXP_DATA_DEFN __attribute__ ((visibility ("default")))
+#define PCRE_EXP_DECL extern __attribute__ ((visibility ("default")))
+#define PCRE_EXP_DEFN __attribute__ ((visibility ("default")))
+#define PCRE_STATIC 1
+#define POSIX_MALLOC_THRESHOLD 10
+/* #undef PTHREAD_CREATE_JOINABLE */
+#define SUPPORT_JIT /**/
+/* #undef SUPPORT_LIBBZ2 */
+/* #undef SUPPORT_LIBEDIT */
+/* #undef SUPPORT_LIBREADLINE */
+/* #undef SUPPORT_LIBZ */
+/* #undef SUPPORT_PCRE16 */
+/* #undef SUPPORT_PCRE32 */
+#define SUPPORT_PCRE8 /**/
+#define SUPPORT_PCREGREP_JIT /**/
+/* #undef SUPPORT_UCP */
+#define SUPPORT_UTF /**/
+/* #undef SUPPORT_VALGRIND */
+#define VERSION "8.38"
