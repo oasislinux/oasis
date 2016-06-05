669133b670598bf4309a5c6819ad816c5a80e3fc
Michael Forney <mforney@mforney.org>
2016-06-04 21:20:55 -0700

    Add the_silver_searcher 0.32.0

---
+++ extra/the_silver_searcher/config.h
@@ -0,0 +1,24 @@
+#include <config-posix.h>
+
+/* #undef HAVE_DIRENT_DNAMLEN */
+#define HAVE_DIRENT_DTYPE /**/
+#define HAVE_FGETLN 1
+/* #undef HAVE_LIBSHLWAPI */
+/* #undef HAVE_LZMA_H */
+#define HAVE_MADVISE 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_PLEDGE */
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+#define HAVE_PTHREAD_SETAFFINITY_NP 1
+#define HAVE_STRLCPY 1
+#define HAVE_VASPRINTF 1
+/* #undef HAVE_ZLIB_H */
+#define PACKAGE_BUGREPORT "https://github.com/ggreer/the_silver_searcher/issues"
+#define PACKAGE_NAME "the_silver_searcher"
+#define PACKAGE_STRING "the_silver_searcher 0.32.0"
+#define PACKAGE_TARNAME "the_silver_searcher"
+#define PACKAGE_URL "https://github.com/ggreer/the_silver_searcher"
+#define PACKAGE_VERSION "0.32.0"
+/* #undef PTHREAD_CREATE_JOINABLE */
+#define USE_CPU_SET /**/
+#define USE_PCRE_JIT 1
