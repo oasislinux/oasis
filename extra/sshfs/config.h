52975fad4b5765514f48628e17622d1f15b6c8ec
Michael Forney <mforney@mforney.org>
2016-06-05 18:42:00 -0700

    Add sshfs 2.7

---
+++ extra/sshfs/config.h
@@ -0,0 +1,11 @@
+#define IDMAP_DEFAULT "none"
+
+#define PACKAGE "sshfs"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "sshfs"
+#define PACKAGE_STRING "sshfs 2.7"
+#define PACKAGE_TARNAME "sshfs"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.7"
+/* #undef SSH_NODELAY_WORKAROUND */
+#define VERSION "2.7"
