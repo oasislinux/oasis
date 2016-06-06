901710bf1988151002ae50cb47cff7d6836826bf
Michael Forney <mforney@mforney.org>
2016-06-05 21:39:57 -0700

    Add transmission 2.92

---
+++ extra/transmission/config.h
@@ -0,0 +1,27 @@
+#include <config-posix.h>
+
+#define PACKAGE_DATA_DIR "/share"
+#define WITH_INOTIFY 1
+
+#define PACKAGE_NAME "transmission"
+#define PACKAGE_TARNAME "transmission"
+#define PACKAGE_VERSION "2.92"
+#define PACKAGE_STRING "transmission\ 2.92"
+#define PACKAGE_BUGREPORT "http://trac.transmissionbt.com/newticket"
+#define PACKAGE_URL ""
+#define PACKAGE "transmission"
+#define VERSION "2.92"
+#define HAVE_MEMORY_H 1
+#define LT_OBJDIR ".libs/"
+#define TIME_WITH_SYS_TIME 1
+#define HAVE_STRLCPY 1
+#define HAVE_DAEMON 1
+#define HAVE_FALLOCATE64 1
+#define HAVE_MEMMEM 1
+#define HAVE_STRSEP 1
+#define HAVE_VALLOC 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_PTHREAD 1
+#define HAVE_GETMNTENT 1
+#define HAVE_DECL_POSIX_FADVISE 1
+#define WITH_UTP 1
