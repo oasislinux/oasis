17d13a356f298214f8e2a4d88aa14893ea66dd2f
Michael Forney <mforney@mforney.org>
2016-06-05 18:28:24 -0700

    Add libfuse 2.9.6

---
+++ extra/libfuse/config.h
@@ -0,0 +1,22 @@
+#include <config-posix.h>
+
+#define FUSERMOUNT_DIR "/bin"
+#define IGNORE_MTAB
+
+#define HAVE_MEMORY_H 1
+#define HAVE_SETXATTR 1
+#define HAVE_SPLICE 1
+#define HAVE_STRUCT_STAT_ST_ATIM 1
+/* #undef HAVE_STRUCT_STAT_ST_ATIMESPEC */
+#define HAVE_VMSPLICE 1
+#define ICONV_CONST 
+/* #undef IGNORE_MTAB */
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "fuse"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "fuse"
+#define PACKAGE_STRING "fuse 2.9.6"
+#define PACKAGE_TARNAME "fuse"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.9.6"
+#define VERSION "2.9.6"
