703a7a0e588927167cb5b38c562181c996b669f0
Michael Forney <mforney@mforney.org>
2016-06-07 01:41:06 -0700

    Add xz 5.2.2

---
+++ core/xz/config.h
@@ -0,0 +1,141 @@
+#include <config-posix.h>
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+#define ASSUME_RAM 128
+/* #undef ENABLE_NLS */
+#define HAVE_BSWAP_16 1
+#define HAVE_BSWAP_32 1
+#define HAVE_BSWAP_64 1
+#define HAVE_BYTESWAP_H 1
+/* #undef HAVE_CC_SHA256_CTX */
+/* #undef HAVE_CC_SHA256_INIT */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+#define HAVE_CHECK_CRC32 1
+#define HAVE_CHECK_CRC64 1
+#define HAVE_CHECK_SHA256 1
+/* #undef HAVE_COMMONCRYPTO_COMMONDIGEST_H */
+/* #undef HAVE_DCGETTEXT */
+#define HAVE_DECL_CLOCK_MONOTONIC 1
+#define HAVE_DECL_PROGRAM_INVOCATION_NAME 1
+#define HAVE_DECODER_ARM 1
+#define HAVE_DECODER_ARMTHUMB 1
+#define HAVE_DECODER_DELTA 1
+#define HAVE_DECODER_IA64 1
+#define HAVE_DECODER_LZMA1 1
+#define HAVE_DECODER_LZMA2 1
+#define HAVE_DECODER_POWERPC 1
+#define HAVE_DECODER_SPARC 1
+#define HAVE_DECODER_X86 1
+#define HAVE_ENCODER_ARM 1
+#define HAVE_ENCODER_ARMTHUMB 1
+#define HAVE_ENCODER_DELTA 1
+#define HAVE_ENCODER_IA64 1
+#define HAVE_ENCODER_LZMA1 1
+#define HAVE_ENCODER_LZMA2 1
+#define HAVE_ENCODER_POWERPC 1
+#define HAVE_ENCODER_SPARC 1
+#define HAVE_ENCODER_X86 1
+/* #undef HAVE_FUTIMES */
+/* #undef HAVE_FUTIMESAT */
+#define HAVE_GETOPT_H 1
+#define HAVE_GETOPT_LONG 1
+/* #undef HAVE_GETTEXT */
+#define HAVE_IMMINTRIN_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MF_BT2 1
+#define HAVE_MF_BT3 1
+#define HAVE_MF_BT4 1
+#define HAVE_MF_HC3 1
+#define HAVE_MF_HC4 1
+/* #undef HAVE_MINIX_SHA2_H */
+#define HAVE_OPTRESET 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+/* #undef HAVE_SHA256INIT */
+/* #undef HAVE_SHA256_CTX */
+/* #undef HAVE_SHA256_H */
+/* #undef HAVE_SHA256_INIT */
+/* #undef HAVE_SHA2_CTX */
+/* #undef HAVE_SHA2_H */
+/* #undef HAVE_SMALL */
+/* #undef HAVE_STRUCT_STAT_ST_ATIMENSEC */
+/* #undef HAVE_STRUCT_STAT_ST_ATIMESPEC_TV_NSEC */
+/* #undef HAVE_STRUCT_STAT_ST_ATIM_ST__TIM_TV_NSEC */
+#define HAVE_STRUCT_STAT_ST_ATIM_TV_NSEC 1
+/* #undef HAVE_STRUCT_STAT_ST_UATIME */
+/* #undef HAVE_SYS_BYTEORDER_H */
+/* #undef HAVE_SYS_ENDIAN_H */
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_VISIBILITY 1
+#define HAVE__BOOL 1
+#define HAVE__MM_MOVEMASK_EPI8 1
+#define LT_OBJDIR ".libs/"
+#define MYTHREAD_POSIX 1
+/* #undef MYTHREAD_VISTA */
+/* #undef MYTHREAD_WIN95 */
+#define NDEBUG 1
+#define PACKAGE "xz"
+#define PACKAGE_BUGREPORT "lasse.collin@tukaani.org"
+#define PACKAGE_NAME "XZ Utils"
+#define PACKAGE_STRING "XZ Utils 5.2.2"
+#define PACKAGE_TARNAME "xz"
+#define PACKAGE_URL "http://tukaani.org/xz/"
+#define PACKAGE_VERSION "5.2.2"
+/* #undef PTHREAD_CREATE_JOINABLE */
+#define SIZEOF_SIZE_T 8
+/* #undef TUKLIB_CPUCORES_CPUSET */
+/* #undef TUKLIB_CPUCORES_PSTAT_GETDYNAMIC */
+#define TUKLIB_CPUCORES_SYSCONF 1
+/* #undef TUKLIB_CPUCORES_SYSCTL */
+#define TUKLIB_FAST_UNALIGNED_ACCESS 1
+/* #undef TUKLIB_PHYSMEM_AIX */
+/* #undef TUKLIB_PHYSMEM_GETINVENT_R */
+/* #undef TUKLIB_PHYSMEM_GETSYSINFO */
+/* #undef TUKLIB_PHYSMEM_PSTAT_GETSTATIC */
+#define TUKLIB_PHYSMEM_SYSCONF 1
+/* #undef TUKLIB_PHYSMEM_SYSCTL */
+/* #undef TUKLIB_PHYSMEM_SYSINFO */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "5.2.2"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef _UINT32_T */
+/* #undef _UINT64_T */
+/* #undef _UINT8_T */
+/* #undef __GETOPT_PREFIX */
+/* #undef int32_t */
+/* #undef int64_t */
+/* #undef uint16_t */
+/* #undef uint32_t */
+/* #undef uint64_t */
+/* #undef uint8_t */
+/* #undef uintptr_t */
