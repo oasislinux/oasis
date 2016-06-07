0c212a89be0a9e78f0e8086520ac826bfc4a91f5
Michael Forney <mforney@mforney.org>
2016-06-06 21:48:27 -0700

    sinit: Run scripts in /etc

---
+++ core/sinit/config.h
@@ -0,0 +1,5 @@
+/* See LICENSE file for copyright and license details. */
+
+static char *const rcinitcmd[]     = { "/etc/rc.init", NULL };
+static char *const rcrebootcmd[]   = { "/etc/rc.shutdown", "reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/etc/rc.shutdown", "poweroff", NULL };
