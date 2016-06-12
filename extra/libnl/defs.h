0d3e0d20597f30878e99aaf8d29d47fa5d88ea9e
Michael Forney <mforney@mforney.org>
2016-06-12 16:21:27 -0700

    Add libnl 3.2.25

---
+++ extra/libnl/defs.h
@@ -0,0 +1,20 @@
+#include <config-posix.h>
+
+/* #undef DISABLE_PTHREADS */
+#define HAVE_LIBM 1
+#define HAVE_LIBPTHREAD 1
+#define HAVE_MEMORY_H 1
+#define LT_OBJDIR ".libs/"
+#define NL_DEBUG 1
+#define PACKAGE "libnl"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "libnl"
+#define PACKAGE_STRING "libnl 3.2.25"
+#define PACKAGE_TARNAME "libnl"
+#define PACKAGE_URL "http://www.infradead.org/~tgr/libnl/"
+#define PACKAGE_VERSION "3.2.25"
+#define VERSION "3.2.25"
+/* #undef const */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
