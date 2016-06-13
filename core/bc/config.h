0512a75e89b0a61cc065021834ed22e90c98c937
Michael Forney <mforney@mforney.org>
2016-06-12 19:59:27 -0700

    Add bc 1.06.95

---
+++ core/bc/config.h
@@ -0,0 +1,40 @@
+#include <config-posix.h>
+
+#define BC_COPYRIGHT "Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006 Free Software Foundation, Inc."
+#define DC_COPYRIGHT "Copyright 1994, 1997, 1998, 2000, 2001, 2004, 2005, 2006 Free Software Foundation, Inc."
+#define DC_VERSION "1.3.95"
+/* #undef HAVE_DOPRNT */
+/* #undef HAVE_LIB_H */
+#define HAVE_MEMORY_H 1
+/* #undef LIBEDIT */
+#define PACKAGE "bc"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "bc"
+#define PACKAGE_STRING "bc 1.06.95"
+#define PACKAGE_TARNAME "bc"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.06.95"
+/* #undef READLINE */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "1.06.95"
+/* #undef YYTEXT_POINTER */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef const */
+/* #undef ptrdiff_t */
+/* #undef size_t */
