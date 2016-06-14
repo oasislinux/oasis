5e9a2daa4d4276da05610cbcd0a84bcb6902e89d
Michael Forney <mforney@mforney.org>
2016-06-13 20:43:25 -0700

    file: Update to 5.28

--- core/file/config.h
+++ core/file/config.h
@@ -1,33 +1,97 @@
 #include <config-posix.h>
 
-/* features */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
 #define BUILTIN_ELF 1
 #define ELFCORE 1
-#define HAVE_LIBZ 1
-#define HAVE_ZLIB_H 1
-
-/* headers */
-#define HAVE_ERR_H 1
-#define HAVE_GETOPT_H 1
-/* #undef HAVE_NBTOOL_CONFIG_H */
-/* #undef HAVE_XLOCALE_H */
-
-/* types */
-#define HAVE_SIG_T 1
-#define HAVE_STRUCT_OPTION 1
-#define HAVE_STRUCT_TM_TM_GMTOFF 1
-#define HAVE_STRUCT_TM_TM_ZONE 1
-
-/* functions */
 #define HAVE_ASPRINTF 1
+#define HAVE_DECL_DAYLIGHT 1
+#define HAVE_DECL_TZNAME 1
+#define HAVE_ERR_H 1
+/* #undef HAVE_FMTCHECK */
+#define HAVE_GETOPT_H 1
 #define HAVE_GETOPT_LONG 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_INTPTR_T 1
+/* #undef HAVE_LIBGNURX */
+#define HAVE_LIBZ 1
+#define HAVE_MBSTATE_T 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MKOSTEMP 1
+#define HAVE_SIG_T 1
 #define HAVE_STRCASESTR 1
 #define HAVE_STRLCAT 1
 #define HAVE_STRLCPY 1
+#define HAVE_STRUCT_OPTION 1
+/* #undef HAVE_STRUCT_TM_TM_GMTOFF */
+#define HAVE_STRUCT_TM_TM_ZONE 1
+#define HAVE_SYS_PARAM_H 1
+/* #undef HAVE_SYS_UTIME_H */
+#define HAVE_TM_ISDST 1
+#define HAVE_TM_ZONE 1
 #define HAVE_VASPRINTF 1
-/* #undef HAVE_FMTCHECK */
-
-/* misc */
-#define HAVE_VISIBILITY 1
-#define MAJOR_IN_SYSMACROS 1
-/* #undef HAVE_DECL_OPTRESET */
+/* #undef HAVE_VFORK_H */
+/* #undef HAVE_XLOCALE_H */
+#define HAVE_ZLIB_H 1
+#define LT_OBJDIR ".libs/"
+/* #undef MAJOR_IN_MKDEV */
+/* #undef MAJOR_IN_SYSMACROS */
+#define PACKAGE "file"
+#define PACKAGE_BUGREPORT "christos@astron.com"
+#define PACKAGE_NAME "file"
+#define PACKAGE_STRING "file 5.27"
+#define PACKAGE_TARNAME "file"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "5.27"
+/* #undef TM_IN_SYS_TIME */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "5.27"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+#define ZLIBSUPPORT 1
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGEFILE_SOURCE */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef _UINT32_T */
+/* #undef _UINT64_T */
+/* #undef _UINT8_T */
+/* #undef const */
+/* #undef int32_t */
+/* #undef int64_t */
+/* #undef intptr_t */
+/* #undef mbstate_t */
+/* #undef off_t */
+/* #undef pid_t */
+/* #undef size_t */
+/* #undef uint16_t */
+/* #undef uint32_t */
+/* #undef uint64_t */
+/* #undef uint8_t */
+/* #undef uintptr_t */
+/* #undef vfork */
