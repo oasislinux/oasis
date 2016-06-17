59aaddcd0f7881d3ff10f8849cb24dab5aae1841
Michael Forney <mforney@mforney.org>
2016-06-16 23:21:28 -0700

    Add unzip 6.0

---
+++ extra/unzip/config.h
@@ -0,0 +1,15 @@
+#define ACORN_FTYPE_NFS
+#define DATE_FORMAT DF_YMD
+#define IZ_HAVE_UXUIDGID
+#define LARGE_FILE_SUPPORT
+#define UNICODE_SUPPORT
+#define UNICODE_WCHAR
+#define UNIX
+#define USE_BZIP2
+#define UTF8_MAYBE_NATIVE
+#define WILD_STOP_AT_DIR
+
+/* why does Debian define these? */
+#define NOMEMCPY
+#define NO_LCHMOD
+#define NO_WORKING_ISPRINT
