663d3abf3e87c0d93128b976d100bb0563ba5052
Michael Forney <mforney@mforney.org>
2016-06-23 23:26:32 -0700

    sshfs: Update to 2.8

--- extra/sshfs/config.h
+++ extra/sshfs/config.h
@@ -3,9 +3,9 @@
 #define PACKAGE "sshfs"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "sshfs"
-#define PACKAGE_STRING "sshfs 2.7"
+#define PACKAGE_STRING "sshfs 2.8"
 #define PACKAGE_TARNAME "sshfs"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.7"
+#define PACKAGE_VERSION "2.8"
 /* #undef SSH_NODELAY_WORKAROUND */
-#define VERSION "2.7"
+#define VERSION "2.8"
