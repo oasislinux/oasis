25ae1ce225ef0df4512a31bce712d224be86b5ce
Michael Forney <mforney@mforney.org>
2016-06-23 23:49:59 -0700

    pcre: Update to 8.39

--- extra/pcre/config.h
+++ extra/pcre/config.h
@@ -33,10 +33,10 @@
 #define PACKAGE "pcre"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "PCRE"
-#define PACKAGE_STRING "PCRE 8.38"
+#define PACKAGE_STRING "PCRE 8.39"
 #define PACKAGE_TARNAME "pcre"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "8.38"
+#define PACKAGE_VERSION "8.39"
 #define PARENS_NEST_LIMIT 250
 #define PCRECPP_EXP_DECL extern __attribute__ ((visibility ("default")))
 #define PCRECPP_EXP_DEFN __attribute__ ((visibility ("default")))
@@ -61,4 +61,4 @@
 #define SUPPORT_UCP /**/
 #define SUPPORT_UTF /**/
 /* #undef SUPPORT_VALGRIND */
-#define VERSION "8.38"
+#define VERSION "8.39"
