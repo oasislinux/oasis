e7e9ede8f3fa87bc95eb9d7b5f963444a5274a53
Michael Forney <mforney@mforney.org>
2016-06-25 12:22:25 -0700

    git: Store global definitions in config.h

---
+++ core/git/config.h
@@ -0,0 +1,11 @@
+#define ETC_GITATTRIBUTES "/etc/gitattributes"
+#define ETC_GITCONFIG "/etc/gitconfig"
+#define GIT_EXEC_PATH "libexec/git-core"
+#define GIT_HTML_PATH "share/doc/git-doc"
+#define GIT_INFO_PATH "share/info"
+#define GIT_LOCALE_PATH "/share/locale"
+#define GIT_MAN_PATH "share/man"
+#define GIT_USER_AGENT "git/" GIT_VERSION
+#define GIT_VERSION "2.9.0"
+#define NO_SYS_POLL_H
+#define SHA1_HEADER <openssl/sha.h>
