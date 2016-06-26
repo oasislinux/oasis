bc1fe603dac0eb4a102b6d32a4dea68f22619442
Michael Forney <mforney@mforney.org>
2016-06-26 13:27:00 -0700

    Add pixman 0.34.0

---
+++ desktop/pixman/config.h
@@ -0,0 +1,48 @@
+#include <config-posix.h>
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+#define HAVE_BUILTIN_CLZ /**/
+#define HAVE_FEDIVBYZERO 1
+/* #undef HAVE_FEENABLEEXCEPT */
+#define HAVE_FLOAT128 /**/
+#define HAVE_GCC_VECTOR_EXTENSIONS /**/
+/* #undef HAVE_GETISAX */
+#define HAVE_GETPAGESIZE 1
+/* #undef HAVE_LIBPIXMAN_1 */
+/* #undef HAVE_LIBPNG */
+#define HAVE_MEMORY_H 1
+#define HAVE_PTHREADS /**/
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "pixman"
+#define PACKAGE_BUGREPORT "pixman@lists.freedesktop.org"
+#define PACKAGE_NAME "pixman"
+#define PACKAGE_STRING "pixman 0.34.0"
+#define PACKAGE_TARNAME "pixman"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "0.34.0"
+/* #undef PIXMAN_TIMERS */
+#define SIZEOF_LONG 8
+#define TLS __thread
+#define TOOLCHAIN_SUPPORTS_ATTRIBUTE_CONSTRUCTOR /**/
+/* #undef USE_ARM_IWMMXT */
+/* #undef USE_ARM_NEON */
+/* #undef USE_ARM_SIMD */
+#define USE_GCC_INLINE_ASM 1
+/* #undef USE_LOONGSON_MMI */
+/* #undef USE_MIPS_DSPR2 */
+/* #undef USE_OPENMP */
+/* #undef USE_VMX */
+#define VERSION "0.34.0"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+/* #undef sqrtf */
