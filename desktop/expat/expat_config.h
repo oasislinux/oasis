ae802b58a9d529e1ac7e6c807e67c63331658747
Michael Forney <mforney@mforney.org>
2016-06-26 10:34:30 -0700

    Add expat 2.2.0

---
+++ desktop/expat/expat_config.h
@@ -0,0 +1,22 @@
+#include <config-posix.h>
+
+#define BYTEORDER 1234
+#define HAVE_BCOPY 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_SYS_PARAM_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
+#define PACKAGE_NAME "expat"
+#define PACKAGE_STRING "expat 2.2.0"
+#define PACKAGE_TARNAME "expat"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.2.0"
+/* #undef WORDS_BIGENDIAN */
+#define XML_CONTEXT_BYTES 1024
+#define XML_DTD 1
+#define XML_NS 1
+/* #undef __func__ */
+/* #undef const */
+/* #undef off_t */
+/* #undef size_t */
