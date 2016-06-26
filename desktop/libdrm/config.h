6ed455ea1cc0af430d926d353944bb5b96c688ba
Michael Forney <mforney@mforney.org>
2016-06-26 11:03:03 -0700

    Add libdrm 2.4.68

---
+++ desktop/libdrm/config.h
@@ -0,0 +1,61 @@
+#include <config-posix.h>
+
+/* #undef CRAY_STACKSEG_END */
+/* #undef C_ALLOCA */
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+/* #undef HAVE_AMDGPU */
+/* #undef HAVE_CAIRO */
+/* #undef HAVE_CUNIT */
+/* #undef HAVE_EXYNOS */
+/* #undef HAVE_FREEDRENO */
+/* #undef HAVE_FREEDRENO_KGSL */
+/* #undef HAVE_INSTALL_TESTS */
+#define HAVE_LIBDRM_ATOMIC_PRIMITIVES 1
+/* #undef HAVE_LIBUDEV */
+/* #undef HAVE_LIB_ATOMIC_OPS */
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_OMAP */
+/* #undef HAVE_RADEON */
+/* #undef HAVE_SYS_MKDEV_H */
+/* #undef HAVE_SYS_SYSCTL_H */
+/* #undef HAVE_TEGRA */
+/* #undef HAVE_VALGRIND */
+/* #undef HAVE_VC4 */
+#define HAVE_VISIBILITY 1
+/* #undef HAVE_VMWGFX */
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libdrm"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=DRI"
+#define PACKAGE_NAME "libdrm"
+#define PACKAGE_STRING "libdrm 2.4.68"
+#define PACKAGE_TARNAME "libdrm"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.4.68"
+/* #undef STACK_DIRECTION */
+/* #undef UDEV */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "2.4.68"
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef size_t */
