4eb283ce9e6b5377a7ce41d834fdc8e9c49e7489
Michael Forney <mforney@mforney.org>
2016-06-25 22:31:24 -0700

    Add libpciaccess 0.13.4

---
+++ desktop/libpciaccess/config.h
@@ -0,0 +1,46 @@
+#include <config-posix.h>
+
+#define HAVE_ERR_H 1
+/* #undef HAVE_MACHINE_MTRR_H */
+/* #undef HAVE_MACHINE_SYSARCH_H */
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_MTRR */
+/* #undef HAVE_PCI_IO_PC_DOMAIN */
+/* #undef HAVE_ZLIB */
+/* #undef LINUX_ROM */
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libpciaccess"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=xorg&component=libpciaccess"
+#define PACKAGE_NAME "libpciaccess"
+#define PACKAGE_STRING "libpciaccess 0.13.4"
+#define PACKAGE_TARNAME "libpciaccess"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "0.13.4"
+#define PACKAGE_VERSION_MAJOR 0
+#define PACKAGE_VERSION_MINOR 13
+#define PACKAGE_VERSION_PATCHLEVEL 4
+#define PCIIDS_PATH "/share/hwdata"
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "0.13.4"
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
