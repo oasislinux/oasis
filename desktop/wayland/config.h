043af3766e4b75dfa5c1c76d49b4f706a0a3f8f9
Michael Forney <mforney@mforney.org>
2016-06-26 12:31:21 -0700

    Add wayland 1.11.0

---
+++ desktop/wayland/config.h
@@ -0,0 +1,17 @@
+#include <config-posix.h>
+
+#define HAVE_ACCEPT4 1
+/* #undef HAVE_EXECINFO_H */
+/* #undef HAVE_EXPAT_H */
+/* #undef HAVE_LIBXML */
+#define HAVE_MEMORY_H 1
+#define HAVE_MKOSTEMP 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "wayland"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=Wayland&component=wayland&version=1.11.0"
+#define PACKAGE_NAME "wayland"
+#define PACKAGE_STRING "wayland 1.11.0"
+#define PACKAGE_TARNAME "wayland"
+#define PACKAGE_URL "http://wayland.freedesktop.org/"
+#define PACKAGE_VERSION "1.11.0"
+#define VERSION "1.11.0"
