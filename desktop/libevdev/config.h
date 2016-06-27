4a6368f55235991e43b47a74963bd0f7882953b4
Michael Forney <mforney@mforney.org>
2016-06-27 00:00:52 -0700

    Add libevdev 1.5.2

---
+++ desktop/libevdev/config.h
@@ -0,0 +1,30 @@
+#include <config-posix.h>
+
+#define HAVE_MEMORY_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libevdev"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=libevdev"
+#define PACKAGE_NAME "libevdev"
+#define PACKAGE_STRING "libevdev 1.5.2"
+#define PACKAGE_TARNAME "libevdev"
+#define PACKAGE_URL "http://freedesktop.org/wiki/Software/libevdev/"
+#define PACKAGE_VERSION "1.5.2"
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "1.5.2"
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
