81ecddb2551467519b2afe0869d4bdb872ab9ac2
Michael Forney <mforney@mforney.org>
2016-07-02 15:41:50 -0700

    Add nasm 2.12.01

---
+++ devel/nasm/config.h
@@ -0,0 +1,61 @@
+#include <config-posix.h>
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+/* #undef HAVE_CANONICALIZE_FILE_NAME */
+#define HAVE_DECL_STRCASECMP 1
+#define HAVE_DECL_STRICMP 0
+#define HAVE_DECL_STRLCPY 1
+#define HAVE_DECL_STRNCASECMP 1
+#define HAVE_DECL_STRNICMP 0
+#define HAVE_DECL_STRSEP 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_STRICMP */
+#define HAVE_STRLCPY 1
+/* #undef HAVE_STRNICMP */
+#define HAVE_STRSEP 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_WORKING_BOOL 1
+/* #undef HAVE__FULLPATH */
+/* #undef HAVE__SNPRINTF */
+/* #undef HAVE__VSNPRINTF */
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME ""
+#define PACKAGE_STRING ""
+#define PACKAGE_TARNAME ""
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION ""
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+/* #undef WORDS_BIGENDIAN */
+#define WORDS_LITTLEENDIAN 1
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef const */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+#define restrict __restrict
+#if defined __SUNPRO_CC && !defined __RESTRICT
+# define _Restrict
+# define __restrict__
+#endif
+/* #undef size_t */
