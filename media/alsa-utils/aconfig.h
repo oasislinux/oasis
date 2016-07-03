388db2beb50f27ff3488b8a1bef4029bf8a7d058
Michael Forney <mforney@mforney.org>
2016-07-03 00:03:45 -0700

    Add alsa-utils 1.1.1

---
+++ media/alsa-utils/aconfig.h
@@ -0,0 +1,48 @@
+#include <config-posix.h>
+
+#define DATADIR "/share/alsa"
+/* #undef ENABLE_NLS */
+/* #undef ENABLE_NLS_IN_CURSES */
+#define HAVE_ALSA_MIXER_H 1
+#define HAVE_ALSA_PCM_H 1
+#define HAVE_ALSA_RAWMIDI_H 1
+#define HAVE_ALSA_SEQ_H 1
+/* #undef HAVE_ALSA_TOPOLOGY_H */
+#define HAVE_ALSA_USE_CASE_H 1
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+/* #undef HAVE_CURSES_ESCDELAY */
+/* #undef HAVE_DCGETTEXT */
+/* #undef HAVE_FORM_H */
+/* #undef HAVE_GETTEXT */
+#define HAVE_LIBASOUND 1
+/* #undef HAVE_LIBFFTW3F */
+#define HAVE_LIBM 1
+#define HAVE_LIBPTHREAD 1
+#define HAVE_LIBRT 1
+/* #undef HAVE_LIBTINYALSA */
+/* #undef HAVE_MEMORY_H */
+/* #undef HAVE_MENU_H */
+/* #undef HAVE_PANEL_H */
+/* #undef HAVE_SAMPLERATE_H */
+/* #undef HAVE_SEQ_CLIENT_INFO_GET_CARD */
+/* #undef HAVE_SEQ_CLIENT_INFO_GET_PID */
+#define PACKAGE "alsa-utils"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "alsa-utils"
+#define PACKAGE_STRING "alsa-utils 1.1.1"
+#define PACKAGE_TARNAME "alsa-utils"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.1.1"
+#define SOUNDSDIR "/share/sounds/alsa"
+#define TIME_WITH_SYS_TIME 1
+#define VERSION "1.1.1"
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef const */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
