1c7b711ae20de4d55728693863b6efc2d3c1deb1
Michael Forney <mforney@mforney.org>
2016-07-06 00:09:12 -0700

    Add pcsc 1.8.17

---
+++ extra/pcsc/config.h
@@ -0,0 +1,66 @@
+#include <config-posix.h>
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+/* #undef ATR_DEBUG */
+/* #undef CRAY_STACKSEG_END */
+/* #undef C_ALLOCA */
+/* #undef FILTER_NAMES */
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_DAEMON 1
+/* #undef HAVE_DL_H */
+/* #undef HAVE_DOPRNT */
+#define HAVE_FLOCK 1
+#define HAVE_GETOPT_H 1
+#define HAVE_GETOPT_LONG 1
+/* #undef HAVE_LIBUDEV */
+/* #undef HAVE_LIBUDEV_H */
+#define HAVE_LIBUSB 1
+#define HAVE_LIBUSB_H 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_NDIR_H */
+/* #undef HAVE_POLKIT */
+#define HAVE_PTHREAD 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+#define HAVE_STRUCT_DIRENT_D_TYPE 1
+/* #undef HAVE_SYS_DIR_H */
+/* #undef HAVE_SYS_FILIO_H */
+/* #undef HAVE_SYS_NDIR_H */
+#define LT_OBJDIR ".libs/"
+/* #undef NO_LOG */
+#define PACKAGE "pcsc-lite"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "pcsc-lite"
+#define PACKAGE_STRING "pcsc-lite 1.8.17"
+#define PACKAGE_TARNAME "pcsc-lite"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.8.17"
+#define PCSCD_BINARY "/sbin/pcscd"
+#define PCSCLITE_FEATURES " Linux x86_64-unknown-linux-gnu usb usbdropdir=/lib/pcsc/drivers ipcdir=/var/run/pcscd configdir=/etc/reader.conf.d"
+#define PCSCLITE_HP_DROPDIR "/lib/pcsc/drivers"
+#define PCSC_ARCH "Linux"
+/* #undef PTHREAD_CREATE_JOINABLE */
+/* #undef STACK_DIRECTION */
+#define TIME_WITH_SYS_TIME 1
+#define USE_IPCDIR "/var/run/pcscd"
+/* #undef USE_SERIAL */
+#define USE_USB 1
+#define VERSION "1.8.17"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+/* #undef YYTEXT_POINTER */
+/* #undef const */
+/* #undef gid_t */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+/* #undef size_t */
+/* #undef uid_t */
