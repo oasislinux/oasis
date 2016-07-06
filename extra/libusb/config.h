9875c7b74f848734cdff95fa31f72839036c72d1
Michael Forney <mforney@mforney.org>
2016-07-05 22:20:47 -0700

    Add libusb 1.0.20

---
+++ extra/libusb/config.h
@@ -0,0 +1,37 @@
+#include <config-posix.h>
+
+#define DEFAULT_VISIBILITY __attribute__((visibility("default")))
+/* #undef ENABLE_DEBUG_LOGGING */
+#define ENABLE_LOGGING 1
+#define HAVE_ASM_TYPES_H 1
+/* #undef HAVE_LIBUDEV */
+/* #undef HAVE_LIBUDEV_H */
+#define HAVE_LINUX_FILTER_H 1
+#define HAVE_LINUX_NETLINK_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_SYSLOG_FUNC 1
+#define LT_OBJDIR ".libs/"
+/* #undef OS_DARWIN */
+/* #undef OS_HAIKU */
+#define OS_LINUX 1
+/* #undef OS_NETBSD */
+/* #undef OS_OPENBSD */
+/* #undef OS_WINDOWS */
+#define PACKAGE "libusb"
+#define PACKAGE_BUGREPORT "libusb-devel@lists.sourceforge.net"
+#define PACKAGE_NAME "libusb"
+#define PACKAGE_STRING "libusb 1.0.20"
+#define PACKAGE_TARNAME "libusb"
+#define PACKAGE_URL "http://libusb.info"
+#define PACKAGE_VERSION "1.0.20"
+#define POLL_NFDS_TYPE nfds_t
+#define THREADS_POSIX 1
+#define USBI_TIMERFD_AVAILABLE 1
+/* #undef USE_SYSTEM_LOGGING_FACILITY */
+/* #undef USE_UDEV */
+#define VERSION "1.0.20"
+/* #undef WINVER */
+#define _GNU_SOURCE 1
+#ifndef __cplusplus
+/* #undef inline */
+#endif
