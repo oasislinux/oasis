aff6246b52d3a56e10b84eab8a4c1ea18ad60ee0
Michael Forney <mforney@mforney.org>
2016-07-10 02:06:04 -0700

    Add fribidi 0.19.7

---
+++ extra/fribidi/config.h
@@ -0,0 +1,19 @@
+#include <config-posix.h>
+
+/* #undef DEBUG */
+/* #undef HAVE_ASM_PAGE_H */
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGIZE 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE_BUGREPORT "http://bugs.freedesktop.org/enter_bug.cgi?product=fribidi"
+#define PACKAGE_NAME "GNU FriBidi"
+#define PACKAGE_STRING "GNU FriBidi 0.19.7"
+#define PACKAGE_TARNAME "fribidi"
+#define PACKAGE_URL "http://fribidi.org/"
+#define PACKAGE_VERSION "0.19.7"
+#define SIZEOF_INT 4
+#define SIZEOF_SHORT 2
+#define SIZEOF_VOID_P 8
+#define SIZEOF_WCHAR_T 4
+#define USE_SIMPLE_MALLOC 1
+/* #undef const */
