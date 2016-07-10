bd694b5fef78e53e536251336a2ceb2b8114729e
Michael Forney <mforney@mforney.org>
2016-07-10 02:17:37 -0700

    Add libass 0.13.2

---
+++ media/libass/config.h
@@ -0,0 +1,25 @@
+#include <config-posix.h>
+
+/* #undef CONFIG_ASM */
+/* #undef CONFIG_CORETEXT */
+/* #undef CONFIG_DIRECTWRITE */
+/* #undef CONFIG_FONTCONFIG */
+#define CONFIG_FREETYPE 1
+#define CONFIG_FRIBIDI 1
+/* #undef CONFIG_HARFBUZZ */
+#define CONFIG_ICONV 1
+/* #undef CONFIG_LARGE_TILES */
+/* #undef CONFIG_LIBPNG */
+#define CONFIG_RASTERIZER 1
+#define HAVE_LIBM 1
+#define HAVE_MEMORY_H 1
+#define HAVE__BOOL 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libass"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "libass"
+#define PACKAGE_STRING "libass 0.13.2"
+#define PACKAGE_TARNAME "libass"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "0.13.2"
+#define VERSION "0.13.2"
