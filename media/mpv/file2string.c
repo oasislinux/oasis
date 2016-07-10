064f02c45807259d435082c93cd9dae4b1ea4621
Michael Forney <mforney@mforney.org>
2016-07-10 12:22:14 -0700

    mpv: Rewrite file2string in C to handle NULL bytes
    It is also used for binary files like osd_font.otf.

---
+++ media/mpv/file2string.c
@@ -0,0 +1,34 @@
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+static const char safe[] =
+	"][ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#%&'()*+,./:;<=>^_{|}~ -";
+
+int main(int argc, char *argv[]) {
+	FILE *f;
+	char *line = NULL, *s;
+	size_t size = 0;
+	ssize_t n;
+
+	f = fopen(argv[1], "r");
+	if (!f) {
+		fprintf(stderr, "fopen %s: %s\n", argv[1], strerror(errno));
+		return 1;
+	}
+
+	printf("/* Generated from %s */\n", argv[1]);
+	while ((n = getline(&line, &size, f)) >= 0) {
+		printf("\"");
+		for (s = line; s < line + n; ++s) {
+			if (memchr(safe, *s, sizeof(safe) - 1))
+				fputc(*s, stdout);
+			else
+				printf("\\%03hho", *s);
+		}
+		printf("\"\n");
+	}
+
+	return 0;
+}
