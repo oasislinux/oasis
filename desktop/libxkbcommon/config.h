7e0bbbf17a5e51d14cfdda633e03dfd75633810b
Michael Forney <mforney@mforney.org>
2016-10-12 21:33:54 -0700

    Add libxkbcommon 0.6.1

---
+++ desktop/libxkbcommon/config.h
@@ -0,0 +1,49 @@
+#include <config-posix.h>
+
+#define DFLT_XKB_CONFIG_ROOT "/share/xkb"
+#define XLOCALEDIR "/share/xlocale"
+
+#define DEFAULT_XKB_LAYOUT "us"
+#define DEFAULT_XKB_MODEL "pc105"
+/* #undef DEFAULT_XKB_OPTIONS */
+#define DEFAULT_XKB_RULES "evdev"
+/* #undef DEFAULT_XKB_VARIANT */
+#define HAVE_EACCESS 1
+#define HAVE_EUIDACCESS 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_SECURE_GETENV */
+#define HAVE___BUILTIN_EXPECT 1
+/* #undef HAVE___SECURE_GETENV */
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libxkbcommon"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=libxkbcommon"
+#define PACKAGE_NAME "libxkbcommon"
+#define PACKAGE_STRING "libxkbcommon 0.6.1"
+#define PACKAGE_TARNAME "libxkbcommon"
+#define PACKAGE_URL "http://xkbcommon.org"
+#define PACKAGE_VERSION "0.6.1"
+#define PACKAGE_VERSION_MAJOR 0
+#define PACKAGE_VERSION_MINOR 6
+#define PACKAGE_VERSION_PATCHLEVEL 1
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "0.6.1"
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
