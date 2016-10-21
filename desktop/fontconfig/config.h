699e88102d63f49ce7fb4f6027616233b7715997
Michael Forney <mforney@mforney.org>
2016-10-21 00:12:21 -0700

    Add fontconfig 2.12.1

---
+++ desktop/fontconfig/config.h
@@ -0,0 +1,108 @@
+#include <config-posix.h>
+
+#define FC_CACHEDIR "/var/cache/fontconfig"
+#define FONTCONFIG_PATH "/etc/fonts"
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+#define ALIGNOF_DOUBLE 8
+#define ALIGNOF_VOID_P 8
+/* #undef ENABLE_LIBXML2 */
+#define FC_ADD_FONTS "yes"
+/* #undef FC_ARCHITECTURE */
+#define FC_DEFAULT_FONTS "/share/fonts"
+#define FLEXIBLE_ARRAY_MEMBER /**/
+/* #undef HAVE_DOPRNT */
+#define HAVE_FSTATFS 1
+#define HAVE_FT_BITMAP_SIZE_Y_PPEM 1
+#define HAVE_FT_GET_BDF_PROPERTY 1
+#define HAVE_FT_GET_NEXT_CHAR 1
+#define HAVE_FT_GET_PS_FONT_INFO 1
+#define HAVE_FT_GET_X11_FONT_FORMAT 1
+#define HAVE_FT_HAS_PS_GLYPH_NAMES 1
+#define HAVE_FT_SELECT_SIZE 1
+/* #undef HAVE_GETEXECNAME */
+#define HAVE_GETOPT_LONG 1
+#define HAVE_GETPAGESIZE 1
+/* #undef HAVE_GETPROGNAME */
+#define HAVE_INTEL_ATOMIC_PRIMITIVES 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MKOSTEMP 1
+/* #undef HAVE_NDIR_H */
+#define HAVE_PTHREAD 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+/* #undef HAVE_RANDOM_R */
+/* #undef HAVE_SOLARIS_ATOMIC_OPS */
+#define HAVE_STRUCT_DIRENT_D_TYPE 1
+#define HAVE_STRUCT_STATFS_F_FLAGS 1
+/* #undef HAVE_STRUCT_STATFS_F_FSTYPENAME */
+/* #undef HAVE_STRUCT_STATVFS_F_BASETYPE */
+/* #undef HAVE_STRUCT_STATVFS_F_FSTYPENAME */
+#define HAVE_STRUCT_STAT_ST_MTIM 1
+/* #undef HAVE_SYS_DIR_H */
+#define HAVE_SYS_MOUNT_H 1
+/* #undef HAVE_SYS_NDIR_H */
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_STATFS_H 1
+#define HAVE_SYS_VFS_H 1
+#define HAVE_TT_OS2_USLOWEROPTICALPOINTSIZE 1
+#define HAVE_TT_OS2_USUPPEROPTICALPOINTSIZE 1
+/* Can use #warning in C files */
+#define HAVE_WARNING_CPP_DIRECTIVE 1
+/* #undef HAVE_XMLPARSE_H */
+#define HAVE_XML_SETDOCTYPEDECLHANDLER 1
+/* #undef HAVE__MKTEMP_S */
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "fontconfig"
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=fontconfig"
+#define PACKAGE_NAME "fontconfig"
+#define PACKAGE_STRING "fontconfig 2.12.1"
+#define PACKAGE_TARNAME "fontconfig"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.12.1"
+/* #undef PTHREAD_CREATE_JOINABLE */
+/* #undef SIZEOF_CHAR */
+/* #undef SIZEOF_INT */
+/* #undef SIZEOF_LONG */
+/* #undef SIZEOF_SHORT */
+/* #undef SIZEOF_VOIDP */
+#define SIZEOF_VOID_P 8
+#define USE_ICONV 0
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define VERSION "2.12.1"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef const */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+/* #undef pid_t */
+#include "config-fixups.h"
