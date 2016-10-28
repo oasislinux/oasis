b1ddd6c018e1a53793b9841a4908a1c5abcfb225
Michael Forney <mforney@mforney.org>
2016-10-27 21:31:08 -0700

    Add dmenu scm

---
+++ desktop/dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Terminus:pixelsize=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#999999", "#1a1a1a" },
+	[SchemeSel] = { "#ffffff", "#338833" },
+	[SchemeOut] = { "#000000", "#aaffaa" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
