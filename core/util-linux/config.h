1dce34dae0791ce13c9e2f89fca77e28db54d9d6
Michael Forney <mforney@mforney.org>
2016-11-09 09:32:59 -0800

    Add util-linux 2.29 (for fdisk)

---
+++ core/util-linux/config.h
@@ -0,0 +1,222 @@
+#include <config-posix.h>
+
+#define _PATH_LOCALSTATEDIR "/run"
+#define LOCALEDIR "/share/locale"
+
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+#define AGETTY_RELOAD 1
+#define CHFN_CHSH_PASSWORD 1
+#define CONFIG_ADJTIME_PATH "/etc/adjtime"
+/* #undef ENABLE_NLS */
+#define FS_SEARCH_PATH "/bin"
+/* #undef HAVE_ASM_IO_H */
+#define HAVE_BTRFS_SUPPORT 1
+#define HAVE_BYTESWAP_H 1
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+#define HAVE_CPU_SET_T 1
+#define HAVE_CRYPT_H 1
+/* #undef HAVE_DCGETTEXT */
+#define HAVE_DECL_CPU_ALLOC 1
+/* #undef HAVE_DECL_DIRFD */
+#define HAVE_DECL__NL_TIME_WEEK_1STDAY 0
+/* #undef HAVE_DIR_DD_FD */
+#define HAVE_ENDIAN_H 1
+#define HAVE_ENVIRON_DECL 1
+#define HAVE_ERR 1
+#define HAVE_ERRX 1
+#define HAVE_ERR_H 1
+#define HAVE_FALLOCATE 1
+#define HAVE_GETDOMAINNAME 1
+#define HAVE_GETDTABLESIZE 1
+/* #undef HAVE_GETEXECNAME */
+/* #undef HAVE_GETMNTINFO */
+#define HAVE_GETOPT_H 1
+/* #undef HAVE_GETSGNAM */
+/* #undef HAVE_GETTEXT */
+#define HAVE_INOTIFY_INIT 1
+#define HAVE_INOTIFY_INIT1 1
+#define HAVE_IOPERM 1
+#define HAVE_IOPL 1
+/* #undef HAVE_LIBAUDIT */
+/* #undef HAVE_LIBBLKID */
+/* #undef HAVE_LIBCAP_NG */
+/* #undef HAVE_LIBCRYPT */
+/* #undef HAVE_LIBMOUNT */
+/* #undef HAVE_LIBNCURSES */
+/* #undef HAVE_LIBNCURSESW */
+/* #undef HAVE_LIBREADLINE */
+/* #undef HAVE_LIBRTAS */
+/* #undef HAVE_LIBSELINUX */
+/* #undef HAVE_LIBSYSTEMD */
+/* #undef HAVE_LIBTINFO */
+/* #undef HAVE_LIBUDEV */
+/* #undef HAVE_LIBUSER */
+/* #undef HAVE_LIBUTEMPTER */
+#define HAVE_LIBUTIL 1
+#define HAVE_LIBUUID 1
+#define HAVE_LINUX_BLKPG_H 1
+#define HAVE_LINUX_BTRFS_H 1
+#define HAVE_LINUX_CDROM_H 1
+/* #undef HAVE_LINUX_COMPILER_H */
+#define HAVE_LINUX_FALLOC_H 1
+#define HAVE_LINUX_FD_H 1
+/* #undef HAVE_LINUX_GSMMUX_H */
+#define HAVE_LINUX_MAJOR_H 1
+#define HAVE_LINUX_RAW_H 1
+#define HAVE_LINUX_SECUREBITS_H 1
+#define HAVE_LINUX_TIOCL_H 1
+#define HAVE_LINUX_VERSION_H 1
+#define HAVE_LINUX_WATCHDOG_H 1
+/* #undef HAVE_LLSEEK */
+/* #undef HAVE_LLSEEK_PROTOTYPE */
+/* #undef HAVE_LOFF_T */
+#define HAVE_LSEEK64 1
+#define HAVE_LSEEK64_PROTOTYPE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MEMPCPY 1
+#define HAVE_MKOSTEMP 1
+#define HAVE_MNTENT_H 1
+/* #undef HAVE_NCURSESW_NCURSES_H */
+/* #undef HAVE_NCURSES_H */
+/* #undef HAVE_NCURSES_NCURSES_H */
+/* #undef HAVE_NET_IF_DL_H */
+/* #undef HAVE_NTP_GETTIME */
+#define HAVE_PATHS_H 1
+#define HAVE_PERSONALITY 1
+#define HAVE_PRCTL 1
+/* #undef HAVE_PRLIMIT */
+#define HAVE_PROGRAM_INVOCATION_SHORT_NAME 1
+#define HAVE_PTY_H 1
+/* #undef HAVE_QSORT_R */
+#define HAVE_REBOOT 1
+/* #undef HAVE_RESIZETERM */
+/* #undef HAVE_RPMATCH */
+/* #undef HAVE_SA_LEN */
+/* #undef HAVE_SCANDIRAT */
+/* #undef HAVE_SCANF_AS_MODIFIER */
+#define HAVE_SCANF_MS_MODIFIER 1
+/* #undef HAVE_SCHED_SETATTR */
+/* #undef HAVE_SECURE_GETENV */
+/* #undef HAVE_SECURITY_GET_INITIAL_CONTEXT */
+/* #undef HAVE_SECURITY_OPENPAM_H */
+/* #undef HAVE_SECURITY_PAM_APPL_H */
+/* #undef HAVE_SECURITY_PAM_MISC_H */
+#define HAVE_SETNS 1
+/* #undef HAVE_SETPROGNAME */
+#define HAVE_SETRESGID 1
+#define HAVE_SETRESUID 1
+#define HAVE_SHADOW_H 1
+#define HAVE_SIGHANDLER_T 1
+/* #undef HAVE_SLANG_H */
+/* #undef HAVE_SLANG_SLANG_H */
+/* #undef HAVE_SLANG_SLCURSES_H */
+/* #undef HAVE_SLCURSES_H */
+/* #undef HAVE_SMACK */
+#define HAVE_STDIO_EXT_H 1
+/* #undef HAVE_STRNCHR */
+#define HAVE_STRSIGNAL_DECL 1
+#define HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC 1
+#define HAVE_STRUCT_TERMIOS_C_LINE 1
+#define HAVE_SYSINFO 1
+/* #undef HAVE_SYS_DISKLABEL_H */
+/* #undef HAVE_SYS_DISK_H */
+/* #undef HAVE_SYS_ENDIAN_H */
+#define HAVE_SYS_FILE_H 1
+/* #undef HAVE_SYS_IOCCOM_H */
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_SYS_IO_H 1
+/* #undef HAVE_SYS_MKDEV_H */
+#define HAVE_SYS_MOUNT_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_PRCTL_H 1
+#define HAVE_SYS_SIGNALFD_H 1
+/* #undef HAVE_SYS_SOCKIO_H */
+#define HAVE_SYS_SWAP_H 1
+#define HAVE_SYS_SYSCALL_H 1
+#define HAVE_SYS_SYSMACROS_H 1
+#define HAVE_SYS_TIMEX_H 1
+#define HAVE_SYS_TTYDEFAULTS_H 1
+/* #undef HAVE_SYS_UCRED_H */
+#define HAVE_TLS 1
+#define HAVE_TM_GMTOFF 1
+/* #undef HAVE_UNION_SEMUN */
+#define HAVE_UNSHARE 1
+#define HAVE_UPDWTMP 1
+/* #undef HAVE_USE_DEFAULT_COLORS */
+#define HAVE_USLEEP 1
+#define HAVE_UTMP_H 1
+/* #undef HAVE_UUIDD */
+#define HAVE_WARN 1
+#define HAVE_WARNX 1
+#define HAVE_WIDECHAR 1
+#define HAVE___FPENDING 1
+#define HAVE___PROGNAME 1
+/* #undef HAVE___SECURE_GETENV */
+#define LIBBLKID_DATE "08-Nov-2016"
+/* #undef LOGIN_CHOWN_VCS */
+/* #undef LOGIN_STAT_MAIL */
+#define LT_OBJDIR ".libs/"
+/* #undef NDEBUG */
+#define ONLY_LISTED_SHELLS 1
+#define PACKAGE "util-linux"
+#define PACKAGE_BUGREPORT "kzak@redhat.com"
+#define PACKAGE_NAME "util-linux"
+#define PACKAGE_STRING "util-linux 2.29"
+#define PACKAGE_TARNAME "util-linux"
+#define PACKAGE_URL "http://www.kernel.org/pub/linux/utils/util-linux/"
+#define PACKAGE_VERSION "2.29"
+#define PG_BELL 1
+#define SWAPON_HAS_TWO_ARGS 1
+/* #undef SYS_fallocate */
+/* #undef SYS_ioprio_get */
+/* #undef SYS_ioprio_set */
+/* #undef SYS_pivot_root */
+/* #undef SYS_prlimit64 */
+/* #undef SYS_sched_getaffinity */
+/* #undef SYS_sched_setattr */
+/* #undef SYS_setns */
+/* #undef SYS_unshare */
+#define USE_COLORS_BY_DEFAULT 1
+/* #undef USE_LIBMOUNT_SUPPORT_MTAB */
+#define USE_PLYMOUTH_SUPPORT 1
+/* #undef USE_SULOGIN_EMERGENCY_MOUNT */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#define USE_TTY_GROUP 1
+/* #undef USE_USRDIR_PATHS_ONLY */
+#define VERSION "2.29"
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+/* #undef _DARWIN_C_SOURCE */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGEFILE_SOURCE */
+/* #undef _LARGE_FILES */
+/* #undef _MINIX */
+/* #undef _POSIX_1_SOURCE */
+/* #undef _POSIX_SOURCE */
+/* #undef const */
+/* #undef volatile */
