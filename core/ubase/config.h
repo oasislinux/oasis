421f1188fb1b3b65d929cb6ba5933172765971c9
Michael Forney <mforney@mforney.org>
2016-11-12 09:33:34 -0800

    ubase: Use custom config.h to specify UTMP_PATH

---
+++ core/ubase/config.h
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+
+#define ENV_SUPATH	"/bin"
+#define ENV_PATH	"/bin"
+#define PW_CIPHER	"$6$"	/* SHA-512 */
+#undef UTMP_PATH
+#define UTMP_PATH	"/run/utmp"
+#undef BTMP_PATH
+#define BTMP_PATH	"/var/log/btmp"
+#undef WTMP_PATH
+#define WTMP_PATH	"/var/log/wtmp"
