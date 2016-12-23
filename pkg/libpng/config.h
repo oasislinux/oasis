9783cc76994abad1ce2825015fc4979f183e40b4
Michael Forney <mforney@mforney.org>
2016-12-23 15:38:52 -0800

    Add libpng 1.6.26

---
+++ pkg/libpng/config.h
@@ -0,0 +1,29 @@
+#include <config-posix.h>
+
+/* #undef HAVE_FEENABLEEXCEPT */
+/* #undef HAVE_LIBM */
+#define HAVE_LIBZ 1
+#define HAVE_MEMORY_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libpng"
+#define PACKAGE_BUGREPORT "png-mng-implement@lists.sourceforge.net"
+#define PACKAGE_NAME "libpng"
+#define PACKAGE_STRING "libpng 1.6.26"
+#define PACKAGE_TARNAME "libpng"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.6.26"
+/* #undef PNG_ARM_NEON_API_SUPPORTED */
+/* #undef PNG_ARM_NEON_CHECK_SUPPORTED */
+/* #undef PNG_ARM_NEON_OPT */
+/* #undef PNG_MIPS_MSA_API_SUPPORTED */
+/* #undef PNG_MIPS_MSA_CHECK_SUPPORTED */
+/* #undef PNG_MIPS_MSA_OPT */
+/* #undef TM_IN_SYS_TIME */
+#define VERSION "1.6.26"
+/* #undef const */
+#define restrict __restrict
+#if defined __SUNPRO_CC && !defined __RESTRICT
+# define _Restrict
+# define __restrict__
+#endif
+/* #undef size_t */
