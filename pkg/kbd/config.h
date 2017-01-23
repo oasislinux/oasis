18fe6aea19c99fbffa2c478ac44ff4ab3aeb0f82
Michael Forney <mforney@mforney.org>
2017-01-22 19:32:00 -0800

    kbd: Update to 2.0.4
    Also, switch to building from source tar archive so that we don't need
    to import pre-generated parsers/lexers, and we don't need to port the
    gen_charset_syms.pl script to awk.

--- pkg/kbd/config.h
+++ pkg/kbd/config.h
@@ -1,31 +1,35 @@
 #include <config-posix.h>
 
+/* #undef CLOSEDIR_VOID */
+/* #undef ENABLE_NLS */
+/* #undef HAVE_DCGETTEXT */
+/* #undef HAVE_GETTEXT */
+#define HAVE_LIBINTL_H 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_NDIR_H */
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+/* #undef HAVE_SYS_DIR_H */
+#define HAVE_SYS_FILE_H 1
+#define HAVE_SYS_IOCTL_H 1
+/* #undef HAVE_SYS_NDIR_H */
+/* #undef HAVE_VFORK_H */
+#define LT_OBJDIR ".libs/"
 #define PACKAGE "kbd"
 #define PACKAGE_BUGREPORT "gladkov.alexey@gmail.com"
 #define PACKAGE_NAME "kbd"
-#define PACKAGE_STRING "kbd 2.0.3"
+#define PACKAGE_STRING "kbd 2.0.4"
 #define PACKAGE_TARNAME "kbd"
 #define PACKAGE_URL "http://kbd-project.org/"
-#define PACKAGE_VERSION "2.0.3"
-#define VERSION "2.0.3"
-
-/* features */
-/* #undef ENABLE_NLS */
-
-/* headers */
-#define HAVE_LIBINTL_H 1
-#define HAVE_MEMORY_H 1
-#define HAVE_SYS_DIR_H 1
-#define HAVE_SYS_FILE_H 1
-#define HAVE_SYS_IOCTL_H 1
-/* #undef HAVE_NDIR_H */
-/* #undef HAVE_SYS_NDIR_H */
-
-/* functions */
-#define HAVE_ICONV 1
-#define HAVE_DCGETTEXT 1
-#define HAVE_GETTEXT 1
-
-/* misc */
-#define YYTEXT_POINTER 1
-/* #undef LT_OBJDIR */
+#define PACKAGE_VERSION "2.0.4"
+/* #undef TM_IN_SYS_TIME */
+#define VERSION "2.0.4"
+/* #undef YYTEXT_POINTER */
+/* #undef const */
+/* #undef gid_t */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+/* #undef pid_t */
+/* #undef size_t */
+/* #undef uid_t */
+/* #undef vfork */
