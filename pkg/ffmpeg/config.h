fa47936948cb7c4a3bb69becc9006f1720f9fd34
Michael Forney <mforney@mforney.org>
2017-02-06 18:43:28 -0800

    ffmpeg: Update to 3.2.3

--- pkg/ffmpeg/config.h
+++ pkg/ffmpeg/config.h
@@ -2,7 +2,7 @@
 #define FFMPEG_CONFIG_H
 #define FFMPEG_CONFIGURATION "--disable-doc --disable-programs --prefix=/"
 #define FFMPEG_LICENSE "LGPL version 2.1 or later"
-#define CONFIG_THIS_YEAR 2016
+#define CONFIG_THIS_YEAR 2017
 #define FFMPEG_DATADIR "/share/ffmpeg"
 #define AVCONV_DATADIR "/share/ffmpeg"
 #define CC_IDENT "gcc 5.3.0 (GCC)"
