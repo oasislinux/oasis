0fe744d4ed01a9975c0747603d558c954f66c80e
Michael Forney <mforney@mforney.org>
2017-02-22 11:29:55 -0800

    mdocml: Update to 1.14.1

--- pkg/mdocml/config.h
+++ pkg/mdocml/config.h
@@ -1,8 +1,13 @@
 #include <config-posix.h>
 
+#define UTF8_LOCALE "C.UTF-8"
+#define EFTYPE EINVAL
+#define HAVE_CMSG_XPG42 0
 #define HAVE_DIRENT_NAMLEN 0
+#define HAVE_ENDIAN 1
 #define HAVE_ERR 1
-#define HAVE_FTS 1
+#define HAVE_FTS 0
+#define HAVE_FTS_COMPARE_CONST 0
 #define HAVE_PLEDGE 0
 #define HAVE_PROGNAME 1
 #define HAVE_REALLOCARRAY 1
@@ -15,15 +20,14 @@
 #define HAVE_STRLCPY 1
 #define HAVE_STRSEP 1
 #define HAVE_STRTONUM 1
+#define HAVE_SYS_ENDIAN 0
 #define HAVE_VASPRINTF 1
 #define HAVE_WCHAR 1
-#define HAVE_SQLITE3 0
-#define HAVE_SQLITE3_ERRSTR 0
 #define HAVE_OHASH 0
-#define HAVE_MANPATH 0
 
 /* binary names */
 #define BINM_APROPOS "apropos"
+#define BINM_CATMAN "catman"
 #define BINM_MAKEWHATIS "makewhatis"
 #define BINM_MAN "man"
 #define BINM_SOELIM "soelim"
--- pkg/openbsd/include/string.h
+++ pkg/openbsd/include/string.h
@@ -1,4 +1,5 @@
 #include_next <string.h>
 void explicit_bzero(void *, size_t);
+char *strcasestr(const char *, const char *);
 void strmode(int, char *);
 int timingsafe_memcmp(const void *, const void *, size_t);
