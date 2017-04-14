ca5a01f8b2eb03f962439af959c8e913e4986d25
Michael Forney <mforney@mforney.org>
2017-04-14 13:16:48 -0700

    nginx: Update to 1.12.0

--- pkg/nginx/ngx_auto_headers.h
+++ pkg/nginx/ngx_auto_headers.h
@@ -1,3 +1,4 @@
+/* auto/headers */
 #define NGX_HAVE_UNISTD_H 1
 #define NGX_HAVE_INTTYPES_H 1
 #define NGX_HAVE_LIMITS_H 1
@@ -5,6 +6,8 @@
 #define NGX_HAVE_SYS_MOUNT_H 1
 #define NGX_HAVE_SYS_STATVFS_H 1
 #define NGX_HAVE_CRYPT_H 1
+
+/* auto/os/linux */
 #define NGX_LINUX 1
 #define NGX_HAVE_SYS_PRCTL_H 1
 #define NGX_HAVE_SYS_VFS_H 1
