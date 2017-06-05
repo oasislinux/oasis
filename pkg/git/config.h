622c42091688fe6658c488d9790d655304086fb7
Michael Forney <mforney@mforney.org>
2017-06-04 22:02:01 -0700

    git: Update to 2.13.1

--- pkg/git/config.h
+++ pkg/git/config.h
@@ -9,9 +9,16 @@
 #define GIT_LOCALE_PATH "/share/locale"
 #define GIT_MAN_PATH "share/man"
 #define GIT_USER_AGENT "git/" GIT_VERSION
-#define GIT_VERSION "2.13.0"
+#define GIT_VERSION "2.13.1"
 #define PAGER_ENV "LESS=FRX LV=-c"
 #define NO_SYS_POLL_H
 #define NO_OPENSSL
 #define USE_CURL_FOR_IMAP_SEND
+
 #define SHA1_DC
+#define SHA1DC_NO_STANDARD_INCLUDES
+#define SHA1DC_INIT_SAFE_HASH_DEFAULT 0
+#define SHA1DC_CUSTOM_INCLUDE_SHA1_C "cache.h"
+#define SHA1DC_CUSTOM_TRAILING_INCLUDE_SHA1_C "sha1dc_git.c"
+#define SHA1DC_CUSTOM_TRAILING_INCLUDE_SHA1_H "sha1dc_git.h"
+#define SHA1DC_CUSTOM_INCLUDE_UBC_CHECK_C "git-compat-util.h"
