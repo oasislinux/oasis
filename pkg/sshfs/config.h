1f918ac05898529d8a33474f70bf7c61032a9929
Michael Forney <mforney@mforney.org>
2017-06-05 00:14:02 -0700

    sshfs: Update to 2.9

--- pkg/sshfs/config.h
+++ pkg/sshfs/config.h
@@ -3,9 +3,9 @@
 #define PACKAGE "sshfs"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "sshfs"
-#define PACKAGE_STRING "sshfs 2.8"
+#define PACKAGE_STRING "sshfs 2.9"
 #define PACKAGE_TARNAME "sshfs"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.8"
+#define PACKAGE_VERSION "2.9"
 /* #undef SSH_NODELAY_WORKAROUND */
-#define VERSION "2.8"
+#define VERSION "2.9"
