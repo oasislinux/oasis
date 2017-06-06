2c56f5451f1738098adb7bd4309da0be5f9a6dde
Michael Forney <mforney@mforney.org>
2017-06-05 21:43:16 -0700

    libnl: Update to 3.3.0

--- pkg/libnl/defs.h
+++ pkg/libnl/defs.h
@@ -1,7 +1,6 @@
 #include <config-posix.h>
 
 /* #undef DISABLE_PTHREADS */
-#define HAVE_LIBM 1
 #define HAVE_LIBPTHREAD 1
 #define HAVE_MEMORY_H 1
 #define LT_OBJDIR ".libs/"
@@ -9,11 +8,11 @@
 #define PACKAGE "libnl"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "libnl"
-#define PACKAGE_STRING "libnl 3.2.29"
+#define PACKAGE_STRING "libnl 3.3.0"
 #define PACKAGE_TARNAME "libnl"
 #define PACKAGE_URL "http://www.infradead.org/~tgr/libnl/"
-#define PACKAGE_VERSION "3.2.29"
-#define VERSION "3.2.29"
+#define PACKAGE_VERSION "3.3.0"
+#define VERSION "3.3.0"
 /* #undef const */
 #ifndef __cplusplus
 /* #undef inline */
