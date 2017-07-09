a6ca58071917538c6efd6c3df86d5d347be681fc
Michael Forney <mforney@mforney.org>
2017-07-09 02:49:39 -0700

    libfuse, sshfs: Update to 3.1.0, 3.0.0

--- pkg/libfuse/config.h
+++ pkg/libfuse/config.h
@@ -3,20 +3,10 @@
 #define FUSERMOUNT_DIR "/bin"
 #define IGNORE_MTAB
 
-#define HAVE_MEMORY_H 1
-#define HAVE_SETXATTR 1
-#define HAVE_SPLICE 1
-#define HAVE_STRUCT_STAT_ST_ATIM 1
-/* #undef HAVE_STRUCT_STAT_ST_ATIMESPEC */
-#define HAVE_VMSPLICE 1
-#define ICONV_CONST
-/* #undef IGNORE_MTAB */
-#define LT_OBJDIR ".libs/"
-#define PACKAGE "fuse"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_NAME "fuse"
-#define PACKAGE_STRING "fuse 2.9.7"
-#define PACKAGE_TARNAME "fuse"
-#define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.9.7"
-#define VERSION "2.9.7"
+#define HAVE_PIPE2
+#define HAVE_SETXATTR
+#define HAVE_SPLICE
+#define HAVE_STRUCT_STAT_ST_ATIM
+#undef HAVE_STRUCT_STAT_ST_ATIMESPEC
+#define HAVE_VMSPLICE
+#define PACKAGE_VERSION "3.1.0"
--- pkg/sshfs/config.h
+++ pkg/sshfs/config.h
@@ -1,11 +1,2 @@
 #define IDMAP_DEFAULT "none"
-
-#define PACKAGE "sshfs"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_NAME "sshfs"
-#define PACKAGE_STRING "sshfs 2.9"
-#define PACKAGE_TARNAME "sshfs"
-#define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.9"
-/* #undef SSH_NODELAY_WORKAROUND */
-#define VERSION "2.9"
+#define PACKAGE_VERSION "3.0.0"
