1b0aa2faa5bc1cc697f3506b70aff92db3179959
Michael Forney <mforney@mforney.org>
2017-07-15 00:42:37 -0700

    expat: Update to 2.2.2

--- pkg/expat/expat_config.h
+++ pkg/expat/expat_config.h
@@ -1,22 +1,25 @@
 #include <config-posix.h>
 
 #define BYTEORDER 1234
+/* #undef HAVE_ARC4RANDOM_BUF */
 #define HAVE_BCOPY 1
 #define HAVE_GETPAGESIZE 1
+/* #undef HAVE_GETRANDOM */
+/* #undef HAVE_LIBBSD */
 #define HAVE_MEMORY_H 1
+#define HAVE_SYSCALL_GETRANDOM 1
 #define HAVE_SYS_PARAM_H 1
 #define LT_OBJDIR ".libs/"
 #define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
 #define PACKAGE_NAME "expat"
-#define PACKAGE_STRING "expat 2.2.0"
+#define PACKAGE_STRING "expat 2.2.2"
 #define PACKAGE_TARNAME "expat"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.2.0"
+#define PACKAGE_VERSION "2.2.2"
 /* #undef WORDS_BIGENDIAN */
 #define XML_CONTEXT_BYTES 1024
 #define XML_DTD 1
 #define XML_NS 1
-/* #undef __func__ */
 /* #undef const */
 /* #undef off_t */
 /* #undef size_t */
