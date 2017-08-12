982b457b400faded6519b308b87ede9537aacced
Michael Forney <mforney@mforney.org>
2017-08-11 19:28:51 -0700

    Add openntpd 6.2p1

---
+++ pkg/openntpd/config.h
@@ -0,0 +1,40 @@
+#include <config-posix.h>
+
+#define __dead _Noreturn
+#define PACKAGE_NAME "OpenNTPD"
+#define PACKAGE_TARNAME "openntpd"
+#define PACKAGE_VERSION "6.2p1"
+#define PACKAGE_STRING "OpenNTPD 6.2p1"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define PACKAGE "openntpd"
+#define VERSION "6.2p1"
+#define HAVE_MEMORY_H 1
+#define LT_OBJDIR ".libs/"
+#define SPT_TYPE SPT_REUSEARGV
+#define HAVE_ARC4RANDOM 1
+#define HAVE_ARC4RANDOM_BUF 1
+#define HAVE_ARC4RANDOM_UNIFORM 1
+#define HAVE_EXPLICIT_BZERO 1
+#define HAVE_GETAUXVAL 1
+#define HAVE_TIMINGSAFE_MEMCMP 1
+#define HAVE_ASPRINTF 1
+#define HAVE_DAEMON 1
+#define HAVE_MEMMEM 1
+#define HAVE_REALLOCARRAY 1
+#define HAVE_RECALLOCARRAY 1
+#define HAVE_SETGROUPS 1
+#define HAVE_SETRESGID 1
+#define HAVE_SETRESUID 1
+#define HAVE_STRLCAT 1
+#define HAVE_STRLCPY 1
+#define HAVE_STRTONUM 1
+#define HAVE_ADJTIMEX 1
+#define HAVE_TLS_CONFIG_SET_CA_MEM 1
+#define HAVE_LIBTLS yes
+#define HAVE___PROGNAME 1
+#define HAVE_ERR_H 1
+#define HAVE_IFADDRS_H 1
+#define HAVE_PATHS_H 1
+#define NTPD_USER "ntpd"
+#define CONSTRAINT_CA "/etc/ssl/cert.pem"
