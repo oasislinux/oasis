0738eb68bf075199acb6566222806e99f4494ff8
Michael Forney <mforney@mforney.org>
2017-08-13 19:22:42 -0700

    Add dosfstools 4.1

---
+++ pkg/dosfstools/config.h
@@ -0,0 +1,10 @@
+#include <config-posix.h>
+
+#define HAVE_MEMORY_H 1
+#define HAVE_ERR_H 1
+#define HAVE_LINUX_FD_H 1
+#define HAVE_LINUX_HDREG_H 1
+#define HAVE_LINUX_VERSION_H 1
+#define HAVE_ENDIAN_H 1
+#define HAVE_DECL_GETMNTENT 1
+#define HAVE_DECL_GETMNTINFO 0
---
+++ pkg/dosfstools/version.h
@@ -0,0 +1,2 @@
+#define VERSION "4.1"
+#define VERSION_DATE "2017-01-24"
