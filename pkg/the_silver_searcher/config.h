6cb93e10fdf3cf374db671f5d0a752fd985b434d
Michael Forney <mforney@mforney.org>
2017-08-30 19:25:25 -0700

    the_silver_searcher: Update to 2.1.0

--- pkg/the_silver_searcher/config.h
+++ pkg/the_silver_searcher/config.h
@@ -2,7 +2,9 @@
 
 /* #undef HAVE_DIRENT_DNAMLEN */
 #define HAVE_DIRENT_DTYPE /**/
+#define HAVE_ERR_H 1
 #define HAVE_FGETLN 1
+/* #undef HAVE_FOPENCOOKIE */
 /* #undef HAVE_LIBSHLWAPI */
 /* #undef HAVE_LZMA_H */
 #define HAVE_MADVISE 1
@@ -11,14 +13,15 @@
 #define HAVE_PTHREAD_PRIO_INHERIT 1
 #define HAVE_PTHREAD_SETAFFINITY_NP 1
 #define HAVE_STRLCPY 1
+/* #undef HAVE_SYS_CPUSET_H */
 #define HAVE_VASPRINTF 1
 #define HAVE_ZLIB_H 1
 #define PACKAGE_BUGREPORT "https://github.com/ggreer/the_silver_searcher/issues"
 #define PACKAGE_NAME "the_silver_searcher"
-#define PACKAGE_STRING "the_silver_searcher 2.0.0"
+#define PACKAGE_STRING "the_silver_searcher 2.1.0"
 #define PACKAGE_TARNAME "the_silver_searcher"
 #define PACKAGE_URL "https://github.com/ggreer/the_silver_searcher"
-#define PACKAGE_VERSION "2.0.0"
+#define PACKAGE_VERSION "2.1.0"
 /* #undef PTHREAD_CREATE_JOINABLE */
 #define USE_CPU_SET /**/
 #define USE_PCRE_JIT /**/
