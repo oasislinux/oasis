9a6793496f3053ac0f38dea323c2b7fb24fc3dd7
Michael Forney <mforney@mforney.org>
2017-10-24 20:46:16 -0700

    alsa-utils: Don't use config-posix.h

---
+++ pkg/alsa-utils/README.md
@@ -0,0 +1,10 @@
+# alsa-utils
+
+## config.h
+Generated with
+
+	./configure \
+		--prefix= \
+		--disable-alsamixer \
+		CPPFLAGS=-I/src/oasis/out/pkg/alsa-lib/include \
+		LDFLAGS=-L/src/oasis/out/pkg/alsa-lib
--- pkg/alsa-utils/aconfig.h
+++ pkg/alsa-utils/aconfig.h
@@ -1,20 +1,21 @@
-#include <config-posix.h>
-
 #define DATADIR "/share/alsa"
 /* #undef ENABLE_NLS */
 /* #undef ENABLE_NLS_IN_CURSES */
 #define HAVE_ALSA_MIXER_H 1
 #define HAVE_ALSA_PCM_H 1
-#define HAVE_ALSA_RAWMIDI_H 1
+/* #undef HAVE_ALSA_RAWMIDI_H */
 #define HAVE_ALSA_SEQ_H 1
 /* #undef HAVE_ALSA_TOPOLOGY_H */
-#define HAVE_ALSA_USE_CASE_H 1
+/* #undef HAVE_ALSA_USE_CASE_H */
 /* #undef HAVE_CFLOCALECOPYCURRENT */
 /* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+#define HAVE_CLOCK_GETTIME 1
 /* #undef HAVE_CURSES_ESCDELAY */
 /* #undef HAVE_DCGETTEXT */
 /* #undef HAVE_FORM_H */
 /* #undef HAVE_GETTEXT */
+#define HAVE_ICONV 1
+/* #undef HAVE_INTTYPES_H */
 #define HAVE_LIBASOUND 1
 /* #undef HAVE_LIBFFTW3F */
 #define HAVE_LIBM 1
@@ -27,6 +28,13 @@
 /* #undef HAVE_SAMPLERATE_H */
 /* #undef HAVE_SEQ_CLIENT_INFO_GET_CARD */
 /* #undef HAVE_SEQ_CLIENT_INFO_GET_PID */
+/* #undef HAVE_STDINT_H */
+/* #undef HAVE_STDLIB_H */
+/* #undef HAVE_STRINGS_H */
+/* #undef HAVE_STRING_H */
+/* #undef HAVE_SYS_STAT_H */
+/* #undef HAVE_SYS_TYPES_H */
+/* #undef HAVE_UNISTD_H */
 #define PACKAGE "alsa-utils"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "alsa-utils"
@@ -35,6 +43,7 @@
 #define PACKAGE_URL ""
 #define PACKAGE_VERSION "1.1.4"
 #define SOUNDSDIR "/share/sounds/alsa"
+#define STDC_HEADERS 1
 #define TIME_WITH_SYS_TIME 1
 #define VERSION "1.1.4"
 #ifndef _DARWIN_USE_64_BIT_INODE
