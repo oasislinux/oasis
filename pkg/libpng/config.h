b70ae08a1bafb3a74a927b65e0bf8f184facff44
Michael Forney <mforney@mforney.org>
2017-11-08 09:56:36 -0800

    libpng: Update to 1.6.34

--- pkg/libpng/config.h
+++ pkg/libpng/config.h
@@ -1,17 +1,26 @@
-#include <config-posix.h>
-
+#define HAVE_DLFCN_H 1
 /* #undef HAVE_FEENABLEEXCEPT */
+#define HAVE_INTTYPES_H 1
 /* #undef HAVE_LIBM */
 #define HAVE_LIBZ 1
 #define HAVE_MEMORY_H 1
+#define HAVE_MEMSET 1
+#define HAVE_POW 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
 #define LT_OBJDIR ".libs/"
 #define PACKAGE "libpng"
 #define PACKAGE_BUGREPORT "png-mng-implement@lists.sourceforge.net"
 #define PACKAGE_NAME "libpng"
-#define PACKAGE_STRING "libpng 1.6.33"
+#define PACKAGE_STRING "libpng 1.6.34"
 #define PACKAGE_TARNAME "libpng"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.6.33"
+#define PACKAGE_VERSION "1.6.34"
 /* #undef PNG_ARM_NEON_API_SUPPORTED */
 /* #undef PNG_ARM_NEON_CHECK_SUPPORTED */
 /* #undef PNG_ARM_NEON_OPT */
@@ -22,8 +31,9 @@
 /* #undef PNG_POWERPC_VSX_API_SUPPORTED */
 /* #undef PNG_POWERPC_VSX_CHECK_SUPPORTED */
 /* #undef PNG_POWERPC_VSX_OPT */
+#define STDC_HEADERS 1
 /* #undef TM_IN_SYS_TIME */
-#define VERSION "1.6.33"
+#define VERSION "1.6.34"
 /* #undef const */
 #define restrict __restrict
 #if defined __SUNPRO_CC && !defined __RESTRICT
