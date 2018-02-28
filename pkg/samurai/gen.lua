0f505594eae34677477193adfd2234fa071fb327
Michael Forney <mforney@mforney.org>
2018-02-28 00:55:46 -0800

    samurai: Update to 0.3

--- pkg/samurai/gen.lua
+++ pkg/samurai/gen.lua
@@ -3,7 +3,20 @@ cflags{
 	'-D _POSIX_C_SOURCE=200809L',
 }
 
-exe('samu', 'build.c deps.c env.c graph.c htab.c lex.c log.c parse.c samurai.c tool.c util.c')
+exe('samu', {
+	'build.c',
+	'deps.c',
+	'env.c',
+	'graph.c',
+	'htab.c',
+	'lex.c',
+	'log.c',
+	'parse.c',
+	'samurai.c',
+	'tool.c',
+	'tree.c',
+	'util.c',
+})
 file('bin/samu', '755', '$outdir/samu')
 man{'samu.1'}
 
