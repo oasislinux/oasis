55ed7feb2c683a6990c0aa11d3f5feba5de8411e
Michael Forney <mforney@mforney.org>
2018-08-11 13:13:35 -0700

    libpng: Update to 1.6.35

--- pkg/libpng/config.h
+++ pkg/libpng/config.h
@@ -17,14 +17,14 @@
 #define PACKAGE "libpng"
 #define PACKAGE_BUGREPORT "png-mng-implement@lists.sourceforge.net"
 #define PACKAGE_NAME "libpng"
-#define PACKAGE_STRING "libpng 1.6.34"
+#define PACKAGE_STRING "libpng 1.6.35"
 #define PACKAGE_TARNAME "libpng"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.6.34"
+#define PACKAGE_VERSION "1.6.35"
 /* #undef PNG_ARM_NEON_API_SUPPORTED */
 /* #undef PNG_ARM_NEON_CHECK_SUPPORTED */
 /* #undef PNG_ARM_NEON_OPT */
-#define PNG_INTEL_SSE_OPT 1
+/* #undef PNG_INTEL_SSE_OPT */
 /* #undef PNG_MIPS_MSA_API_SUPPORTED */
 /* #undef PNG_MIPS_MSA_CHECK_SUPPORTED */
 /* #undef PNG_MIPS_MSA_OPT */
@@ -33,7 +33,7 @@
 /* #undef PNG_POWERPC_VSX_OPT */
 #define STDC_HEADERS 1
 /* #undef TM_IN_SYS_TIME */
-#define VERSION "1.6.34"
+#define VERSION "1.6.35"
 /* #undef const */
 #define restrict __restrict
 #if defined __SUNPRO_CC && !defined __RESTRICT
