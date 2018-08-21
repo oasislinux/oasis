37ac685731b82d4163f22fd47152c2668f6d9e62
Michael Forney <mforney@mforney.org>
2018-08-21 12:17:55 -0700

    elfutils: Update to 0.173

--- pkg/elfutils/config.h
+++ pkg/elfutils/config.h
@@ -4,8 +4,11 @@
 #define HAVE_DECL_MEMRCHR 1
 #define HAVE_DECL_POWEROF2 1
 #define HAVE_DECL_RAWMEMCHR 0
+#define HAVE_FALLTHROUGH 1
+#define HAVE_GCC_STRUCT 1
 #define HAVE_INTTYPES_H 1
 #define HAVE_MEMORY_H 1
+#define HAVE_PROCESS_VM_READV 1
 #define HAVE_STDINT_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
@@ -14,15 +17,16 @@
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_USER_REGS 1
 #define HAVE_UNISTD_H 1
+#define HAVE_VISIBILITY 1
 #define LIBEBL_SUBDIR "elfutils"
-#define MODVERSION "Build for elfutils 170 x86_64-pc-linux-gnu"
+#define MODVERSION "Build for elfutils 173 x86_64-pc-linux-gnu"
 #define PACKAGE "elfutils"
 #define PACKAGE_BUGREPORT "https://sourceware.org/bugzilla"
 #define PACKAGE_NAME "elfutils"
-#define PACKAGE_STRING "elfutils 0.170"
+#define PACKAGE_STRING "elfutils 0.173"
 #define PACKAGE_TARNAME "elfutils"
 #define PACKAGE_URL "http://elfutils.org/"
-#define PACKAGE_VERSION "0.170"
+#define PACKAGE_VERSION "0.173"
 #define SIZEOF_LONG 8
 #define STDC_HEADERS 1
 /* #undef USE_BZLIB */
@@ -30,7 +34,7 @@
 /* #undef USE_LOCKS */
 /* #undef USE_LZMA */
 #define USE_ZLIB 1
-#define VERSION "0.170"
+#define VERSION "0.173"
 /* #undef YYTEXT_POINTER */
 #ifndef _DARWIN_USE_64_BIT_INODE
 # define _DARWIN_USE_64_BIT_INODE 1
