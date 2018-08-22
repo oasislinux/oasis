99228bd135a83aa9b1f649cf1ff5720d88427667
Michael Forney <mforney@mforney.org>
2018-08-21 17:47:35 -0700

    fribidi: Update to 1.0.5

--- pkg/fribidi/fribidi-config.h
+++ pkg/fribidi/fribidi-config.h
@@ -3,10 +3,10 @@
 #define FRIBIDI "fribidi"
 #define FRIBIDI_NAME "GNU FriBidi"
 #define FRIBIDI_BUGREPORT "https://github.com/fribidi/fribidi/issues/new"
-#define FRIBIDI_VERSION "1.0.2"
+#define FRIBIDI_VERSION "1.0.5"
 #define FRIBIDI_MAJOR_VERSION 1
 #define FRIBIDI_MINOR_VERSION 0
-#define FRIBIDI_MICRO_VERSION 2
+#define FRIBIDI_MICRO_VERSION 5
 #define FRIBIDI_INTERFACE_VERSION 4
 #define FRIBIDI_INTERFACE_VERSION_STRING "4"
 #define FRIBIDI_SIZEOF_INT 4
