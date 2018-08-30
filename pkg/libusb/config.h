c5972ea47ea0de927e1460e48e516d2c41a840fd
Michael Forney <mforney@mforney.org>
2018-08-30 14:18:30 -0700

    libusb: Update to 1.0.22

--- pkg/libusb/config.h
+++ pkg/libusb/config.h
@@ -1,14 +1,29 @@
-#include <config-posix.h>
-
 #define DEFAULT_VISIBILITY __attribute__((visibility("default")))
 /* #undef ENABLE_DEBUG_LOGGING */
 #define ENABLE_LOGGING 1
 #define HAVE_ASM_TYPES_H 1
+#define HAVE_DECL_TFD_CLOEXEC 1
+#define HAVE_DECL_TFD_NONBLOCK 1
+#define HAVE_DLFCN_H 1
+#define HAVE_INTTYPES_H 1
 /* #undef HAVE_LIBUDEV */
 /* #undef HAVE_LIBUDEV_H */
 #define HAVE_LINUX_NETLINK_H 1
 #define HAVE_MEMORY_H 1
-#define HAVE_SYSLOG_FUNC 1
+#define HAVE_PIPE2 1
+#define HAVE_POLL_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRUCT_TIMESPEC 1
+/* #undef HAVE_SYSLOG_FUNC */
+/* #undef HAVE_SYSLOG_H */
+#define HAVE_SYS_SOCKET_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
 #define LT_OBJDIR ".libs/"
 /* #undef OS_DARWIN */
 /* #undef OS_HAIKU */
@@ -20,19 +35,20 @@
 #define PACKAGE "libusb"
 #define PACKAGE_BUGREPORT "libusb-devel@lists.sourceforge.net"
 #define PACKAGE_NAME "libusb"
-#define PACKAGE_STRING "libusb 1.0.21"
+#define PACKAGE_STRING "libusb 1.0.22"
 #define PACKAGE_TARNAME "libusb"
 #define PACKAGE_URL "http://libusb.info"
-#define PACKAGE_VERSION "1.0.21"
+#define PACKAGE_VERSION "1.0.22"
 #define POLL_NFDS_TYPE nfds_t
+#define STDC_HEADERS 1
 #define THREADS_POSIX 1
 #define USBI_TIMERFD_AVAILABLE 1
 /* #undef USE_SYSTEM_LOGGING_FACILITY */
 /* #undef USE_UDEV */
-/* #undef USE_USBDK */
-#define VERSION "1.0.21"
+#define VERSION "1.0.22"
 /* #undef WINVER */
 #define _GNU_SOURCE 1
+/* #undef _WIN32_WINNT */
 #ifndef __cplusplus
 /* #undef inline */
 #endif
--- pkg/libusb/gen.lua
+++ pkg/libusb/gen.lua
@@ -1,5 +1,4 @@
 cflags{
-	'-I include',
 	'-I $dir',
 	'-I $srcdir/libusb',
 }
