6ae1b32cef213c6910f7986fea6d46e83826ad8a
Michael Forney <mforney@mforney.org>
2018-08-30 12:58:06 -0700

    make: Don't use config-posix.h

--- pkg/make/config.h
+++ pkg/make/config.h
@@ -1,10 +1,3 @@
-#include <config-posix.h>
-
-/* these cause make to use glibc symbols */
-#undef HAVE_WCHTYPE_H
-#undef HAVE_WCHAR_H
-
-/* paths */
 #define INCLUDEDIR "/include"
 #define LIBDIR "/lib"
 #define LOCALEDIR "/share/locale"
@@ -19,9 +12,11 @@
 /* #undef GETLOADAVG_PRIVILEGED */
 #define HAVE_ALLOCA 1
 #define HAVE_ALLOCA_H 1
+#define HAVE_ATEXIT 1
 /* #undef HAVE_CASE_INSENSITIVE_FS */
 /* #undef HAVE_CFLOCALECOPYCURRENT */
 /* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+#define HAVE_CLOCK_GETTIME 1
 /* #undef HAVE_DCGETTEXT */
 #define HAVE_DECL_BSD_SIGNAL 1
 #define HAVE_DECL_DLERROR 1
@@ -30,35 +25,91 @@
 #define HAVE_DECL_SYS_SIGLIST 0
 #define HAVE_DECL__SYS_SIGLIST 0
 #define HAVE_DECL___SYS_SIGLIST 0
+#define HAVE_DIRENT_H 1
 /* #undef HAVE_DOS_PATHS */
+#define HAVE_DUP 1
+#define HAVE_DUP2 1
+#define HAVE_FCNTL_H 1
+#define HAVE_FDOPEN 1
+#define HAVE_FILENO 1
+#define HAVE_FORK 1
+#define HAVE_GETCWD 1
+#define HAVE_GETGROUPS 1
 /* #undef HAVE_GETHOSTBYNAME */
+/* #undef HAVE_GETHOSTNAME */
 #define HAVE_GETLOADAVG 1
+#define HAVE_GETRLIMIT 1
 /* #undef HAVE_GETTEXT */
+#define HAVE_GETTIMEOFDAY 1
 /* #undef HAVE_GUILE */
+/* #undef HAVE_ICONV */
+#define HAVE_INTTYPES_H 1
+#define HAVE_ISATTY 1
 /* #undef HAVE_LIBDGC */
 /* #undef HAVE_LIBKSTAT */
+#define HAVE_LIMITS_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_LSTAT 1
 /* #undef HAVE_MACH_MACH_H */
 #define HAVE_MEMORY_H 1
+#define HAVE_MKSTEMP 1
 #define HAVE_MKTEMP 1
 /* #undef HAVE_NDIR_H */
 /* #undef HAVE_NLIST_H */
+#define HAVE_PIPE 1
+#define HAVE_PSELECT 1
 /* #undef HAVE_PSTAT_GETDYNAMIC */
+#define HAVE_READLINK 1
+#define HAVE_REALPATH 1
 #define HAVE_SA_RESTART 1
+#define HAVE_SETEGID 1
+#define HAVE_SETEUID 1
 #define HAVE_SETLINEBUF 1
+/* #undef HAVE_SETLOCALE */
+#define HAVE_SETREGID 1
+#define HAVE_SETREUID 1
+#define HAVE_SETRLIMIT 1
+#define HAVE_SETVBUF 1
+#define HAVE_SIGACTION 1
 /* #undef HAVE_SIGSETMASK */
+/* #undef HAVE_SOCKET */
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCASECMP 1
 /* #undef HAVE_STRCMPI */
+#define HAVE_STRCOLL 1
+#define HAVE_STRDUP 1
+#define HAVE_STRERROR 1
 /* #undef HAVE_STRICMP */
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRNCASECMP 1
 /* #undef HAVE_STRNCMPI */
+#define HAVE_STRNDUP 1
 /* #undef HAVE_STRNICMP */
+#define HAVE_STRSIGNAL 1
 /* #undef HAVE_STRUCT_NLIST_N_UN_N_NAME */
 /* #undef HAVE_SYS_DIR_H */
 /* #undef HAVE_SYS_NDIR_H */
 #define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_RESOURCE_H 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TIMEB_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_TTYNAME 1
+#define HAVE_UINTMAX_T 1
 /* #undef HAVE_UNION_WAIT */
+#define HAVE_UNISTD_H 1
 #define HAVE_UNSIGNED_LONG_LONG_INT 1
+#define HAVE_VFORK 1
 /* #undef HAVE_VFORK_H */
 #define HAVE_WAIT3 1
+#define HAVE_WAITPID 1
+#define HAVE_WORKING_FORK 1
+#define HAVE_WORKING_VFORK 1
 #define MAKE_HOST "x86_64-linux-musl"
 #define MAKE_JOBSERVER 1
 #define MAKE_LOAD 1
@@ -73,10 +124,12 @@
 #define PACKAGE_URL "http://www.gnu.org/software/make/"
 #define PACKAGE_VERSION "4.2.1"
 #define PATH_SEPARATOR_CHAR ':'
+#define RETSIGTYPE void
 #define SCCS_GET "get"
 /* #undef SCCS_GET_MINUS_G */
 /* #undef STACK_DIRECTION */
 /* #undef STAT_MACROS_BROKEN */
+#define STDC_HEADERS 1
 #define ST_MTIM_NSEC st_mtim.tv_nsec
 /* #undef SVR4 */
 #define TIME_WITH_SYS_TIME 1
