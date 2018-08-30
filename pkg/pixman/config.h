a668c326a39cb2643c76908ba271763cf9f89c42
Michael Forney <mforney@mforney.org>
2018-08-30 13:56:29 -0700

    pixman: Don't use config-posix.h

--- pkg/pixman/config.h
+++ pkg/pixman/config.h
@@ -1,17 +1,32 @@
-#include <config-posix.h>
-
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
+#define HAVE_ALARM 1
 #define HAVE_BUILTIN_CLZ /**/
+#define HAVE_DLFCN_H 1
 #define HAVE_FEDIVBYZERO 1
 /* #undef HAVE_FEENABLEEXCEPT */
+#define HAVE_FENV_H 1
 #define HAVE_FLOAT128 /**/
 #define HAVE_GCC_VECTOR_EXTENSIONS /**/
 /* #undef HAVE_GETISAX */
 #define HAVE_GETPAGESIZE 1
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_INTTYPES_H 1
 /* #undef HAVE_LIBPIXMAN_1 */
 /* #undef HAVE_LIBPNG */
 #define HAVE_MEMORY_H 1
+#define HAVE_MMAP 1
+#define HAVE_MPROTECT 1
+#define HAVE_POSIX_MEMALIGN 1
 #define HAVE_PTHREADS /**/
+#define HAVE_SIGACTION 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_MMAN_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
 #define LT_OBJDIR ".libs/"
 #define PACKAGE "pixman"
 #define PACKAGE_BUGREPORT "pixman@lists.freedesktop.org"
@@ -22,6 +37,7 @@
 #define PACKAGE_VERSION "0.34.0"
 /* #undef PIXMAN_TIMERS */
 #define SIZEOF_LONG 8
+#define STDC_HEADERS 1
 #define TLS __thread
 #define TOOLCHAIN_SUPPORTS_ATTRIBUTE_CONSTRUCTOR /**/
 /* #undef USE_ARM_IWMMXT */
