ad59e960b0170c57ed954336508b8ade4f899fc5
Michael Forney <mforney@mforney.org>
2018-11-02 15:44:51 -0700

    libinput: Update to 1.12.2

--- pkg/libinput/config.h
+++ pkg/libinput/config.h
@@ -1,3 +1,5 @@
-#define LIBINPUT_VERSION "1.11.3"
 #define HAVE_LIBWACOM 0
+#define HAVE_VERSIONSORT 1
+#define LIBINPUT_QUIRKS_DIR "/share/libinput"
+#define LIBINPUT_QUIRKS_OVERRIDE_FILE "/etc/libinput/local-overrides.quirks"
 #define _GNU_SOURCE 1
--- pkg/libinput/gen.lua
+++ pkg/libinput/gen.lua
@@ -37,6 +37,7 @@ lib('libinput.a', [[
 		filter-trackpoint.c
 		netlink-seat.c
 		path-seat.c
+		quirks.c
 		timer.c
 	)
 	$builddir/pkg/(
@@ -44,5 +45,37 @@ lib('libinput.a', [[
 		mtdev/libmtdev.a
 	)
 ]])
+
+local quirks = {
+	'10-generic-keyboard.quirks',
+	'10-generic-lid.quirks',
+	'10-generic-trackball.quirks',
+	'30-vendor-aiptek.quirks',
+	'30-vendor-alps.quirks',
+	'30-vendor-cyapa.quirks',
+	'30-vendor-elantech.quirks',
+	'30-vendor-huion.quirks',
+	'30-vendor-ibm.quirks',
+	'30-vendor-kensington.quirks',
+	'30-vendor-logitech.quirks',
+	'30-vendor-microsoft.quirks',
+	'30-vendor-razer.quirks',
+	'30-vendor-synaptics.quirks',
+	'30-vendor-wacom.quirks',
+	'30-vendor-vmware.quirks',
+	'50-system-acer.quirks',
+	'50-system-apple.quirks',
+	'50-system-asus.quirks',
+	'50-system-chicony.quirks',
+	'50-system-cyborg.quirks',
+	'50-system-dell.quirks',
+	'50-system-google.quirks',
+	'50-system-hp.quirks',
+	'50-system-lenovo.quirks',
+	'50-system-system76.quirks',
+}
+for _, quirk in ipairs(quirks) do
+	file('share/libinput/'..quirk, '644', '$srcdir/quirks/'..quirk)
+end
 
 fetch 'git'
