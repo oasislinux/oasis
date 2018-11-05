e650796725ad68a3705a5a7d05bfbe9db3165933
Michael Forney <mforney@mforney.org>
2018-11-05 12:43:41 -0800

    expat: Use musl 1.1.20 getrandom wrapper

--- pkg/expat/expat_config.h
+++ pkg/expat/expat_config.h
@@ -5,7 +5,7 @@
 #define HAVE_DLFCN_H 1
 #define HAVE_FCNTL_H 1
 #define HAVE_GETPAGESIZE 1
-/* #undef HAVE_GETRANDOM */
+#define HAVE_GETRANDOM 1
 #define HAVE_INTTYPES_H 1
 /* #undef HAVE_LIBBSD */
 #define HAVE_MEMMOVE 1
@@ -15,7 +15,7 @@
 #define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
-#define HAVE_SYSCALL_GETRANDOM 1
+/* #undef HAVE_SYSCALL_GETRANDOM */
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TYPES_H 1
