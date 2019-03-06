c7d55edad461cbf326803b3ced8b8187470f5b6a
Michael Forney <mforney@mforney.org>
2019-03-05 23:58:15 -0800

    bc: Replace with Gavin Howard's implementation

--- pkg/bc/config.h
+++ pkg/bc/config.h
@@ -1,40 +1,8 @@
-#define BC_COPYRIGHT "Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006 Free Software Foundation, Inc."
-#define DC_COPYRIGHT "Copyright 1994, 1997, 1998, 2000, 2001, 2004, 2005, 2006 Free Software Foundation, Inc."
-#define DC_VERSION "1.3.95"
-/* #undef HAVE_DOPRNT */
-#define HAVE_ERRNO_H 1
-#define HAVE_FSTAT 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_ISGRAPH 1
-/* #undef HAVE_LIB_H */
-#define HAVE_LIMITS_H 1
-#define HAVE_MEMORY_H 1
-#define HAVE_SETVBUF 1
-#define HAVE_STDARG_H 1
-#define HAVE_STDDEF_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_STRING_H 1
-#define HAVE_STRTOL 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_VPRINTF 1
-/* #undef LIBEDIT */
-#define PACKAGE "bc"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_NAME "bc"
-#define PACKAGE_STRING "bc 1.06.95"
-#define PACKAGE_TARNAME "bc"
-#define PACKAGE_VERSION "1.06.95"
-/* #undef READLINE */
-#define STDC_HEADERS 1
-#define VERSION "1.06.95"
-/* #undef YYTEXT_POINTER */
-/* #undef _MINIX */
-/* #undef _POSIX_1_SOURCE */
-/* #undef _POSIX_SOURCE */
-/* #undef const */
-/* #undef ptrdiff_t */
-/* #undef size_t */
+#define BC_ENABLED 1
+#define DC_ENABLED 0
+
+#define BC_ENABLE_EXTRA_MATH 1
+#define BC_ENABLE_HISTORY 1
+#define BC_ENABLE_REFERENCES 1
+#define BC_ENABLE_SIGNALS 1
+#define BC_NUM_KARATSUBA_LEN 32
--- pkg/bc/gen.lua
+++ pkg/bc/gen.lua
@@ -1,15 +1,48 @@
 cflags{
-	'-D HAVE_CONFIG_H',
-	'-I $dir',
-	'-I $srcdir/h',
+	'-D NDEBUG',
+	'-D VERSION=1.1.4',
+	'-D _POSIX_C_SOURCE=200809L',
+	'-I $srcdir/include',
+	'-include $dir/config.h',
 }
 
-lib('libbc.a', {'lib/number.c'})
+sub('tools.ninja', function()
+	exe('strgen', {'gen/strgen.c'})
+	rule('strgen', [[$outdir/strgen $in $out.tmp $name $header '$label' '$define' $removetabs && mv $out.tmp $out]])
+	build('strgen', '$outdir/bc_help.c', {'$srcdir/gen/bc_help.txt', '|', '$outdir/strgen'}, {
+		name='bc_help', header='bc.h', label='', define='BC_ENABLED', removetabs='',
+	})
+	build('strgen', '$outdir/dc_help.c', {'$srcdir/gen/dc_help.txt', '|', '$outdir/strgen'}, {
+		name='dc_help', header='dc.h', label='', define='DC_ENABLED', removetabs='',
+	})
+	build('strgen', '$outdir/lib.c', {'$srcdir/gen/lib.bc', '|', '$outdir/strgen'}, {
+		name='bc_lib', header='bc.h', label='bc_lib_name', define='BC_ENABLED', removetabs='1',
+	})
+	build('strgen', '$outdir/lib2.c', {'$srcdir/gen/lib2.bc', '|', '$outdir/strgen'}, {
+		name='bc_lib2', header='bc.h', label='bc_lib2_name', define='BC_ENABLED && BC_ENABLE_EXTRA_MATH', removetabs='1',
+	})
+end)
+
 exe('bin/bc', [[
-	bc/(main.c bc.c scan.c execute.c load.c storage.c util.c global.c warranty.c)
-	libbc.a
+	src/(
+		args.c
+		data.c
+		lang.c
+		lex.c
+		main.c
+		num.c
+		parse.c
+		program.c
+		read.c
+		vector.c
+		vm.c
+		bc/(bc.c lex.c parse.c)
+		dc/(dc.c lex.c parse.c)
+		history/history.c
+	)
+	$outdir/(bc_help.c dc_help.c lib.c lib2.c)
 ]])
 file('bin/bc', '755', '$outdir/bin/bc')
-man{'doc/bc.1'}
+man{'manuals/bc.1'}
 
-fetch 'curl'
+fetch 'git'
