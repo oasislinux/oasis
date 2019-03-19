10a498f912d67a62c960035c005f3cd6990e8ac6
Michael Forney <mforney@mforney.org>
2019-03-18 23:48:05 -0700

    mupdf: Update to 1.14.0

--- pkg/mupdf/gen.lua
+++ pkg/mupdf/gen.lua
@@ -10,7 +10,7 @@ cflags{
 	'-D FZ_ENABLE_JPX=0',
 	'-D HAVE_PTHREAD',
 	'-D NOTO_SMALL',
-	'-D NOCJK',
+	'-D NO_CJK',
 	'-D NO_ICC',
 	'-D SHARE_JPEG',
 	'-I $srcdir/include',
@@ -24,24 +24,16 @@ cflags{
 sub('tools.ninja', function()
 	toolchain 'host'
 	cflags{'-Wall'}
-
-	exe('namedump', {'scripts/namedump.c'})
 	exe('hexdump', {'scripts/hexdump.c'})
 end)
 
-rule('namedump', '$outdir/namedump $in $out')
-build('namedump', {'$outdir/mupdf/pdf/name-table.h', '$outdir/pdf-name-table.h'}, {
-	'$srcdir/resources/pdf/names.txt', '|', '$outdir/namedump',
-})
-
-rule('hexdump', '$outdir/hexdump -s -p $srcdir/ $out.tmp $in && mv $out.tmp $out')
+rule('hexdump', '$outdir/hexdump -s $out.tmp $in && mv $out.tmp $out')
 local fonts = lines('fonts.txt')
 for _, font in ipairs(fonts) do
 	build('hexdump', '$outdir/'..font..'.c', {'$srcdir/resources/fonts/'..font, '|', '$outdir/hexdump'})
 end
 
 pkg.deps = {
-	'$outdir/mupdf/pdf/name-table.h',
 	'pkg/curl/headers',
 	'pkg/freetype/fetch',
 	'pkg/jbig2dec/fetch',
@@ -57,14 +49,23 @@ lib('libmupdf.a', {
 	'$builddir/pkg/libjpeg-turbo/libjpeg-turbo.a',
 	'$builddir/pkg/zlib/libz.a',
 })
+lib('libmupdf-threads.a', {
+	'source/helpers/mu-threads/mu-threads.c',
+})
+lib('libmupdf-pkcs7.a', {
+	'source/helpers/pkcs7/pkcs7-check.c',
+	'source/helpers/pkcs7/pkcs7-openssl.c',
+})
 
 exe('bin/mutool', [[
 	source/tools/(
-		mutool.c muconvert.c mudraw.c murun.c mutrace.c
+		mutool.c muconvert.c mudraw.c murun.c mutrace.c cmapdump.c
 		pdfclean.c pdfcreate.c pdfextract.c pdfinfo.c pdfmerge.c pdfpages.c
 		pdfportfolio.c pdfposter.c pdfshow.c pdfsign.c
 	)
 	libmupdf.a.d
+	libmupdf-pkcs7.a
+	libmupdf-threads.a
 ]])
 file('bin/mutool', '755', '$outdir/bin/mutool')
 
@@ -87,6 +88,7 @@ cc('platform/x11/wl_main.c', {
 exe('bin/mupdf', [[
 	platform/x11/(pdfapp.c curl_stream.c.o wl_main.c.o)
 	libmupdf.a.d
+	libmupdf-pkcs7.a
 	$builddir/pkg/(
 		curl/libcurl.a.d
 		jbig2dec/libjbig2dec.a
