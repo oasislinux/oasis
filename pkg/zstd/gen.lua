a10eff96c55e452fd0bd906f80b1f61736321c4e
Michael Forney <mforney@mforney.org>
2019-04-19 01:25:01 -0700

    Add zstd 1.4.0

---
+++ pkg/zstd/gen.lua
@@ -0,0 +1,54 @@
+cflags{
+	'-Wall', '-Wextra', '-pedantic',
+	'-D ZSTD_LEGACY_SUPPORT=0',
+	'-D ZSTD_MULTITHREAD',
+	'-D XXH_NAMESPACE=ZSTD_',
+	'-D ZSTD_NOBENCH',
+	'-D ZSTD_NODICT',
+	'-I $srcdir/lib',
+	'-I $srcdir/lib/common',
+	'-I $srcdir/lib/dictBuilder',
+}
+
+lib('libzstd.a', [[lib/(
+	common/(
+		debug.c
+		entropy_common.c
+		error_private.c
+		fse_decompress.c
+		pool.c
+		threading.c
+		xxhash.c
+		zstd_common.c
+	)
+	compress/(
+		fse_compress.c
+		hist.c
+		huf_compress.c
+		zstd_compress.c
+		zstd_double_fast.c
+		zstd_fast.c
+		zstd_lazy.c
+		zstd_ldm.c
+		zstd_opt.c
+		zstdmt_compress.c
+	)
+	decompress/(
+		huf_decompress.c
+		zstd_ddict.c
+		zstd_decompress.c
+		zstd_decompress_block.c
+	)
+)]])
+
+exe('zstd', [[
+	programs/(zstdcli.c util.c timefn.c fileio.c)
+	libzstd.a
+]])
+file('bin/zstd', '755', '$outdir/zstd')
+sym('bin/unzstd', 'zstd')
+sym('bin/zstdcat', 'zstd')
+
+man{'programs/zstd.1'}
+
+fetch 'git'
