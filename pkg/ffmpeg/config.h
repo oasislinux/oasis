9ee08747f740979005a30b4ecf4930640cf33ac8
Michael Forney <mforney@mforney.org>
2019-08-06 12:48:56 -0700

    ffmpeg: Update to 4.2

--- pkg/ffmpeg/config.h
+++ pkg/ffmpeg/config.h
@@ -2,10 +2,10 @@
 #define FFMPEG_CONFIG_H
 #define FFMPEG_CONFIGURATION ""
 #define FFMPEG_LICENSE "LGPL version 2.1 or later"
-#define CONFIG_THIS_YEAR 2018
+#define CONFIG_THIS_YEAR 2019
 #define FFMPEG_DATADIR "/share/ffmpeg"
 #define AVCONV_DATADIR "/share/ffmpeg"
-#define CC_IDENT "gcc 7.3.0 (GCC)"
+#define CC_IDENT "gcc 8.3.0 (GCC)"
 #define av_restrict restrict
 #define EXTERN_PREFIX ""
 #define EXTERN_ASM
