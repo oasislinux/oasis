307c22e46d1a5613210ccb8e9b7ea5bcb0cc601a
Michael Forney <mforney@mforney.org>
2019-10-17 20:40:53 -0700

    libdrm: Update to 2.4.100

--- pkg/libdrm/README.md
+++ pkg/libdrm/README.md
@@ -3,12 +3,7 @@
 # `config.h`
 Generated with
 
-	./configure \
-		--disable-amdgpu \
-		--disable-intel \
-		--disable-nouveau \
-		--disable-radeon \
-		--disable-vmwgfx
+	meson build -Dintel=false -Dnouveau=false -Dradeon=false -Dvmwgfx=false -Dlibkms=false
 
 `HAVE_INTEL`, `HAVE_NOUVEAU`, and `HAVE_AMDGPU` were removed since they get
 defined in `gen.lua` based on the keys in `config.video_drivers`.
--- pkg/libdrm/config.h
+++ pkg/libdrm/config.h
@@ -1,66 +1,17 @@
-/* #undef CRAY_STACKSEG_END */
-/* #undef C_ALLOCA */
-#define HAVE_ALLOCA 1
-#define HAVE_ALLOCA_H 1
+#define HAVE_ALLOCA_H
 #define HAVE_CAIRO 0
-#define HAVE_CLOCK_GETTIME 1
-#define HAVE_DLFCN_H 1
 #define HAVE_EXYNOS 0
 #define HAVE_FREEDRENO_KGSL 0
-#define HAVE_INTTYPES_H 1
 #define HAVE_LIBDRM_ATOMIC_PRIMITIVES 1
 #define HAVE_LIB_ATOMIC_OPS 0
-#define HAVE_MEMORY_H 1
 #define HAVE_OPEN_MEMSTREAM 1
 #define HAVE_RADEON 0
-#define HAVE_STDINT_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_STRING_H 1
-#define HAVE_SYS_SELECT_H 1
-#define HAVE_SYS_STAT_H 1
-/* #undef HAVE_SYS_SYSCTL_H */
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_UNISTD_H 1
+#define HAVE_SYS_SELECT_H
+#undef HAVE_SYS_SYSCTL_H
 #define HAVE_VALGRIND 0
 #define HAVE_VC4 0
 #define HAVE_VISIBILITY 1
 #define HAVE_VMWGFX 0
-#define LT_OBJDIR ".libs/"
-/* #undef MAJOR_IN_MKDEV */
 #define MAJOR_IN_SYSMACROS 1
-#define PACKAGE "libdrm"
-#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=DRI"
-#define PACKAGE_NAME "libdrm"
-#define PACKAGE_STRING "libdrm 2.4.99"
-#define PACKAGE_TARNAME "libdrm"
-#define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.4.99"
-/* #undef STACK_DIRECTION */
-#define STDC_HEADERS 1
 #define UDEV 0
-#ifndef _ALL_SOURCE
-# define _ALL_SOURCE 1
-#endif
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE 1
-#endif
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
-#endif
-#ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
-#endif
-#ifndef __EXTENSIONS__
-# define __EXTENSIONS__ 1
-#endif
-#define VERSION "2.4.99"
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
-#endif
-/* #undef _FILE_OFFSET_BITS */
-/* #undef _LARGE_FILES */
-/* #undef _MINIX */
-/* #undef _POSIX_1_SOURCE */
-/* #undef _POSIX_SOURCE */
-/* #undef size_t */
+#define _GNU_SOURCE 1
