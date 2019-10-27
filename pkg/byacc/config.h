3b073f5ad77a94468344c4d8d06f7e31ce1e9b4f
Michael Forney <mforney@mforney.org>
2019-10-26 21:46:37 -0700

    Add byacc 20190617

---
+++ pkg/byacc/config.h
@@ -0,0 +1,31 @@
+/* #undef GCC_NORETURN */
+/* #undef GCC_PRINTF */
+/* #undef GCC_PRINTFLIKE */
+/* #undef GCC_SCANF */
+/* #undef GCC_SCANFLIKE */
+/* #undef GCC_UNUSED */
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+/* #undef HAVE_LIBDBMALLOC */
+/* #undef HAVE_LIBDMALLOC */
+#define HAVE_MEMORY_H 1
+#define HAVE_MKSTEMP 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_VSNPRINTF 1
+/* #undef MAXTABLE */
+#define MIXEDCASE_FILENAMES 1
+/* #undef NO_LEAKS */
+#define STDC_HEADERS 1
+#define SYSTEM_NAME "linux-gnu"
+/* #undef USE_DBMALLOC */
+/* #undef USE_DMALLOC */
+/* #undef USE_VALGRIND */
+/* #undef YYBTYACC */
+/* #undef YY_NO_LEAKS */
+/* #undef mode_t */
---
+++ pkg/byacc/gen.lua
@@ -0,0 +1,27 @@
+cflags{
+	'-Wall', '-pedantic',
+	'-D HAVE_CONFIG_H',
+	'-D YYPATCH=20190617',
+	'-I $dir',
+}
+
+exe('yacc', {
+	'closure.c',
+	'error.c',
+	'graph.c',
+	'lalr.c',
+	'lr0.c',
+	'main.c',
+	'mkpar.c',
+	'mstring.c',
+	'output.c',
+	'reader.c',
+	'yaccpar.c',
+	'symtab.c',
+	'verbose.c',
+	'warshall.c',
+})
+file('bin/yacc', '755', '$outdir/yacc')
+man{'yacc.1'}
+
+fetch 'curl'
