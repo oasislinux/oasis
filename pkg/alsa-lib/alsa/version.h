e0b1383ffb09c5d241a29949f33124cb01a0ad81
Michael Forney <mforney@mforney.org>
2019-11-13 23:24:15 -0800

    alsa-lib: Update to 1.2.1

--- pkg/alsa-lib/alsa/version.h
+++ pkg/alsa-lib/alsa/version.h
@@ -3,13 +3,13 @@
  */
 
 #define SND_LIB_MAJOR		1 /**< major number of library version */
-#define SND_LIB_MINOR		1 /**< minor number of library version */
-#define SND_LIB_SUBMINOR	9 /**< subminor number of library version */
+#define SND_LIB_MINOR		2 /**< minor number of library version */
+#define SND_LIB_SUBMINOR	1 /**< subminor number of library version */
 #define SND_LIB_EXTRAVER	1000000 /**< extra version number, used mainly for betas */
 /** library version */
 #define SND_LIB_VERSION		((SND_LIB_MAJOR<<16)|\
 				 (SND_LIB_MINOR<<8)|\
 				  SND_LIB_SUBMINOR)
 /** library version (string) */
-#define SND_LIB_VERSION_STR	"1.1.9"
+#define SND_LIB_VERSION_STR	"1.2.1"
 
