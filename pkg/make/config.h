511b4673b7177f3e3be3b7ae6b867bdf10190b1c
Michael Forney <mforney@mforney.org>
2020-01-21 14:19:18 -0800

    make: Update to 4.3

--- pkg/make/config.h
+++ pkg/make/config.h
@@ -2,14 +2,34 @@
 #define LIBDIR "/lib"
 #define LOCALEDIR "/share/locale"
 
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+/* #undef BITSIZEOF_PTRDIFF_T */
+/* #undef BITSIZEOF_SIG_ATOMIC_T */
+/* #undef BITSIZEOF_SIZE_T */
+/* #undef BITSIZEOF_WCHAR_T */
+/* #undef BITSIZEOF_WINT_T */
 /* #undef CLOSEDIR_VOID */
 /* #undef CRAY_STACKSEG_END */
 /* #undef C_ALLOCA */
-/* #undef C_GETLOADAVG */
 /* #undef DGUX */
+/* #undef DOUBLE_SLASH_IS_DISTINCT_ROOT */
 /* #undef ENABLE_NLS */
+/* #undef FCNTL_DUPFD_BUGGY */
 #define FILE_TIMESTAMP_HI_RES 1
-/* #undef GETLOADAVG_PRIVILEGED */
+#define GNULIB_FSCANF 1
+#define GNULIB_MSVC_NOTHROW 1
+/* #undef GNULIB_PRINTF_ATTRIBUTE_FLAVOR_GNU */
+#define GNULIB_SCANF 1
+#define GNULIB_STRERROR 1
+#define GNULIB_TEST_ACCESS 1
+#define GNULIB_TEST_CLOSE 1
+#define GNULIB_TEST_DUP2 1
+#define GNULIB_TEST_FCNTL 1
+#define GNULIB_TEST_GETDTABLESIZE 1
+#define GNULIB_TEST_GETLOADAVG 1
+#define GNULIB_TEST_MALLOC_POSIX 1
+#define GNULIB_TEST_STPCPY 1
+#define GNULIB_TEST_STRERROR 1
 #define HAVE_ALLOCA 1
 #define HAVE_ALLOCA_H 1
 #define HAVE_ATEXIT 1
@@ -22,64 +42,83 @@
 #define HAVE_DECL_DLERROR 1
 #define HAVE_DECL_DLOPEN 1
 #define HAVE_DECL_DLSYM 1
+#define HAVE_DECL_GETDTABLESIZE 1
+#define HAVE_DECL_PROGRAM_INVOCATION_NAME 1
+#define HAVE_DECL_PROGRAM_INVOCATION_SHORT_NAME 1
+#define HAVE_DECL_STRERROR_R 1
 #define HAVE_DECL_SYS_SIGLIST 0
 #define HAVE_DECL__SYS_SIGLIST 0
+#define HAVE_DECL___ARGV 0
 #define HAVE_DECL___SYS_SIGLIST 0
 #define HAVE_DIRENT_H 1
 /* #undef HAVE_DOS_PATHS */
 #define HAVE_DUP 1
 #define HAVE_DUP2 1
+#define HAVE_FCNTL 1
 #define HAVE_FCNTL_H 1
 #define HAVE_FDOPEN 1
-#define HAVE_FILENO 1
 #define HAVE_FORK 1
 #define HAVE_GETCWD 1
+#define HAVE_GETDTABLESIZE 1
+/* #undef HAVE_GETEXECNAME */
 #define HAVE_GETGROUPS 1
 /* #undef HAVE_GETHOSTBYNAME */
 /* #undef HAVE_GETHOSTNAME */
-#define HAVE_GETLOADAVG 1
+/* #undef HAVE_GETPROGNAME */
 #define HAVE_GETRLIMIT 1
 /* #undef HAVE_GETTEXT */
 #define HAVE_GETTIMEOFDAY 1
 /* #undef HAVE_GUILE */
-/* #undef HAVE_ICONV */
+#define HAVE_ICONV 1
 #define HAVE_INTTYPES_H 1
 #define HAVE_ISATTY 1
 /* #undef HAVE_LIBDGC */
 /* #undef HAVE_LIBKSTAT */
+/* #undef HAVE_LIBPERFSTAT */
 #define HAVE_LIMITS_H 1
 #define HAVE_LOCALE_H 1
+#define HAVE_LONG_LONG_INT 1
 #define HAVE_LSTAT 1
 /* #undef HAVE_MACH_MACH_H */
+#define HAVE_MALLOC_POSIX 1
 #define HAVE_MEMORY_H 1
+#define HAVE_MEMRCHR 1
 #define HAVE_MKSTEMP 1
 #define HAVE_MKTEMP 1
+/* #undef HAVE_MSVC_INVALID_PARAMETER_HANDLER */
 /* #undef HAVE_NDIR_H */
 /* #undef HAVE_NLIST_H */
 #define HAVE_PIPE 1
+#define HAVE_POSIX_SPAWN 1
+#define HAVE_POSIX_SPAWNATTR_SETSIGMASK 1
 #define HAVE_PSELECT 1
 /* #undef HAVE_PSTAT_GETDYNAMIC */
 #define HAVE_READLINK 1
 #define HAVE_REALPATH 1
 #define HAVE_SA_RESTART 1
+/* #undef HAVE_SETDTABLESIZE */
 #define HAVE_SETEGID 1
 #define HAVE_SETEUID 1
 #define HAVE_SETLINEBUF 1
-/* #undef HAVE_SETLOCALE */
 #define HAVE_SETREGID 1
 #define HAVE_SETREUID 1
 #define HAVE_SETRLIMIT 1
 #define HAVE_SETVBUF 1
 #define HAVE_SIGACTION 1
+/* #undef HAVE_SIGNED_SIG_ATOMIC_T */
+/* #undef HAVE_SIGNED_WCHAR_T */
+/* #undef HAVE_SIGNED_WINT_T */
 /* #undef HAVE_SIGSETMASK */
 /* #undef HAVE_SOCKET */
+#define HAVE_SPAWN_H 1
 #define HAVE_STDINT_H 1
 #define HAVE_STDLIB_H 1
+#define HAVE_STPCPY 1
 #define HAVE_STRCASECMP 1
 /* #undef HAVE_STRCMPI */
 #define HAVE_STRCOLL 1
 #define HAVE_STRDUP 1
-#define HAVE_STRERROR 1
+#define HAVE_STRERROR_R 1
 /* #undef HAVE_STRICMP */
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
@@ -88,12 +127,19 @@
 #define HAVE_STRNDUP 1
 /* #undef HAVE_STRNICMP */
 #define HAVE_STRSIGNAL 1
+#define HAVE_STRUCT_DIRENT_D_TYPE 1
 /* #undef HAVE_STRUCT_NLIST_N_UN_N_NAME */
+#define HAVE_SYMLINK 1
+/* #undef HAVE_SYS_BITYPES_H */
 /* #undef HAVE_SYS_DIR_H */
+#define HAVE_SYS_FILE_H 1
+/* #undef HAVE_SYS_INTTYPES_H */
+/* #undef HAVE_SYS_LOADAVG_H */
 /* #undef HAVE_SYS_NDIR_H */
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_RESOURCE_H 1
 #define HAVE_SYS_SELECT_H 1
+#define HAVE_SYS_SOCKET_H 1
 #define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TIMEB_H 1
 #define HAVE_SYS_TIME_H 1
@@ -101,72 +147,248 @@
 #define HAVE_SYS_WAIT_H 1
 #define HAVE_TTYNAME 1
 #define HAVE_UINTMAX_T 1
+#define HAVE_UMASK 1
 /* #undef HAVE_UNION_WAIT */
 #define HAVE_UNISTD_H 1
 #define HAVE_UNSIGNED_LONG_LONG_INT 1
+/* #undef HAVE_VAR___PROGNAME */
 #define HAVE_VFORK 1
 /* #undef HAVE_VFORK_H */
 #define HAVE_WAIT3 1
 #define HAVE_WAITPID 1
+#define HAVE_WCHAR_H 1
+#define HAVE_WCHAR_T 1
+/* #undef HAVE_WINSOCK2_H */
+#define HAVE_WINT_T 1
 #define HAVE_WORKING_FORK 1
+#define HAVE_WORKING_O_NOATIME 1
+#define HAVE_WORKING_O_NOFOLLOW 1
 #define HAVE_WORKING_VFORK 1
-#define MAKE_HOST "x86_64-linux-musl"
+#define HAVE__BOOL 1
+/* #undef HAVE__SET_INVALID_PARAMETER_HANDLER */
+#define MAKE_HOST "x86_64-pc-linux-musl"
 #define MAKE_JOBSERVER 1
 #define MAKE_LOAD 1
 #define MAKE_SYMLINKS 1
-/* #undef NLIST_NAME_UNION */
-/* #undef NLIST_STRUCT */
+#ifndef __USE_MINGW_ANSI_STDIO
+# define __USE_MINGW_ANSI_STDIO 1
+#endif
+/* #undef N_NAME_POINTER */
 #define PACKAGE "make"
 #define PACKAGE_BUGREPORT "bug-make@gnu.org"
 #define PACKAGE_NAME "GNU make"
-#define PACKAGE_STRING "GNU make 4.2.1"
+#define PACKAGE_STRING "GNU make 4.3"
 #define PACKAGE_TARNAME "make"
 #define PACKAGE_URL "http://www.gnu.org/software/make/"
-#define PACKAGE_VERSION "4.2.1"
+#define PACKAGE_VERSION "4.3"
 #define PATH_SEPARATOR_CHAR ':'
+/* #undef PTRDIFF_T_SUFFIX */
+/* #undef REPLACE_STRERROR_0 */
 #define RETSIGTYPE void
 #define SCCS_GET "get"
 /* #undef SCCS_GET_MINUS_G */
+/* #undef SIG_ATOMIC_T_SUFFIX */
+/* #undef SIZE_T_SUFFIX */
 /* #undef STACK_DIRECTION */
 /* #undef STAT_MACROS_BROKEN */
 #define STDC_HEADERS 1
+/* #undef STRERROR_R_CHAR_P */
 #define ST_MTIM_NSEC st_mtim.tv_nsec
 /* #undef SVR4 */
 #define TIME_WITH_SYS_TIME 1
 /* #undef UMAX */
 /* #undef UMAX4_3 */
+#define USE_POSIX_SPAWN 1
 #ifndef _ALL_SOURCE
 # define _ALL_SOURCE 1
+#endif
+#ifndef _DARWIN_C_SOURCE
+# define _DARWIN_C_SOURCE 1
 #endif
 #ifndef _GNU_SOURCE
 # define _GNU_SOURCE 1
 #endif
+#ifndef _NETBSD_SOURCE
+# define _NETBSD_SOURCE 1
+#endif
+#ifndef _OPENBSD_SOURCE
+# define _OPENBSD_SOURCE 1
+#endif
 #ifndef _POSIX_PTHREAD_SEMANTICS
 # define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_ATTRIBS_EXT__
+# define __STDC_WANT_IEC_60559_ATTRIBS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_BFP_EXT__
+# define __STDC_WANT_IEC_60559_BFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_DFP_EXT__
+# define __STDC_WANT_IEC_60559_DFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_FUNCS_EXT__
+# define __STDC_WANT_IEC_60559_FUNCS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_TYPES_EXT__
+# define __STDC_WANT_IEC_60559_TYPES_EXT__ 1
+#endif
+#ifndef __STDC_WANT_LIB_EXT2__
+# define __STDC_WANT_LIB_EXT2__ 1
+#endif
+#ifndef __STDC_WANT_MATH_SPEC_FUNCS__
+# define __STDC_WANT_MATH_SPEC_FUNCS__ 1
 #endif
 #ifndef _TANDEM_SOURCE
 # define _TANDEM_SOURCE 1
 #endif
+#ifndef _XOPEN_SOURCE
+/* # undef _XOPEN_SOURCE */
+#endif
+#ifndef _HPUX_ALT_XOPEN_SOCKET_API
+# define _HPUX_ALT_XOPEN_SOCKET_API 1
+#endif
 #ifndef __EXTENSIONS__
 # define __EXTENSIONS__ 1
 #endif
-#define VERSION "4.2.1"
+#define VERSION "4.3"
+/* #undef WCHAR_T_SUFFIX */
 /* #undef WINDOWS32 */
+/* #undef WINT_T_SUFFIX */
 /* #undef WITH_DMALLOC */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
 #ifndef _DARWIN_USE_64_BIT_INODE
 # define _DARWIN_USE_64_BIT_INODE 1
 #endif
 /* #undef _FILE_OFFSET_BITS */
 /* #undef _LARGE_FILES */
 /* #undef _MINIX */
+#define _NETBSD_SOURCE 1
+#ifndef _Noreturn
+# if (defined __cplusplus \
+      && ((201103 <= __cplusplus && !(__GNUC__ == 4 && __GNUC_MINOR__ == 7)) \
+          || (defined _MSC_VER && 1900 <= _MSC_VER)) \
+      && 0)
+#  define _Noreturn [[noreturn]]
+# elif ((!defined __cplusplus || defined __clang__) \
+        && (201112 <= (defined __STDC_VERSION__ ? __STDC_VERSION__ : 0)  \
+            || 4 < __GNUC__ + (7 <= __GNUC_MINOR__)))
+# elif 2 < __GNUC__ + (8 <= __GNUC_MINOR__) || 0x5110 <= __SUNPRO_C
+#  define _Noreturn __attribute__ ((__noreturn__))
+# elif 1200 <= (defined _MSC_VER ? _MSC_VER : 0)
+#  define _Noreturn __declspec (noreturn)
+# else
+#  define _Noreturn
+# endif
+#endif
 /* #undef _POSIX_1_SOURCE */
 /* #undef _POSIX_SOURCE */
+#define _USE_STD_STAT 1
+/* #undef __STDC_CONSTANT_MACROS */
+/* #undef __STDC_LIMIT_MACROS */
+#define _GL_ASYNC_SAFE
 /* #undef const */
+/* #undef eaccess */
+#if (((defined __APPLE__ && defined __MACH__) \
+      || defined __DragonFly__ || defined __FreeBSD__) \
+     && (defined __header_inline \
+         ? (defined __cplusplus && defined __GNUC_STDC_INLINE__ \
+            && ! defined __clang__) \
+         : ((! defined _DONT_USE_CTYPE_INLINE_ \
+             && (defined __GNUC__ || defined __cplusplus)) \
+            || (defined _FORTIFY_SOURCE && 0 < _FORTIFY_SOURCE \
+                && defined __GNUC__ && ! defined __cplusplus))))
+# define _GL_EXTERN_INLINE_STDHEADER_BUG
+#endif
+#if ((__GNUC__ \
+      ? defined __GNUC_STDC_INLINE__ && __GNUC_STDC_INLINE__ \
+      : (199901L <= __STDC_VERSION__ \
+         && !defined __HP_cc \
+         && !defined __PGI \
+         && !(defined __SUNPRO_C && __STDC__))) \
+     && !defined _GL_EXTERN_INLINE_STDHEADER_BUG)
+# define _GL_INLINE inline
+# define _GL_EXTERN_INLINE extern inline
+# define _GL_EXTERN_INLINE_IN_USE
+#elif (2 < __GNUC__ + (7 <= __GNUC_MINOR__) && !defined __STRICT_ANSI__ \
+       && !defined _GL_EXTERN_INLINE_STDHEADER_BUG)
+# if defined __GNUC_GNU_INLINE__ && __GNUC_GNU_INLINE__
+#  define _GL_INLINE extern inline __attribute__ ((__gnu_inline__))
+# else
+#  define _GL_INLINE extern inline
+# endif
+# define _GL_EXTERN_INLINE extern
+# define _GL_EXTERN_INLINE_IN_USE
+#else
+# define _GL_INLINE static _GL_UNUSED
+# define _GL_EXTERN_INLINE static _GL_UNUSED
+#endif
+#if __GNUC__ == 4 && 6 <= __GNUC_MINOR__
+# if defined __GNUC_STDC_INLINE__ && __GNUC_STDC_INLINE__
+#  define _GL_INLINE_HEADER_CONST_PRAGMA
+# else
+#  define _GL_INLINE_HEADER_CONST_PRAGMA \
+     _Pragma ("GCC diagnostic ignored \"-Wsuggest-attribute=const\"")
+# endif
+# define _GL_INLINE_HEADER_BEGIN \
+    _Pragma ("GCC diagnostic push") \
+    _Pragma ("GCC diagnostic ignored \"-Wmissing-prototypes\"") \
+    _Pragma ("GCC diagnostic ignored \"-Wmissing-declarations\"") \
+    _GL_INLINE_HEADER_CONST_PRAGMA
+# define _GL_INLINE_HEADER_END \
+    _Pragma ("GCC diagnostic pop")
+#else
+# define _GL_INLINE_HEADER_BEGIN
+# define _GL_INLINE_HEADER_END
+#endif
 /* #undef gid_t */
+#if defined __APPLE__ && defined __MACH__ && __APPLE_CC__ >= 5465 && !defined __cplusplus && __STDC_VERSION__ >= 199901L && !defined __GNUC_STDC_INLINE__
+# define __GNUC_STDC_INLINE__ 1
+#endif
+/* #undef mode_t */
 /* #undef off_t */
 /* #undef pid_t */
+#define restrict __restrict
+#if defined __SUNPRO_CC && !defined __RESTRICT && !defined __restrict__
+# define _Restrict
+# define __restrict__
+#endif
 /* #undef size_t */
 /* #undef ssize_t */
 /* #undef uid_t */
 /* #undef uintmax_t */
+#if __GNUC__ >= 3 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
+# define _GL_UNUSED __attribute__ ((__unused__))
+#else
+# define _GL_UNUSED
+#endif
+#define _UNUSED_PARAMETER_ _GL_UNUSED
+#if !defined __cplusplus || __GNUC__ > 4 \
+    || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
+# define _GL_UNUSED_LABEL _GL_UNUSED
+#else
+# define _GL_UNUSED_LABEL
+#endif
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 96)
+# define _GL_ATTRIBUTE_PURE __attribute__ ((__pure__))
+#else
+# define _GL_ATTRIBUTE_PURE /* empty */
+#endif
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
+# define _GL_ATTRIBUTE_CONST __attribute__ ((__const__))
+#else
+# define _GL_ATTRIBUTE_CONST /* empty */
+#endif
+#if 3 <= __GNUC__
+# define _GL_ATTRIBUTE_MALLOC __attribute__ ((__malloc__))
+#else
+# define _GL_ATTRIBUTE_MALLOC /* empty */
+#endif
 /* #undef vfork */
--- pkg/make/gen.lua
+++ pkg/make/gen.lua
@@ -1,21 +1,44 @@
 cflags{
 	'-D HAVE_CONFIG_H',
 	'-I $dir',
-	'-I $srcdir',
-	'-I $srcdir/glob',
+	'-I $srcdir/src',
+	'-I $srcdir/lib',
+	'-I $outdir',
 }
 
-lib('libglob.a', {'glob/glob.c', 'glob/fnmatch.c'})
+build('copy', '$outdir/fnmatch.h', '$srcdir/lib/fnmatch.in.h')
+build('copy', '$outdir/glob.h', '$srcdir/lib/glob.in.h')
+
+pkg.deps = {
+	'$outdir/fnmatch.h',
+	'$outdir/glob.h',
+}
+
+lib('libgnu.a', [[
+	lib/(
+		concat-filename.c exitfail.c findprog-in.c fnmatch.c
+		glob.c xalloc-die.c xconcat-filename.c
+	)
+]])
 
 exe('make', [[
-	ar.c arscan.c commands.c default.c dir.c expand.c file.c
-	function.c getopt.c getopt1.c guile.c implicit.c job.c load.c
-	loadapi.c main.c misc.c posixos.c output.c read.c remake.c
-	rule.c signame.c strcache.c variable.c version.c vpath.c
-	hash.c remote-stub.c
-	libglob.a
+	src/(
+		ar.c arscan.c commands.c
+		default.c dir.c expand.c
+		file.c function.c getopt.c
+		getopt1.c guile.c
+		hash.c implicit.c job.c
+		load.c loadapi.c main.c misc.c
+		output.c read.c remake.c
+		rule.c signame.c strcache.c
+		variable.c version.c vpath.c
+
+		posixos.c
+		remote-stub.c
+	)
+	libgnu.a
 ]])
 file('bin/make', '755', '$outdir/make')
 man{'make.1'}
 
-fetch 'git'
+fetch 'curl'
