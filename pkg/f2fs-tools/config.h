b8450d1d4cc65064a053328ca3e5939d44895c09
Michael Forney <mforney@mforney.org>
2020-02-22 17:22:30 -0800

    Add f2fs-tools 1.13.0

---
+++ pkg/f2fs-tools/config.h
@@ -0,0 +1,66 @@
+#define F2FS_MAJOR_VERSION 1
+#define F2FS_MINOR_VERSION 13
+#define F2FS_TOOLS_DATE "2019-09-24"
+#define F2FS_TOOLS_VERSION "1.13.0"
+/* #undef HAVE_ADD_KEY */
+/* #undef HAVE_ATTR_XATTR_H */
+#define HAVE_BYTESWAP_H 1
+#define HAVE_DECL_BSWAP_64 1
+#define HAVE_DLFCN_H 1
+#define HAVE_FALLOCATE 1
+#define HAVE_FCNTL_H 1
+#define HAVE_FSETXATTR 1
+#define HAVE_FSTAT 1
+#define HAVE_FSTAT64 1
+#define HAVE_GETMNTENT 1
+#define HAVE_INTTYPES_H 1
+/* #undef HAVE_KEYCTL */
+/* #undef HAVE_LIBBLKID */
+/* #undef HAVE_LIBSELINUX */
+#define HAVE_LINUX_BLKZONED_H 1
+#define HAVE_LINUX_FALLOC_H 1
+#define HAVE_LINUX_FS_H 1
+#define HAVE_LINUX_HDREG_H 1
+#define HAVE_LINUX_LIMITS_H 1
+#define HAVE_LINUX_POSIX_ACL_H 1
+#define HAVE_LINUX_TYPES_H 1
+#define HAVE_LINUX_XATTR_H 1
+/* #undef HAVE_LLSEEK */
+/* #undef HAVE_LLSEEK_PROTOTYPE */
+#define HAVE_LSEEK64 1
+#define HAVE_LSEEK64_PROTOTYPE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MEMSET 1
+#define HAVE_MNTENT_H 1
+#define HAVE_SCSI_SG_H 1
+#define HAVE_SETMNTENT 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+/* #undef HAVE_SYS_ACL_H */
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_SYS_MOUNT_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_SYSCALL_H 1
+#define HAVE_SYS_SYSMACROS_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_UTSNAME_H 1
+#define HAVE_SYS_XATTR_H 1
+#define HAVE_UNISTD_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "f2fs-tools"
+#define PACKAGE_BUGREPORT "linux-f2fs-devel@lists.sourceforge.net"
+#define PACKAGE_NAME "F2FS tools"
+#define PACKAGE_STRING "F2FS tools 1.13.0"
+#define PACKAGE_TARNAME "f2fs-tools"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.13.0"
+#define STDC_HEADERS 1
+#define VERSION "1.13.0"
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+/* #undef int32_t */
+/* #undef int8_t */
+/* #undef size_t */
---
+++ pkg/f2fs-tools/gen.lua
@@ -0,0 +1,32 @@
+cflags{
+	'-Wall',
+	'-D HAVE_CONFIG_H',
+	'-I $dir',
+	'-I $srcdir/include',
+	'-I $builddir/pkg/util-linux/include',
+}
+
+pkg.deps = {
+	'pkg/util-linux/headers',
+}
+
+lib('libf2fs.a', 'lib/(libf2fs.c libf2fs_io.c libf2fs_zoned.c nls_utf8.c)')
+
+exe('mkfs.f2fs', [[
+	mkfs/(f2fs_format_main.c f2fs_format.c f2fs_format_utils.c)
+	libf2fs.a
+	$builddir/pkg/util-linux/libuuid.a.d
+]])
+file('bin/mkfs.f2fs', '755', '$outdir/mkfs.f2fs')
+man{'man/mkfs.f2fs.8'}
+
+exe('fsck.f2fs', [[
+	fsck/(
+		main.c fsck.c dump.c mount.c defrag.c resize.c
+		node.c segment.c dir.c sload.c xattr.c
+		dict.c mkquota.c quotaio.c quotaio_tree.c quotaio_v2.c
+	)
+	libf2fs.a
+]])
+file('bin/fsck.f2fs', '755', '$outdir/fsck.f2fs')
+man{'man/fsck.f2fs.8'}
