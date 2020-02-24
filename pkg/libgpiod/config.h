b89685c32730730ac3dd04033d7eaf2307fda830
Michael Forney <mforney@mforney.org>
2020-02-24 13:13:01 -0800

    Add libgpiod 1.5

---
+++ pkg/libgpiod/config.h
@@ -0,0 +1,33 @@
+#define GPIOD_VERSION_STR "1.5"
+/* #undef HAVE_CATCH2_CATCH_HPP */
+/* #undef HAVE_CXX11 */
+#define HAVE_DIRENT_H 1
+#define HAVE_DLFCN_H 1
+#define HAVE_GETOPT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LINUX_GPIO_H 1
+#define HAVE_LINUX_VERSION_H 1
+#define HAVE_MALLOC 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_POLL_H 1
+#define HAVE_SYS_SIGNALFD_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_SYSMACROS_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libgpiod"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "libgpiod"
+#define PACKAGE_STRING "libgpiod 1.5"
+#define PACKAGE_TARNAME "libgpiod"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.5"
+#define STDC_HEADERS 1
+#define VERSION "1.5"
+#define _GNU_SOURCE /**/
+/* #undef malloc */
---
+++ pkg/libgpiod/gen.lua
@@ -0,0 +1,20 @@
+cflags{
+	'-std=c99', '-Wall', '-Wextra', '-Wpedantic',
+	'-I $srcdir/include',
+	'-I $builddir/pkg/linux-headers/include',
+	'-include $dir/config.h',
+}
+
+pkg.deps = {
+	'pkg/linux-headers/headers',
+}
+
+lib('libgpiod.a', 'lib/(core.c ctxless.c helpers.c iter.c misc.c)')
+
+cc('tools/tools-common.c')
+for _, tool in ipairs{'gpiodetect', 'gpioinfo', 'gpioget', 'gpioset', 'gpiomon', 'gpiofind'} do
+	exe(tool, {'tools/'..tool..'.c', 'tools/tools-common.c.o', 'libgpiod.a'})
+	file('bin/'..tool, '755', '$outdir/'..tool)
+end
+
+fetch 'git'
