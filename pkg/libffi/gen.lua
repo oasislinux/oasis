d2e28f93b38666ec241c679e404398d32a9a18ae
Michael Forney <mforney@mforney.org>
2020-02-29 14:58:12 -0800

    libffi: Add support for aarch64

--- pkg/libffi/gen.lua
+++ pkg/libffi/gen.lua
@@ -1,3 +1,4 @@
+local arch = config.target.platform:match('[^-]*')
 cflags{
 	'-I $dir',
 	'-I $outdir/include',
@@ -7,7 +8,7 @@ cflags{
 build('sed', '$outdir/include/ffi.h', '$srcdir/include/ffi.h.in', {
 	expr={
 		'-e s,@VERSION@,3.3,',
-		'-e s,@TARGET@,X86_64,',
+		string.format('-e s,@TARGET@,%s,', arch:upper()),
 		'-e s,@HAVE_LONG_DOUBLE@,1,',
 		'-e s,@HAVE_LONG_DOUBLE_VARIANT@,0,',
 		'-e s,@FFI_EXEC_TRAMPOLINE_TABLE@,0,',
@@ -15,15 +16,22 @@ build('sed', '$outdir/include/ffi.h', '$srcdir/include/ffi.h.in', {
 })
 
 pkg.hdrs = {
-	copy('$outdir/include', '$srcdir/src/x86', {'ffitarget.h'}),
+	copy('$outdir/include', '$srcdir/src/'..({x86_64='x86', aarch64='aarch64'})[arch], {'ffitarget.h'}),
 	'$outdir/include/ffi.h',
 	install=true,
 }
 
-lib('libffi.a', [[src/(
-	prep_cif.c types.c raw_api.c java_raw_api.c closures.c
-	x86/(ffi64.c unix64.S ffiw64.c win64.S)
-)]], {'$gendir/headers'})
+pkg.deps = {
+	'$gendir/headers',
+}
+
+lib('libffi.a', [[
+	src/(
+		prep_cif.c types.c raw_api.c java_raw_api.c closures.c
+		@aarch64 aarch64/(ffi.c sysv.S)
+		@x86_64 x86/(ffi64.c unix64.S ffiw64.c win64.S)
+	)
+]])
 file('lib/libffi.a', '644', '$outdir/libffi.a')
 
 fetch 'git'
