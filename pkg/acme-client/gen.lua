b1ae871f5ce31533aab6486a926e3ce660fb18a0
Michael Forney <mforney@mforney.org>
2020-04-22 23:49:42 -0700

    acme-client: Build against libtls-bearssl

--- pkg/acme-client/gen.lua
+++ pkg/acme-client/gen.lua
@@ -3,12 +3,14 @@ cflags{
 	'-D _GNU_SOURCE',  -- for memmem
 	'-I $basedir/pkg/openbsd/include',
 	'-I $builddir/pkg/libressl/include',
+	'-I $builddir/pkg/libtls-bearssl/include',
 	'-I $srcdir',
 	'-idirafter $basedir/pkg/openbsd/src/sys',
 }
 
 pkg.deps = {
 	'pkg/libressl/headers',
+	'pkg/libtls-bearssl/headers',
 }
 
 yacc('parse', 'parse.y')
@@ -16,7 +18,8 @@ exe('acme-client', [[
 	acctproc.c base64.c certproc.c chngproc.c dbg.c dnsproc.c
 	fileproc.c http.c jsmn.c json.c keyproc.c main.c netproc.c
 	$outdir/parse.tab.c revokeproc.c key.c util.c
-	$builddir/pkg/libressl/libtls.a.d
+	$builddir/pkg/libressl/libcrypto.a.d
+	$builddir/pkg/libtls-bearssl/libtls.a.d
 ]])
 file('bin/acme-client', '755', '$outdir/acme-client')
 man{'acme-client.1', 'acme-client.conf.5'}
