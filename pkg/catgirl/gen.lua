58ffa15ab948306181a5279c448c98d181bf09eb
Michael Forney <mforney@mforney.org>
2020-05-07 01:26:00 -0700

    Add catgirl git

---
+++ pkg/catgirl/gen.lua
@@ -0,0 +1,31 @@
+cflags{
+	'-std=c11', '-Wall', '-Wextra', '-Wpedantic',
+	'-D _GNU_SOURCE',
+	'-I $builddir/pkg/libtls-bearssl/include',
+	'-I $builddir/pkg/ncurses/include',
+}
+
+pkg.deps = {
+	'pkg/libtls-bearssl/headers',
+	'pkg/ncurses/headers',
+}
+
+exe('catgirl', [[
+	chat.c
+	command.c
+	complete.c
+	config.c
+	edit.c
+	handle.c
+	ignore.c
+	irc.c
+	log.c
+	ui.c
+	url.c
+	xdg.c
+	$builddir/pkg/libtls-bearssl/libtls.a.d
+	$builddir/pkg/ncurses/libncurses.a
+]])
+file('bin/catgirl', '755', '$outdir/catgirl')
+
+fetch 'git'
