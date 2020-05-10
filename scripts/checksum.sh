714ee37eea5590240508adadc8f5d81579ff890d
Michael Forney <mforney@mforney.org>
2020-05-10 13:47:12 -0700

    Support checksum via sha256(1)

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -1,8 +1,10 @@
 if command -v sha256sum >/dev/null; then
 	exec sha256sum "$@"
+elif command -v sha256 >/dev/null; then
+	exec sha256 "$@"
 elif command -v shasum >/dev/null; then
 	exec shasum -a 256 "$@"
 else
-	printf '%s: sha256sum or shasum required\n' "$0" >&2
+	printf '%s: sha256sum, sha256, or shasum required\n' "$0" >&2
 	exit 1
 fi
