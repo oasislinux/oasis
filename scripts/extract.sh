91fc847d0c9794c9ecc3043e970a4cc552b05ea9
Michael Forney <mforney@mforney.org>
2020-05-10 13:44:30 -0700

    Fix extraction with pax

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -11,7 +11,7 @@ esac
 if command -v bsdtar >/dev/null; then
 	exec bsdtar -xf "$file" "$@"
 elif command -v pax >/dev/null; then
-	"$tool" -d -c "$file" | pax "$@"
+	"$tool" -d -c "$file" | pax -r "$@"
 else
 	printf '%s: bsdtar or pax is required' "$0" >&2
 	exit 1
