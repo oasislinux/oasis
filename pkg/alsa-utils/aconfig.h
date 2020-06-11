911ed5c6258ade5df03e595e2802c9d32118b86d
Michael Forney <mforney@mforney.org>
2020-06-10 17:44:55 -0700

    alsa-utils: Update to 1.2.3

--- pkg/alsa-utils/aconfig.h
+++ pkg/alsa-utils/aconfig.h
@@ -39,14 +39,14 @@
 #define PACKAGE "alsa-utils"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "alsa-utils"
-#define PACKAGE_STRING "alsa-utils 1.2.2"
+#define PACKAGE_STRING "alsa-utils 1.2.3"
 #define PACKAGE_TARNAME "alsa-utils"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.2.2"
+#define PACKAGE_VERSION "1.2.3"
 #define SOUNDSDIR "/share/sounds/alsa"
 #define STDC_HEADERS 1
 #define TIME_WITH_SYS_TIME 1
-#define VERSION "1.2.2"
+#define VERSION "1.2.3"
 /* #undef WITH_FFADO */
 #ifndef _DARWIN_USE_64_BIT_INODE
 # define _DARWIN_USE_64_BIT_INODE 1
