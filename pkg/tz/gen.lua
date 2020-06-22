300196a0d281c5f54ed315efbd536e941ac58d34
Michael Forney <mforney@mforney.org>
2020-06-22 00:11:34 -0700

    tz: Sort zones to produce stable output

--- pkg/tz/gen.lua
+++ pkg/tz/gen.lua
@@ -22,7 +22,7 @@ local srcs = {
 local zones = {}
 local data = load 'data.lua'
 for _, src in ipairs(srcs) do
-	for name, target in pairs(data[src]) do
+	for _, name, target in sortedpairs(data[src]) do
 		if target then
 			target = name:gsub('[^/]+', '..'):sub(1, -3)..target
 			sym('share/zoneinfo/'..name, target)
