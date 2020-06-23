53e09653d94bb395beb81742ed4dc1dc09b76829
Michael Forney <mforney@mforney.org>
2020-06-22 23:05:44 -0700

    fspec-sync: Install executables

--- pkg/fspec-sync/gen.lua
+++ pkg/fspec-sync/gen.lua
@@ -17,7 +17,12 @@ end)
 lib('libcommon.a', {'fatal.c', 'parse.c', 'reallocarray.c'})
 
 exe('fspec-hash', {'fspec-hash.c', 'libcommon.a', '$builddir/pkg/b3sum/libblake3.a'}, {'pkg/b3sum/headers'})
+file('bin/fspec-hash', '755', '$outdir/fspec-hash')
+
 exe('fspec-sort', {'fspec-sort.c', 'libcommon.a'})
+file('bin/fspec-sort', '755', '$outdir/fspec-sort')
+
 exe('fspec-tar', {'fspec-tar.c', 'libcommon.a'})
+file('bin/fspec-tar', '755', '$outdir/fspec-tar')
 
 fetch 'git'
