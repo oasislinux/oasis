a2231574a9e9d95e02ea46317f46c6ab6bd044f9
Michael Forney <mforney@mforney.org>
2020-09-22 00:09:41 -0700

    Add sfeed_curses 0.9.2

---
+++ pkg/sfeed_curses/gen.lua
@@ -0,0 +1,16 @@
+cflags{
+	'-std=c99', '-Wall', '-Wpedantic',
+	'-D _XOPEN_SOURCE=700',
+	'-isystem $builddir/pkg/ncurses/include',
+}
+
+pkg.deps = {'pkg/ncurses/headers'}
+
+exe('sfeed_curses', {'sfeed_curses.c', '$builddir/pkg/ncurses/libncurses.a'})
+file('bin/sfeed_curses', '755', '$outdir/sfeed_curses')
+file('bin/sfeed_content', '755', '$srcdir/sfeed_content')
+file('bin/sfeed_markread', '755', '$srcdir/sfeed_markread')
+
+man{'sfeed_curses.1', 'sfeed_content.1', 'sfeed_markread.1'}
+
+fetch 'git'
