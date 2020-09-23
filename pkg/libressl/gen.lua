038ebaa0acafe1d70aba68c48a6db88598cd7340
Michael Forney <mforney@mforney.org>
2020-09-22 20:25:44 -0700

    libressl: Update to 3.2.1

--- pkg/libressl/gen.lua
+++ pkg/libressl/gen.lua
@@ -268,12 +268,11 @@ lib('libcrypto.a', [[crypto/(
 		x509name.c x509_v3.c x509_ext.c x509_att.c
 		x509type.c x509_lu.c x_all.c x509_txt.c
 		x509_trs.c by_file.c by_dir.c by_mem.c x509_vpm.c
-	)
-	x509v3/(
-		v3_bcons.c v3_bitst.c v3_conf.c v3_extku.c v3_ia5.c v3_lib.c
-		v3_prn.c v3_utl.c v3err.c v3_genn.c v3_alt.c v3_skey.c v3_akey.c v3_pku.c
-		v3_int.c v3_enum.c v3_sxnet.c v3_cpols.c v3_crld.c v3_purp.c v3_info.c
-		v3_ocsp.c v3_akeya.c v3_pmaps.c v3_pcons.c v3_ncons.c v3_pcia.c v3_pci.c
+
+		x509_bcons.c x509_bitst.c x509_conf.c x509_extku.c x509_ia5.c x509_lib.c
+		x509_prn.c x509_utl.c x509_genn.c x509_alt.c x509_skey.c x509_akey.c x509_pku.c
+		x509_int.c x509_enum.c x509_sxnet.c x509_cpols.c x509_crld.c x509_purp.c x509_info.c
+		x509_ocsp.c x509_akeya.c x509_pmaps.c x509_pcons.c x509_ncons.c x509_pcia.c x509_pci.c
 		pcy_cache.c pcy_node.c pcy_data.c pcy_map.c pcy_tree.c pcy_lib.c
 	)
 
