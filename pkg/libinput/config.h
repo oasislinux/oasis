b383981da5ae9fc0692796c23e5426dd4bae6f1e
Michael Forney <mforney@mforney.org>
2020-11-04 15:02:26 -0800

    libinput: Update to 1.16.3

--- pkg/libinput/config.h
+++ pkg/libinput/config.h
@@ -1,6 +1,6 @@
 #define HAVE_LIBWACOM 0
 #define HAVE_VERSIONSORT 1
-#define HTTP_DOC_LINK "https://wayland.freedesktop.org/libinput/doc/1.16.2/"
+#define HTTP_DOC_LINK "https://wayland.freedesktop.org/libinput/doc/1.16.3/"
 #define LIBINPUT_QUIRKS_DIR "/share/libinput"
 #define LIBINPUT_QUIRKS_OVERRIDE_FILE "/etc/libinput/local-overrides.quirks"
 #define _GNU_SOURCE 1
