6637dda00b25457be71f1c78aa805db9ab9477b5
Michael Forney <mforney@mforney.org>
2020-11-29 21:36:27 -0800

    youtube-dl: Install man page

--- pkg/youtube-dl/gen.lua
+++ pkg/youtube-dl/gen.lua
@@ -2,5 +2,6 @@ file('bin/youtube-dl', '755', '$srcdir/bin/youtube-dl')
 for f in iterlines('pylibs.txt') do
 	file('lib/python3.9/'..f, '644', '$srcdir/'..f)
 end
+man{'youtube-dl.1'}
 
 fetch 'curl'
