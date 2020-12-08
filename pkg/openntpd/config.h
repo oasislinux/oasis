ce79db6bc2504e866c89dd479224e29cf46f2df6
Michael Forney <mforney@mforney.org>
2020-12-07 23:22:26 -0800

    openntpd: Update to 6.8p1

--- pkg/openntpd/config.h
+++ pkg/openntpd/config.h
@@ -1,56 +1,72 @@
-#define __dead _Noreturn
-#define PACKAGE_NAME "OpenNTPD"
-#define PACKAGE_TARNAME "openntpd"
-#define PACKAGE_VERSION "6.2p3"
-#define PACKAGE_STRING "OpenNTPD 6.2p3"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_URL ""
-#define PACKAGE "openntpd"
-#define VERSION "6.2p3"
-#define STDC_HEADERS 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRING_H 1
-#define HAVE_MEMORY_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_DLFCN_H 1
-#define LT_OBJDIR ".libs/"
-#define SPT_TYPE SPT_REUSEARGV
+/* #undef BROKEN_SETREGID */
+/* #undef BROKEN_SETREUID */
+#define CONSTRAINT_CA "/etc/ssl/cert.pem"
+/* #undef HAVE_ADJFREQ */
+#define HAVE_ADJTIMEX 1
 #define HAVE_ARC4RANDOM 1
 #define HAVE_ARC4RANDOM_BUF 1
 #define HAVE_ARC4RANDOM_UNIFORM 1
-#define HAVE_EXPLICIT_BZERO 1
-#define HAVE_GETAUXVAL 1
-#define HAVE_TIMINGSAFE_MEMCMP 1
 #define HAVE_ASPRINTF 1
+/* #undef HAVE_ATTRIBUTE__BOUNDED__ */
+/* #undef HAVE_ATTRIBUTE__DEAD */
+/* #undef HAVE_ATTRIBUTE__PACKED */
+#define HAVE_CLOCK_GETRES 1
+#define HAVE_CLOCK_GETTIME 1
+/* #undef HAVE_CLOSEFROM */
 #define HAVE_DAEMON 1
+#define HAVE_DLFCN_H 1
+#define HAVE_ERR_H 1
+#define HAVE_EXPLICIT_BZERO 1
 #define HAVE_FREEZERO 1
+#define HAVE_GETAUXVAL 1
+/* #undef HAVE_IBUF_OPEN */
+#define HAVE_IFADDRS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIBTLS yes
 #define HAVE_MEMMEM 1
+#define HAVE_MEMORY_H 1
+/* #undef HAVE_NTP_ADJTIME */
+#define HAVE_PATHS_H 1
 #define HAVE_POLL 1
 #define HAVE_REALLOCARRAY 1
 #define HAVE_RECALLOCARRAY 1
 #define HAVE_SETGROUPS 1
+/* #undef HAVE_SETPROCTITLE */
 #define HAVE_SETREGID 1
 #define HAVE_SETRESGID 1
-#define HAVE_SETREUID 1
 #define HAVE_SETRESUID 1
+#define HAVE_SETREUID 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
 #define HAVE_STRLCAT 1
 #define HAVE_STRLCPY 1
 #define HAVE_STRTONUM 1
+/* #undef HAVE_STRUCT_SOCKADDR_IN_SIN_LEN */
 #define HAVE_SYSCONF 1
-#define HAVE_ADJTIMEX 1
-#define HAVE_CLOCK_GETTIME 1
-#define HAVE_CLOCK_GETRES 1
+/* #undef HAVE_SYS_SENSORS_H */
+#define HAVE_SYS_STAT_H 1
+/* #undef HAVE_SYS_SYSCTL_H */
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_TIMINGSAFE_BCMP 1
+#define HAVE_TIMINGSAFE_MEMCMP 1
 #define HAVE_TLS_CONFIG_SET_CA_MEM 1
-#define HAVE_LIBTLS yes
-#define HAVE___PROGNAME 1
+#define HAVE_UNISTD_H 1
 #define HAVE_VA_COPY 1
-#define HAVE_ERR_H 1
-#define HAVE_IFADDRS_H 1
-#define HAVE_PATHS_H 1
+#define HAVE___PROGNAME 1
+/* #undef HAVE___VA_COPY */
+#define LT_OBJDIR ".libs/"
 #define NTPD_USER "ntpd"
-#define CONSTRAINT_CA "/etc/ssl/cert.pem"
+#define PACKAGE "openntpd"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "OpenNTPD"
+#define PACKAGE_STRING "OpenNTPD 6.8p1"
+#define PACKAGE_TARNAME "openntpd"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "6.8p1"
+/* #undef SETEUID_BREAKS_SETUID */
+#define SPT_TYPE SPT_REUSEARGV
+#define STDC_HEADERS 1
+#define VERSION "6.8p1"
+/* #undef YYSTYPE_IS_DECLARED */
