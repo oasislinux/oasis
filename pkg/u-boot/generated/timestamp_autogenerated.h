3bcba3aa6c63bdcd1d329915615144620642144b
Michael Forney <mforney@mforney.org>
2021-01-28 15:54:48 -0800

    u-boot: Update to 2021.01

--- pkg/u-boot/generated/timestamp_autogenerated.h
+++ pkg/u-boot/generated/timestamp_autogenerated.h
@@ -1,5 +1,5 @@
-#define U_BOOT_DATE "Apr 13 2020"
-#define U_BOOT_TIME "11:02:18"
-#define U_BOOT_TZ "-0400"
-#define U_BOOT_DMI_DATE "04/13/2020"
-#define U_BOOT_BUILD_DATE 0x20200414
+#define U_BOOT_DATE "Jan 11 2021"
+#define U_BOOT_TIME "13:11:43"
+#define U_BOOT_TZ "-0500"
+#define U_BOOT_DMI_DATE "01/11/2021"
+#define U_BOOT_BUILD_DATE 0x20210111
--- pkg/u-boot/generated/version_autogenerated.h
+++ pkg/u-boot/generated/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2020.04"
+#define PLAIN_VERSION "2021.01"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CC_VERSION_STRING "cc"
 #define LD_VERSION_STRING "ld"
