d545550d4d2a92b6dfdfa3a20e4509c72e0ba9f3
Michael Forney <mforney@mforney.org>
2021-01-29 14:01:15 -0800

    e2fsprogs: Update to 1.45.7

--- pkg/e2fsprogs/gen.lua
+++ pkg/e2fsprogs/gen.lua
@@ -1,4 +1,4 @@
-set('version', '1.45.6')
+set('version', '1.45.7')
 cflags{
 	'-std=c99', '-Wall',
 	'-Wno-address-of-packed-member', '-Wno-format-truncation', '-Wno-format-overflow',
@@ -23,7 +23,7 @@ build('cat', '$outdir/config.h', {
 set('subst', {
 	'-e s,@E2FSPROGS_VERSION@,$version,',
 	'-e s,@E2FSPROGS_MONTH@,Jan,',
-	'-e s,@E2FSPROGS_YEAR@,2020,',
+	'-e s,@E2FSPROGS_YEAR@,2021,',
 	'-e s,@JDEV@,,',
 })
 
