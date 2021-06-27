db2038052c929ca06bc315ba06e7bfa837fc1418
Michael Forney <mforney@mforney.org>
2021-06-27 01:40:32 -0700

    alsa-utils: Update to 1.2.5.1

--- pkg/alsa-utils/aconfig.h
+++ pkg/alsa-utils/aconfig.h
@@ -3,11 +3,11 @@
 /* #undef ENABLE_NLS_IN_CURSES */
 #define HAVE_ALSA_MIXER_H 1
 #define HAVE_ALSA_PCM_H 1
-/* #undef HAVE_ALSA_RAWMIDI_H */
+#define HAVE_ALSA_RAWMIDI_H 1
 #define HAVE_ALSA_SEQ_H 1
 /* #undef HAVE_ALSA_TOPOLOGY_H */
 #define HAVE_ALSA_USE_CASE_H 1
-/* #undef HAVE_CFLOCALECOPYCURRENT */
+/* #undef HAVE_CFLOCALECOPYPREFERREDLANGUAGES */
 /* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
 #define HAVE_CLOCK_GETTIME 1
 /* #undef HAVE_CURSES_ESCDELAY */
@@ -23,6 +23,7 @@
 #define HAVE_LIBPTHREAD 1
 #define HAVE_LIBRT 1
 /* #undef HAVE_LIBTINYALSA */
+#define HAVE_MEMFD_CREATE 1
 /* #undef HAVE_MEMORY_H */
 /* #undef HAVE_MENU_H */
 /* #undef HAVE_PANEL_H */
@@ -39,14 +40,14 @@
 #define PACKAGE "alsa-utils"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "alsa-utils"
-#define PACKAGE_STRING "alsa-utils 1.2.4"
+#define PACKAGE_STRING "alsa-utils 1.2.5.1"
 #define PACKAGE_TARNAME "alsa-utils"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.2.4"
+#define PACKAGE_VERSION "1.2.5.1"
 #define SOUNDSDIR "/share/sounds/alsa"
 #define STDC_HEADERS 1
 #define TIME_WITH_SYS_TIME 1
-#define VERSION "1.2.4"
+#define VERSION "1.2.5.1"
 /* #undef WITH_FFADO */
 #ifndef _DARWIN_USE_64_BIT_INODE
 # define _DARWIN_USE_64_BIT_INODE 1
