995b307746340db5a569d8c0f35f3a9938838d46
Michael Forney <mforney@mforney.org>
2021-07-24 18:04:04 -0700

    Move shutdown helper to sinit package

--- pkg/sinit/gen.lua
+++ pkg/sinit/gen.lua
@@ -1,5 +1,5 @@
 cflags{
-	'-std=c99', '-Wall', '-Wextra',
+	'-std=c99', '-Wall', '-Wpedantic',
 	'-D _POSIX_C_SOURCE=200809L',
 	string.format([[-D 'PREFIX="%s"']], config.prefix),
 	'-I $dir',
@@ -8,5 +8,8 @@ cflags{
 file('bin/sinit', '755', exe('sinit', {'sinit.c'}))
 sym('bin/init', 'sinit')
 man{'sinit.8'}
+
+build('cc', '$outdir/shutdown.c.o', '$dir/shutdown.c')
+file('libexec/shutdown', '755', exe('shutdown', {'shutdown.c.o'}))
 
 fetch'git'
--- src/gen.lua
+++ src/gen.lua
@@ -5,4 +5,3 @@ cflags{
 
 file('libexec/applyperms',   '755', exe('applyperms',   {'applyperms.c'}))
 file('libexec/mergeperms',   '755', exe('mergeperms',   {'mergeperms.c'}))
-file('libexec/shutdown',     '755', exe('shutdown',     {'shutdown.c'}))
