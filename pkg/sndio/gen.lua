ed93a32292df4e93cd37bde7712db66da1b76d80
Michael Forney <mforney@mforney.org>
2021-08-31 14:45:21 -0700

    sndio: Port to tinyalsa

--- pkg/sndio/gen.lua
+++ pkg/sndio/gen.lua
@@ -4,12 +4,12 @@ cflags{
 	'-D _GNU_SOURCE',
 	'-I $srcdir/libsndio',
 	'-I $srcdir/bsd-compat',
-	'-isystem $builddir/pkg/alsa-lib/include',
+	'-isystem $builddir/pkg/tinyalsa/include',
 	'-include $dir/config.h',
 }
 
 pkg.deps = {
-	'pkg/alsa-lib/headers',
+	'pkg/tinyalsa/headers',
 }
 pkg.hdrs = copy('$outdir/include', '$srcdir/libsndio', {'sndio.h'})
 
@@ -17,31 +17,30 @@ pkg.hdrs = copy('$outdir/include', '$srcdir/libsndio', {'sndio.h'})
 local objs = objects[[
 	libsndio/(
 		debug.c aucat.c
-		mio_aucat.c
+		mio.c mio_aucat.c
 		sio_aucat.c
 		sioctl.c sioctl_aucat.c
 	)
 	bsd-compat/(issetugid.c strlcat.c strlcpy.c strtonum.c clock_gettime.c)
-	$builddir/pkg/alsa-lib/libasound.a
+	$builddir/pkg/tinyalsa/libtinyalsa.a
 ]]
 
 -- build mio.c and sio.c for sndiod with alsa support
-for _, src in ipairs{'mio.c', 'mio_alsa.c', 'sio.c', 'sio_alsa.c'} do
+for _, src in ipairs{'sio.c', 'sio_alsa.c'} do
 	build('cc', '$outdir/sndiod/'..src..'.o', {'$srcdir/libsndio/'..src, '||', '$gendir/deps'}, {
 		cflags='$cflags -D USE_ALSA',
 	})
 end
-lib('libsndio.a', {objs, 'libsndio/mio.c', 'libsndio/sio.c'})
+lib('libsndio.a', {objs, 'libsndio/sio.c'})
 
 exe('bin/sndiod', {objs, paths[[
 	sndiod/(
 		abuf.c utils.c dev.c dev_sioctl.c dsp.c file.c listen.c midi.c miofile.c
 		opt.c siofile.c sndiod.c sock.c
 
-		mio.c.o mio_alsa.c.o
 		sio.c.o sio_alsa.c.o
 	)
-	$builddir/pkg/alsa-lib/libasound.a
+	$builddir/pkg/tinyalsa/libtinyalsa.a
 ]]})
 file('bin/sndiod', '755', '$outdir/bin/sndiod')
 man{'sndiod/sndiod.8'}
