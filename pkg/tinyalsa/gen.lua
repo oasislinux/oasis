e4f1e4f50393a005ae1bb9a9ccbf02b8bd3c7670
Michael Forney <mforney@mforney.org>
2021-08-30 20:13:14 -0700

    Add tinyalsa 2.0.0

---
+++ pkg/tinyalsa/gen.lua
@@ -0,0 +1,33 @@
+cflags{
+	'-std=c99', '-Wall', '-Wpedantic', '-Wno-overflow',
+	'-D _POSIX_C_SOURCE=201112L',
+	'-I $srcdir/include',
+	'-isystem $builddir/pkg/linux-headers/include',
+}
+
+pkg.deps = {'pkg/linux-headers/headers'}
+
+pkg.hdrs = copy('$outdir/include/tinyalsa', '$srcdir/include/tinyalsa', {
+	'attributes.h',
+	'pcm.h',
+	'mixer.h',
+	'asoundlib.h',
+	'version.h',
+})
+
+lib('libtinyalsa.a', [[
+	src/(
+		limits.c
+		pcm.c
+		pcm_hw.c
+		mixer.c
+		mixer_hw.c
+	)
+]])
+
+for _, tool in ipairs{'tinycap', 'tinymix', 'tinypcminfo', 'tinyplay'} do
+	file('bin/'..tool, '755', exe(tool, {'utils/'..tool..'.c', 'libtinyalsa.a'}))
+	man{'$srcdir/utils/'..tool..'.1'}
+end
+
+fetch 'git'
