d86c0d8b7fdd0835d97d8646a084153cbfb9a181
Michael Forney <mforney@mforney.org>
2021-10-28 11:00:31 -0700

    youtube-dl: Update installation location for python 3.10

--- pkg/youtube-dl/gen.lua
+++ pkg/youtube-dl/gen.lua
@@ -1,6 +1,6 @@
 file('bin/youtube-dl', '755', '$srcdir/bin/youtube-dl')
 for f in iterlines('pylibs.txt') do
-	file('lib/python3.9/'..f, '644', '$srcdir/'..f)
+	file('lib/python3.10/'..f, '644', '$srcdir/'..f)
 end
 man{'youtube-dl.1'}
 
