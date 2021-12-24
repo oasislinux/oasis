12e1ceb79496cf23073e1f7b2e0cd9b322b77054
Michael Forney <mforney@mforney.org>
2021-12-23 16:06:21 -0800

    wayland: Update to 1.20.0

--- pkg/wayland/README.md
+++ pkg/wayland/README.md
@@ -3,10 +3,4 @@
 ## config.h
 Generated with
 
-	./configure \
-		--disable-documentation \
-		--disable-dtd-validation \
-		FFI_CFLAGS=-I/src/oasis/out/pkg/libffi/include \
-		FFI_LIBS=/src/oasis/out/pkg/libffi/libffi.a \
-		EXPAT_CFLAGS=-I/src/oasis/out/pkg/expat/include \
-		EXPAT_LIBS=/src/oasis/out/pkg/expat/libexpat.a
+	meson -Ddocumentation=false build
--- pkg/wayland/config.h
+++ pkg/wayland/config.h
@@ -1,28 +1,14 @@
-#define HAVE_ACCEPT4 1
-#define HAVE_DLFCN_H 1
-#define HAVE_INTTYPES_H 1
-/* #undef HAVE_LIBXML */
-#define HAVE_MEMFD_CREATE 1
-#define HAVE_MKOSTEMP 1
-#define HAVE_POSIX_FALLOCATE 1
-#define HAVE_PRCTL 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDIO_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_STRING_H 1
-#define HAVE_STRNDUP 1
-#define HAVE_SYS_PRCTL_H 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_UNISTD_H 1
-#define LT_OBJDIR ".libs/"
+#define HAVE_ACCEPT4
+#define HAVE_BROKEN_MSG_CMSG_CLOEXEC 0
+#define HAVE_MEMFD_CREATE
+#define HAVE_MKOSTEMP
+#define HAVE_MREMAP
+#define HAVE_POSIX_FALLOCATE
+#define HAVE_PRCTL
+#define HAVE_STRNDUP
+#define HAVE_SYS_PRCTL_H
+#undef HAVE_SYS_PROCCTL_H
+#undef HAVE_SYS_UCRED_H
+#define HAVE_XUCRED_CR_PID 0
 #define PACKAGE "wayland"
-#define PACKAGE_BUGREPORT "https://gitlab.freedesktop.org/wayland/wayland/issues/"
-#define PACKAGE_NAME "wayland"
-#define PACKAGE_STRING "wayland 1.19.0"
-#define PACKAGE_TARNAME "wayland"
-#define PACKAGE_URL "https://wayland.freedesktop.org/"
-#define PACKAGE_VERSION "1.19.0"
-#define STDC_HEADERS 1
-#define VERSION "1.19.0"
+#define PACKAGE_VERSION "1.20.0"
