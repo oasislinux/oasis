096bc8161c480e13d55b43c11c399d00d326a4d7
Michael Forney <mforney@mforney.org>
2023-02-17 21:48:37 -0800

    hotplugd: Add ata_id

--- pkg/hotplugd/gen.lua
+++ pkg/hotplugd/gen.lua
@@ -6,7 +6,8 @@ cflags{
 
 pkg.deps = {'pkg/linux-headers/headers'}
 
-file('bin/hotplugd',             '755', exe('hotplugd',         {'hotplugd.c'}))
-file('libexec/hotplugd-trigger', '755', exe('hotplugd-trigger', {'hotplugd-trigger.c'}))
+file('bin/hotplugd',             '755', exe('hotplugd', {'hotplugd.c'}))
+file('libexec/hotplugd/ata_id',  '755', exe('ata_id',   {'ata_id.c'}))
+file('libexec/hotplugd/trigger', '755', exe('trigger',  {'trigger.c'}))
 
 fetch 'git'
