64f79037ce92852e07a64bd24752f30c063bf4b9
Michael Forney <mforney@mforney.org>
2023-12-03 11:55:22 -0800

    lua: Update to 5.4.6

--- pkg/lua/gen.lua
+++ pkg/lua/gen.lua
@@ -2,6 +2,7 @@ cflags{
 	'-std=c99', '-Wall', '-Wextra', '-Wpedantic', '-Wno-implicit-fallthrough',
 	'-D _DEFAULT_SOURCE',
 	'-D LUA_USE_POSIX',
+	'-D LUA_USE_JUMPTABLE=0',
 }
 
 pkg.hdrs = {
@@ -13,8 +14,8 @@ lib('liblua.a', [[src/(
 	lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c
 	lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c
 	ltm.c lundump.c lvm.c lzio.c
-	lauxlib.c lbaselib.c lbitlib.c lcorolib.c ldblib.c liolib.c
-	lmathlib.c loslib.c lstrlib.c ltablib.c loadlib.c linit.c
+	lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c
+	loslib.c lstrlib.c ltablib.c lutf8lib.c loadlib.c linit.c
 )]])
 file('lib/liblua.a', '644', '$outdir/liblua.a')
 
