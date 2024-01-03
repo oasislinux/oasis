c372148401bc47393505840a72d4edfaaf791c4f
hovercats <hovercatswithlasereyes@protonmail.com>
2024-01-03 15:33:36 +0100

    file: 5.45

--- pkg/file/gen.lua
+++ pkg/file/gen.lua
@@ -1,4 +1,4 @@
-local version = '5.42'
+local version = '5.45'
 cflags{
 	'-Wall',
 	'-Wno-maybe-uninitialized',  -- false positive in src/readelf.c
@@ -27,12 +27,10 @@ pkg.hdrs = {'$outdir/include/magic.h'}
 pkg.deps = {'$gendir/headers', 'pkg/zlib/headers'}
 
 lib('libmagic.a', [[src/(
-	buffer.c magic.c apprentice.c softmagic.c ascmagic.c
-	encoding.c compress.c is_csv.c is_json.c is_tar.c readelf.c print.c
-	fsmagic.c funcs.c apptype.c der.c
-	cdf.c cdf_time.c readcdf.c
-
-	fmtcheck.c
+	buffer.c magic.c apprentice.c softmagic.c
+	ascmagic.c encoding.c compress.c is_csv.c is_json.c
+	is_simh.c is_tar.c readelf.c print.c fsmagic.c funcs.c
+	apptype.c der.c cdf.c cdf_time.c readcdf.c fmtcheck.c
 )]])
 
 exe('file', {'src/file.c', 'src/seccomp.c', 'libmagic.a', '$builddir/pkg/zlib/libz.a'})
