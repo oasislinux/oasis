af8ccff1e7758154640419ddc620a4e54c570a10
Randy Palamar <randy@rnpnr.xyz>
2024-03-09 16:11:52 -0700

    libpng: bump to 1.6.43

--- pkg/libpng/config.h
+++ pkg/libpng/config.h
@@ -3,9 +3,9 @@
 #define HAVE_INTTYPES_H 1
 /* #undef HAVE_LIBM */
 #define HAVE_LIBZ 1
-#define HAVE_MEMORY_H 1
 #define HAVE_POW 1
 #define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
@@ -16,14 +16,18 @@
 #define PACKAGE "libpng"
 #define PACKAGE_BUGREPORT "png-mng-implement@lists.sourceforge.net"
 #define PACKAGE_NAME "libpng"
-#define PACKAGE_STRING "libpng 1.6.37"
+#define PACKAGE_STRING "libpng 1.6.43"
 #define PACKAGE_TARNAME "libpng"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.6.37"
+#define PACKAGE_VERSION "1.6.43"
 /* #undef PNG_ARM_NEON_API_SUPPORTED */
 /* #undef PNG_ARM_NEON_CHECK_SUPPORTED */
 /* #undef PNG_ARM_NEON_OPT */
 /* #undef PNG_INTEL_SSE_OPT */
+/* #undef PNG_LOONGARCH_LSX_OPT */
+/* #undef PNG_MIPS_MMI_API_SUPPORTED */
+/* #undef PNG_MIPS_MMI_CHECK_SUPPORTED */
+/* #undef PNG_MIPS_MMI_OPT */
 /* #undef PNG_MIPS_MSA_API_SUPPORTED */
 /* #undef PNG_MIPS_MSA_CHECK_SUPPORTED */
 /* #undef PNG_MIPS_MSA_OPT */
@@ -32,11 +36,9 @@
 /* #undef PNG_POWERPC_VSX_OPT */
 #define STDC_HEADERS 1
 /* #undef TM_IN_SYS_TIME */
-#define VERSION "1.6.37"
-/* #undef const */
-#define restrict __restrict
-#if defined __SUNPRO_CC && !defined __RESTRICT
+#define VERSION "1.6.43"
+#define restrict __restrict__
+#if defined __SUNPRO_CC && !defined __RESTRICT && !defined __restrict__
 # define _Restrict
 # define __restrict__
 #endif
-/* #undef size_t */
