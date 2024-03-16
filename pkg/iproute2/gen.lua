b2f81fb67cfd273ed556da99d8489dfd4fe1e9c0
Michael Forney <mforney@mforney.org>
2024-03-16 00:17:10 -0700

    iproute2: Update to 6.8.0

--- pkg/iproute2/gen.lua
+++ pkg/iproute2/gen.lua
@@ -1,12 +1,16 @@
 cflags{
 	'-Wall', '-Wformat=2', '-Wmissing-declarations', '-Wmissing-prototypes',
 	'-Wold-style-definition', '-Wstrict-prototypes', '-Wno-maybe-uninitialized',
-	'-Wno-format-truncation',
+	'-Wno-format-truncation', '-Wno-stringop-overflow',
 	'-Wno-cpp',  -- we don't have libmnl
 	'-D _GNU_SOURCE',
 	'-D HAVE_SETNS',
 	'-D HAVE_HANDLE_AT',
+	[[-D 'CONF_USR_DIR="/share/iproute2"']],
+	[[-D 'CONF_ETC_DIR="/etc/iproute2"']],
 	[[-D 'NETNS_RUN_DIR="/run/netns"']],
+	[[-D 'NETNS_RUN_DIR="/etc/netns"']],
+	'-D CONF_COLOR:=COLOR_OPT_AUTO',
 	'-I $srcdir/include',
 	'-I $srcdir/include/uapi',
 	'-I $srcdir/misc',
@@ -22,7 +26,7 @@ lib('libnetlink.a', [[lib/(
 
 	utils.c utils_math.c rt_names.c ll_map.c ll_types.c ll_proto.c ll_addr.c
 	inet_proto.c namespace.c json_writer.c json_print.c json_print_math.c
-	names.c color.c bpf_legacy.c bpf_glue.c exec.c fs.c cg_map.c
+	names.c color.c bpf_legacy.c bpf_glue.c exec.c fs.c cg_map.c ppp_proto.c
 	ax25_ntop.c
 	rose_ntop.c
 	mpls_ntop.c mpls_pton.c
@@ -39,16 +43,17 @@ exe('bin/ip', [[ip/(
 	iplink_macvlan.c ipl2tp.c link_vti.c link_vti6.c link_xfrm.c
 	iplink_vxlan.c tcp_metrics.c iplink_ipoib.c ipnetconf.c link_ip6tnl.c
 	link_iptnl.c link_gre6.c iplink_bond.c iplink_bond_slave.c iplink_hsr.c
-	iplink_bridge.c iplink_bridge_slave.c ipfou.c iplink_ipvlan.c
+	iplink_bridge.c iplink_bridge_slave.c iplink_dsa.c ipfou.c iplink_ipvlan.c
 	iplink_geneve.c iplink_vrf.c iproute_lwtunnel.c ipmacsec.c ipila.c
 	ipvrf.c iplink_xstats.c ipseg6.c iplink_netdevsim.c iplink_rmnet.c
 	ipnexthop.c ipmptcp.c iplink_bareudp.c iplink_wwan.c ipioam6.c
-	iplink_amt.c
+	iplink_amt.c iplink_batadv.c iplink_gtp.c iplink_virt_wifi.c
+	iplink_netkit.c ipstats.c
 ) libnetlink.a]])
 file('bin/ip', '755', '$outdir/bin/ip')
 
 exe('bin/bridge', [[
-	bridge/(bridge.c fdb.c monitor.c link.c mdb.c vlan.c)
+	bridge/(bridge.c fdb.c monitor.c link.c mdb.c vlan.c vni.c)
 	libnetlink.a
 ]])
 file('bin/bridge', '755', '$outdir/bin/bridge')
@@ -68,9 +73,10 @@ for _, name in ipairs{'ip-address', 'ip-link', 'ip-netns', 'ip-route'} do
 	local out = '$outdir/'..name..'.8'
 	build('sed', out, '$srcdir/man/man8/'..name..'.8.in', {
 		expr={
-			'-e s,@SYSCONFDIR@,/etc,g',
 			'-e s,@NETNS_ETC_DIR@,/etc/netns,g',
 			'-e s,@NETNS_RUN_DIR@,/run/netns,g',
+			'-e s,@SYSCONF_ETC_DIR@,/etc/iproute2,g',
+			'-e s,@SYSCONF_USR_DIR@,/share/iproute2,g',
 		},
 	})
 	man{out}
