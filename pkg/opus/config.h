a1c751df36148ef4bec4e45a2807f215733d078a
hovercats <hovercatswithlasereyes@protonmail.com>
2025-02-09 08:21:23 +0100

    opus: 1.5.2

--- pkg/opus/config.h
+++ pkg/opus/config.h
@@ -1,6 +1,79 @@
-#define HAVE_LRINTF
-#if __STDC_NO_VLA__
-#define USE_ALLOCA
-#else
-#define VAR_ARRAYS
+/* #undef CPU_INFO_BY_ASM */
+/* #undef CPU_INFO_BY_C */
+/* #undef CUSTOM_MODES */
+#define DISABLE_DEBUG_FLOAT 1
+/* #undef DISABLE_DOT_PROD */
+/* #undef DISABLE_FLOAT_API */
+/* #undef DISABLE_UPDATE_DRAFT */
+/* #undef ENABLE_ASSERTIONS */
+/* #undef ENABLE_DEEP_PLC */
+/* #undef ENABLE_DRED */
+/* #undef ENABLE_HARDENING */
+/* #undef ENABLE_LOSSGEN */
+/* #undef ENABLE_OSCE */
+/* #undef ENABLE_OSCE_TRAINING_DATA */
+/* #undef FIXED_DEBUG */
+/* #undef FIXED_POINT */
+#define FLOAT_APPROX 1
+/* #undef FUZZING */
+/* #undef HAVE_ALLOCA_H */
+/* #undef HAVE_ARM_NE10 */
+#define HAVE_DLFCN_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LRINT 1
+#define HAVE_LRINTF 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+/* #undef HAVE___MALLOC_HOOK */
+#define LT_OBJDIR ".libs/"
+/* #undef OPUS_ARM_ASM */
+/* #undef OPUS_ARM_INLINE_ASM */
+/* #undef OPUS_ARM_INLINE_EDSP */
+/* #undef OPUS_ARM_INLINE_MEDIA */
+/* #undef OPUS_ARM_INLINE_NEON */
+/* #undef OPUS_ARM_MAY_HAVE_DOTPROD */
+/* #undef OPUS_ARM_MAY_HAVE_EDSP */
+/* #undef OPUS_ARM_MAY_HAVE_MEDIA */
+/* #undef OPUS_ARM_MAY_HAVE_NEON */
+/* #undef OPUS_ARM_MAY_HAVE_NEON_INTR */
+/* #undef OPUS_ARM_PRESUME_AARCH64_NEON_INTR */
+/* #undef OPUS_ARM_PRESUME_DOTPROD */
+/* #undef OPUS_ARM_PRESUME_EDSP */
+/* #undef OPUS_ARM_PRESUME_MEDIA */
+/* #undef OPUS_ARM_PRESUME_NEON */
+/* #undef OPUS_ARM_PRESUME_NEON_INTR */
+#define OPUS_BUILD /**/
+/* #undef OPUS_CHECK_ASM */
+/* #undef OPUS_HAVE_RTCD */
+/* #undef OPUS_X86_MAY_HAVE_AVX2 */
+/* #undef OPUS_X86_MAY_HAVE_SSE */
+/* #undef OPUS_X86_MAY_HAVE_SSE2 */
+/* #undef OPUS_X86_MAY_HAVE_SSE4_1 */
+/* #undef OPUS_X86_PRESUME_AVX2 */
+/* #undef OPUS_X86_PRESUME_SSE */
+/* #undef OPUS_X86_PRESUME_SSE2 */
+/* #undef OPUS_X86_PRESUME_SSE4_1 */
+#define PACKAGE_BUGREPORT "opus@xiph.org"
+#define PACKAGE_NAME "opus"
+#define PACKAGE_STRING "opus 1.5.2"
+#define PACKAGE_TARNAME "opus"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.5.2"
+#define STDC_HEADERS 1
+/* #undef USE_ALLOCA */
+#define VAR_ARRAYS 1
+/* #undef const */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+#define restrict __restrict
+#if defined __SUNPRO_CC && !defined __RESTRICT
+# define _Restrict
+# define __restrict__
 #endif
