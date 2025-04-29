06624ed7867c902e0ca44b5ca381e9d4dc157e14
Michael Forney <mforney@mforney.org>
2025-04-29 16:35:44 -0700

    openbsd: Update to 7.7

--- pkg/openbsd/include/string.h
+++ pkg/openbsd/include/string.h
@@ -1,5 +1,6 @@
+#define __NEED_mode_t
 #include_next <string.h>
 char *strcasestr(const char *, const char *);
-void strmode(int, char *);
+void strmode(mode_t, char *);
 int timingsafe_memcmp(const void *, const void *, size_t);
 int timingsafe_bcmp(const void *, const void *, size_t);
