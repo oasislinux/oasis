f3381bb880a2379d9c3763d3237d60bfd723abc7
Michael Forney <mforney@mforney.org>
2025-04-28 23:26:53 -0700

    acpid: Update to 2.0.34

--- pkg/acpid/config.h
+++ pkg/acpid/config.h
@@ -12,8 +12,8 @@
 /* #undef HAVE_ISFDTYPE */
 #define HAVE_MALLOC 1
 #define HAVE_MALLOC_H 1
-#define HAVE_MEMORY_H 1
 #define HAVE_MEMSET 1
+/* #undef HAVE_MINIX_CONFIG_H */
 #define HAVE_NETINET_IN_H 1
 #define HAVE_OPENAT 1
 #define HAVE_REALLOC 1
@@ -21,6 +21,7 @@
 #define HAVE_SELECT 1
 #define HAVE_SOCKET 1
 #define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRCASECMP 1
 #define HAVE_STRDUP 1
@@ -37,40 +38,80 @@
 #define HAVE_UNISTD_H 1
 #define HAVE_VFORK 1
 /* #undef HAVE_VFORK_H */
+#define HAVE_WCHAR_H 1
 #define HAVE_WORKING_FORK 1
 #define HAVE_WORKING_VFORK 1
 #define PACKAGE "acpid"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "acpid"
-#define PACKAGE_STRING "acpid 2.0.33"
+#define PACKAGE_STRING "acpid 2.0.34"
 #define PACKAGE_TARNAME "acpid"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "2.0.33"
+#define PACKAGE_VERSION "2.0.34"
 #define STDC_HEADERS 1
 #ifndef _ALL_SOURCE
 # define _ALL_SOURCE 1
 #endif
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE 1
-#endif
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
-#endif
-#ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
+#ifndef _DARWIN_C_SOURCE
+# define _DARWIN_C_SOURCE 1
 #endif
 #ifndef __EXTENSIONS__
 # define __EXTENSIONS__ 1
 #endif
-#define VERSION "2.0.33"
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
 #endif
+#ifndef _HPUX_ALT_XOPEN_SOCKET_API
+# define _HPUX_ALT_XOPEN_SOCKET_API 1
+#endif
+#ifndef _MINIX
+/* # undef _MINIX */
+#endif
+#ifndef _NETBSD_SOURCE
+# define _NETBSD_SOURCE 1
+#endif
+#ifndef _OPENBSD_SOURCE
+# define _OPENBSD_SOURCE 1
+#endif
+#ifndef _POSIX_SOURCE
+/* # undef _POSIX_SOURCE */
+#endif
+#ifndef _POSIX_1_SOURCE
+/* # undef _POSIX_1_SOURCE */
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_ATTRIBS_EXT__
+# define __STDC_WANT_IEC_60559_ATTRIBS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_BFP_EXT__
+# define __STDC_WANT_IEC_60559_BFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_DFP_EXT__
+# define __STDC_WANT_IEC_60559_DFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_FUNCS_EXT__
+# define __STDC_WANT_IEC_60559_FUNCS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_TYPES_EXT__
+# define __STDC_WANT_IEC_60559_TYPES_EXT__ 1
+#endif
+#ifndef __STDC_WANT_LIB_EXT2__
+# define __STDC_WANT_LIB_EXT2__ 1
+#endif
+#ifndef __STDC_WANT_MATH_SPEC_FUNCS__
+# define __STDC_WANT_MATH_SPEC_FUNCS__ 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef _XOPEN_SOURCE
+/* # undef _XOPEN_SOURCE */
+#endif
+#define VERSION "2.0.34"
 /* #undef _FILE_OFFSET_BITS */
 /* #undef _LARGE_FILES */
-/* #undef _MINIX */
-/* #undef _POSIX_1_SOURCE */
-/* #undef _POSIX_SOURCE */
 /* #undef mode_t */
 /* #undef pid_t */
 /* #undef size_t */
