bee63f757b1a31a0d76037bbce3d1c4d9cfae4c2
Michael Forney <mforney@mforney.org>
2026-03-03 15:44:16 -0800

    bc: Update to 1.08.2

--- pkg/bc/config.h
+++ pkg/bc/config.h
@@ -1,18 +1,22 @@
-#define BC_COPYRIGHT "Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006, 2008, 2012-2017 Free Software Foundation, Inc."
-#define DC_COPYRIGHT "Copyright 1994, 1997, 1998, 2000, 2001, 2003-2006, 2008, 2010, 2012-2017 Free Software Foundation, Inc."
-#define DC_VERSION "1.4.1"
+#define BC_COPYRIGHT "Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006, 2008, 2012-2018, 2024 Free Software Foundation, Inc."
+#define DC_COPYRIGHT "Copyright 1994, 1997, 1998, 2000, 2001, 2003-2006, 2008, 2010, 2012-2018, 2022, 2024 Free Software Foundation, Inc."
+#define DC_VERSION "1.5.2"
+#define ENABLE_DC_BANG_SHELL 1
 /* #undef HAVE_DOPRNT */
 #define HAVE_ERRNO_H 1
 #define HAVE_FSTAT 1
+#define HAVE_GETOPT_H 1
+#define HAVE_GETOPT_LONG 1
 #define HAVE_INTTYPES_H 1
 #define HAVE_ISGRAPH 1
 /* #undef HAVE_LIB_H */
 #define HAVE_LIMITS_H 1
-#define HAVE_MEMORY_H 1
+/* #undef HAVE_MINIX_CONFIG_H */
 #define HAVE_SETVBUF 1
 #define HAVE_STDARG_H 1
 #define HAVE_STDDEF_H 1
 #define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_STRING_H 1
@@ -21,36 +25,83 @@
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_UNISTD_H 1
 #define HAVE_VPRINTF 1
+#define HAVE_WCHAR_H 1
+#define HAVE___ATTRIBUTE__ 1
 /* #undef LIBEDIT */
 #define PACKAGE "bc"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME "bc"
-#define PACKAGE_STRING "bc 1.07.1"
+#define PACKAGE_STRING "bc 1.08.2"
 #define PACKAGE_TARNAME "bc"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.07.1"
+#define PACKAGE_VERSION "1.08.2"
 /* #undef READLINE */
 #define STDC_HEADERS 1
 #ifndef _ALL_SOURCE
 # define _ALL_SOURCE 1
 #endif
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE 1
-#endif
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
-#endif
-#ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
+#ifndef _DARWIN_C_SOURCE
+# define _DARWIN_C_SOURCE 1
 #endif
 #ifndef __EXTENSIONS__
 # define __EXTENSIONS__ 1
 #endif
-#define VERSION "1.07.1"
-/* #undef YYTEXT_POINTER */
-/* #undef _MINIX */
-/* #undef _POSIX_1_SOURCE */
-/* #undef _POSIX_SOURCE */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+#ifndef _HPUX_ALT_XOPEN_SOCKET_API
+# define _HPUX_ALT_XOPEN_SOCKET_API 1
+#endif
+#ifndef _MINIX
+/* # undef _MINIX */
+#endif
+#ifndef _NETBSD_SOURCE
+# define _NETBSD_SOURCE 1
+#endif
+#ifndef _OPENBSD_SOURCE
+# define _OPENBSD_SOURCE 1
+#endif
+#ifndef _POSIX_SOURCE
+/* # undef _POSIX_SOURCE */
+#endif
+#ifndef _POSIX_1_SOURCE
+/* # undef _POSIX_1_SOURCE */
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_ATTRIBS_EXT__
+# define __STDC_WANT_IEC_60559_ATTRIBS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_BFP_EXT__
+# define __STDC_WANT_IEC_60559_BFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_DFP_EXT__
+# define __STDC_WANT_IEC_60559_DFP_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_EXT__
+# define __STDC_WANT_IEC_60559_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_FUNCS_EXT__
+# define __STDC_WANT_IEC_60559_FUNCS_EXT__ 1
+#endif
+#ifndef __STDC_WANT_IEC_60559_TYPES_EXT__
+# define __STDC_WANT_IEC_60559_TYPES_EXT__ 1
+#endif
+#ifndef __STDC_WANT_LIB_EXT2__
+# define __STDC_WANT_LIB_EXT2__ 1
+#endif
+#ifndef __STDC_WANT_MATH_SPEC_FUNCS__
+# define __STDC_WANT_MATH_SPEC_FUNCS__ 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+#ifndef _XOPEN_SOURCE
+/* # undef _XOPEN_SOURCE */
+#endif
+#define VERSION "1.08.2"
+#define YYTEXT_POINTER 1
 /* #undef const */
 /* #undef ptrdiff_t */
 /* #undef size_t */
