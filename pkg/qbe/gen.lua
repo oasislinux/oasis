0f549e4566042aaf35aef2e6573256fef8c0b9e2
Michael Forney <mforney@mforney.org>
2026-03-04 14:17:17 -0800

    qbe: Update to latest git

--- pkg/qbe/gen.lua
+++ pkg/qbe/gen.lua
@@ -6,8 +6,9 @@ cflags{
 
 exe('qbe', [[
 	main.c util.c parse.c abi.c cfg.c mem.c ssa.c alias.c load.c
-	copy.c fold.c gvn.c gcm.c simpl.c live.c spill.c rega.c emit.c
-	amd64/(targ.c sysv.c isel.c emit.c)
+	copy.c fold.c gvn.c gcm.c simpl.c ifopt.c live.c spill.c rega.c
+	emit.c
+	amd64/(targ.c sysv.c isel.c emit.c winabi.c)
 	arm64/(targ.c abi.c isel.c emit.c)
 	rv64/(targ.c abi.c isel.c emit.c)
 ]])
