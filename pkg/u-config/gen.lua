d5afb42018b74e9469b581fa5b90632457f8d5b5
hovercats <hovercatswithlasereyes@protonmail.com>
2026-04-16 09:45:55 +0200

    u-config: add

---
+++ pkg/u-config/gen.lua
@@ -0,0 +1,14 @@
+cflags{
+	'-std=c99',
+	'-Wall', '-Wpedantic',
+}
+
+exe('u-config', {'main_posix.c'})
+
+file('bin/u-config', '755', '$outdir/u-config')
+man{'u-config.1'}
+
+sym('bin/pkg-config', 'u-config')
+sym('share/man/man1/pkg-config.1.gz', 'u-config.1.gz')
+
+fetch 'git'
