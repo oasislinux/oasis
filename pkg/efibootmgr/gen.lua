8f2fe6baaa3a5786c7b95ec638a26e97359d69c1
Michael Forney <mforney@mforney.org>
2026-05-16 23:58:38 -0700

    efibootmgr: Update to 18

--- pkg/efibootmgr/gen.lua
+++ pkg/efibootmgr/gen.lua
@@ -1,6 +1,6 @@
 cflags{
 	'-std=c99', '-Wall',
-	[[-D 'EFIBOOTMGR_VERSION="17"']],
+	[[-D 'EFIBOOTMGR_VERSION="18"']],
 	[[-D 'DEFAULT_LOADER="/linux"']],
 	'-I $srcdir/src/include',
 	'-isystem $builddir/pkg/efivar/include',
@@ -13,11 +13,15 @@ pkg.deps = {
 	'pkg/linux-headers/headers',
 }
 
+build('sed', '$outdir/efibootmgr.8', '$srcdir/src/efibootmgr.8.in', {
+	expr='s,@@DEFAULT_LOADER@@,/linux,g',
+})
+
 exe('efibootmgr', [[
-	src/(efibootmgr.c efi.c unparse_path.c)
+	src/(efibootmgr.c efi.c parse_loader_data.c)
 	$builddir/pkg/efivar/(libefiboot.a libefivar.a)
 ]])
 file('bin/efibootmgr', '755', '$outdir/efibootmgr')
-man{'src/efibootmgr.8'}
+man{'$outdir/efibootmgr.8'}
 
 fetch 'git'
