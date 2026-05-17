d238efe975be548a63bebbe308ebe3c0a9de7671
Michael Forney <mforney@mforney.org>
2026-05-16 23:52:00 -0700

    efivar: Update to 39

--- pkg/efivar/gen.lua
+++ pkg/efivar/gen.lua
@@ -1,12 +1,10 @@
-cc('src/guids.S', {'$outdir/guids.bin', '$outdir/names.bin'}, {
-	cflags='$cflags -Wa,-I,$outdir',
-})
-
 cflags{
-	'-Wall', '-Wextra', '-Wno-address-of-packed-member',
+	'-Wall', '-Wextra',
 	'-D _GNU_SOURCE',
+	'-D LIBEFIVAR_VERSION=39',
 	'-include $dir/compat.h',
 	'-I $outdir/include',
+	'-I $srcdir/src',
 	'-I $srcdir/src/include',
 	'-isystem $builddir/pkg/linux-headers/include',
 }
@@ -19,14 +17,12 @@ sub('tools.ninja', function()
 		'-D EFIVAR_BUILD_ENVIRONMENT',
 		'-I $srcdir/src/include',
 	}
-	build('cc', '$outdir/host-guid.c.o', '$srcdir/src/guid.c')
-	exe('makeguids', {'src/makeguids.c', 'host-guid.c.o'}, nil, {ldlibs='-ldl'})
+	build('cc', '$outdir/host-util.c.o', '$srcdir/src/util.c')
+	exe('makeguids', {'src/makeguids.c', 'host-util.c.o'})
 end)
 
 rule('makeguids', '$outdir/makeguids $in $out')
 build('makeguids', {
-	'$outdir/guids.bin',
-	'$outdir/names.bin',
 	'$outdir/guid-symbols.c',
 	'$outdir/include/efivar/efivar-guids.h',
 }, {'$srcdir/src/guids.txt', '|', '$outdir/makeguids'})
@@ -38,6 +34,8 @@ pkg.hdrs = {
 		'efiboot-loadopt.h',
 		'efivar.h',
 		'efivar-dp.h',
+		'efivar-time.h',
+		'efivar-types.h',
 	}),
 	'$outdir/include/efivar/efivar-guids.h',
 }
@@ -46,17 +44,19 @@ pkg.deps = {
 	'pkg/linux-headers/headers',
 }
 
+cc('src/crc32.c')
+
 lib('libefiboot.a', [[
 	src/(
-		crc32.c creator.c disk.c gpt.c loadopt.c path-helpers.c linux.c
-		linux-(acpi acpi-root ata emmc i2o md nvme pci pci-root pmem sas sata scsi soc-root virtblk).c
+		crc32.c.o creator.c disk.c gpt.c loadopt.c path-helpers.c linux.c
+		linux-(acpi acpi-root ata emmc i2o md nvme pci pci-root pmem sas sata scsi soc-root virtblk virtual-root).c
 	)
 ]])
 lib('libefivar.a', [[
 	src/(
-		dp.c dp-acpi.c dp-hw.c dp-media.c dp-message.c
-		efivarfs.c error.c export.c guid.c guids.S.o
-		lib.c vars.c
+		crc32.c.o dp.c dp-acpi.c dp-hw.c dp-media.c dp-message.c
+		efivarfs.c error.c export.c guid.c
+		lib.c vars.c time.c
 	)
 	$outdir/guid-symbols.c
 ]])
