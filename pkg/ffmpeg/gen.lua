6ff9a5073c5dbb36f84f711883c8ab5892997cd7
Samuel Marín Soto <samarin@estudiantec.cr>
2026-05-26 00:26:40 -0600

    ffmpeg: build on riscv64
    Gated x86-only features in config.h, and used paths[[]] to select source files for each architecture

--- pkg/ffmpeg/gen.lua
+++ pkg/ffmpeg/gen.lua
@@ -1,3 +1,12 @@
+local targets = {
+	aarch64={dir='aarch64'},
+	riscv64={dir='riscv'},
+	x86_64={dir='x86'},
+}
+local arch = config.target.platform:match('[^-]*')
+local targ = targets[arch]
+if not targ then return end
+
 local arch = 'x86'
 cflags{
 	'-Wno-deprecated-declarations',
@@ -13,8 +22,8 @@ cflags{
 }
 nasmflags{
 	'-i $srcdir/',
-	'-i $srcdir/libavcodec/'..arch..'/',
-	'-i $srcdir/libavutil/'..arch..'/',
+	'-i $srcdir/libavcodec/'..targ.dir..'/',
+	'-i $srcdir/libavutil/'..targ.dir..'/',
 	'-f elf64',
 	'-P $outdir/config.asm',
 }
@@ -113,15 +122,18 @@ for line in iterlines('sources.txt', 1) do
 	local i = line:find(' ', 1, true)
 	if i and options[line:sub(1, i - 1)] then
 		for src, lib, dir in line:gmatch('(([%w_-]+)/?([%w_-]*)[%w_/-]*/[%w_.-]+)', i + 1) do
-			if not archs[dir] or dir == arch then
+			if not archs[dir] or dir == targ.dir then
 				sources[lib][src] = true
 			end
 		end
 	end
 end
 -- combination option in libavutil/x86/Makefile
+-- probe false positive on riscv64
+if arch == 'x86_64' then
 if not options.HAVE_MMX_INLINE and options.HAVE_MMX_EXTERNAL and not options.HAVE_MM_EMPTY then
 	sources.libavutil['libavutil/x86/emms.asm'] = true
+end
 end
 for lib, srcs in pairs(sources) do
 	sources[lib] = table.keys(srcs)
@@ -173,44 +185,47 @@ end
 sub('lib.ninja', function()
 cflags{'-D HAVE_AV_CONFIG_H'}
 lib('libavcodec.a', {
-	expand{'libavcodec/', {
-		'ac3_parser.c',
-		'adts_parser.c',
-		'allcodecs.c',
-		'avcodec.c',
-		'avdct.c',
-		'avfft.c',
-		'avpacket.c',
-		'bitstream.c',
-		'bitstream_filters.c',
-		'bsf.c',
-		'codec_desc.c',
-		'codec_par.c',
-		'd3d11va.c',
-		'decode.c',
-		'dirac.c',
-		'dv_profile.c',
-		'encode.c',
-		'get_buffer.c',
-		'imgconvert.c',
-		'jni.c',
-		'mathtables.c',
-		'mediacodec.c',
-		'mpeg12framerate.c',
-		'options.c',
-		'parser.c',
-		'parsers.c',
-		'profiles.c',
-		'qsv_api.c',
-		'raw.c',
-		'refstruct.c',
-		'utils.c',
-		'version.c',
-		'vlc.c',
-		'vorbis_parser.c',
-		'xiph.c',
-		'x86/constants.c',
-	}},
+	paths[[
+		libavcodec/(
+			ac3_parser.c
+			adts_parser.c
+			allcodecs.c
+			avcodec.c
+			avdct.c
+			avfft.c
+			avpacket.c
+			bitstream.c
+			bitstream_filters.c
+			bsf.c
+			codec_desc.c
+			codec_par.c
+			d3d11va.c
+			decode.c
+			dirac.c
+			dv_profile.c
+			encode.c
+			get_buffer.c
+			imgconvert.c
+			jni.c
+			mathtables.c
+			mediacodec.c
+			mpeg12framerate.c
+			options.c
+			parser.c
+			parsers.c
+			profiles.c
+			qsv_api.c
+			raw.c
+			refstruct.c
+			utils.c
+			version.c
+			vlc.c
+			vorbis_parser.c
+			xiph.c
+
+			@x86_64 x86/constants.c
+		)
+	]],
 	sources.libavcodec,
 	'libavutil.a',
 })
@@ -285,160 +300,218 @@ lib('libavformat.a', {
 })
 
 lib('libavutil.a', {
-	expand{'libavutil/', {
-		'adler32.c',
-		'aes.c',
-		'aes_ctr.c',
-		'ambient_viewing_environment.c',
-		'audio_fifo.c',
-		'avstring.c',
-		'avsscanf.c',
-		'base64.c',
-		'blowfish.c',
-		'bprint.c',
-		'buffer.c',
-		'cast5.c',
-		'camellia.c',
-		'channel_layout.c',
-		'cpu.c',
-		'crc.c',
-		'csp.c',
-		'des.c',
-		'detection_bbox.c',
-		'dict.c',
-		'display.c',
-		'dovi_meta.c',
-		'downmix_info.c',
-		'encryption_info.c',
-		'error.c',
-		'eval.c',
-		'executor.c',
-		'fifo.c',
-		'file.c',
-		'file_open.c',
-		'float_dsp.c',
-		'fixed_dsp.c',
-		'frame.c',
-		'hash.c',
-		'hdr_dynamic_metadata.c',
-		'hdr_dynamic_vivid_metadata.c',
-		'hmac.c',
-		'hwcontext.c',
-		'iamf.c',
-		'imgutils.c',
-		'integer.c',
-		'intmath.c',
-		'lfg.c',
-		'lls.c',
-		'log.c',
-		'log2_tab.c',
-		'lzo.c',
-		'mathematics.c',
-		'mastering_display_metadata.c',
-		'md5.c',
-		'mem.c',
-		'murmur3.c',
-		'opt.c',
-		'parseutils.c',
-		'pixdesc.c',
-		'pixelutils.c',
-		'random_seed.c',
-		'rational.c',
-		'reverse.c',
-		'rc4.c',
-		'ripemd.c',
-		'samplefmt.c',
-		'sha.c',
-		'sha512.c',
-		'slicethread.c',
-		'spherical.c',
-		'stereo3d.c',
-		'threadmessage.c',
-		'time.c',
-		'timecode.c',
-		'timestamp.c',
-		'tree.c',
-		'twofish.c',
-		'utils.c',
-		'xga_font_data.c',
-		'xtea.c',
-		'tea.c',
-		'tx.c',
-		'tx_float.c',
-		'tx_double.c',
-		'tx_int32.c',
-		'uuid.c',
-		'version.c',
-		'video_enc_params.c',
-		'video_hint.c',
-		'film_grain_params.c',
-		'x86/cpu.c',
-		'x86/fixed_dsp_init.c',
-		'x86/float_dsp_init.c',
-		'x86/imgutils_init.c',
-		'x86/lls_init.c',
-		'x86/cpuid.asm',
-		'x86/fixed_dsp.asm',
-		'x86/float_dsp.asm',
-		'x86/imgutils.asm',
-		'x86/lls.asm',
-		'x86/tx_float.asm',
-	}},
+	paths[[
+		libavutil/(
+			adler32.c
+			aes.c
+			aes_ctr.c
+			ambient_viewing_environment.c
+			audio_fifo.c
+			avstring.c
+			avsscanf.c
+			base64.c
+			blowfish.c
+			bprint.c
+			buffer.c
+			cast5.c
+			camellia.c
+			channel_layout.c
+			cpu.c
+			crc.c
+			csp.c
+			des.c
+			detection_bbox.c
+			dict.c
+			display.c
+			dovi_meta.c
+			downmix_info.c
+			encryption_info.c
+			error.c
+			eval.c
+			executor.c
+			fifo.c
+			file.c
+			file_open.c
+			float_dsp.c
+			fixed_dsp.c
+			frame.c
+			hash.c
+			hdr_dynamic_metadata.c
+			hdr_dynamic_vivid_metadata.c
+			hmac.c
+			hwcontext.c
+			iamf.c
+			imgutils.c
+			integer.c
+			intmath.c
+			lfg.c
+			lls.c
+			log.c
+			log2_tab.c
+			lzo.c
+			mathematics.c
+			mastering_display_metadata.c
+			md5.c
+			mem.c
+			murmur3.c
+			opt.c
+			parseutils.c
+			pixdesc.c
+			pixelutils.c
+			random_seed.c
+			rational.c
+			reverse.c
+			rc4.c
+			ripemd.c
+			samplefmt.c
+			sha.c
+			sha512.c
+			slicethread.c
+			spherical.c
+			stereo3d.c
+			threadmessage.c
+			time.c
+			timecode.c
+			timestamp.c
+			tree.c
+			twofish.c
+			utils.c
+			xga_font_data.c
+			xtea.c
+			tea.c
+			tx.c
+			tx_float.c
+			tx_double.c
+			tx_int32.c
+			uuid.c
+			version.c
+			video_enc_params.c
+			video_hint.c
+			film_grain_params.c
+
+			@x86_64 x86/(
+				cpu.c
+				fixed_dsp_init.c
+				float_dsp_init.c
+				imgutils_init.c
+				lls_init.c
+				cpuid.asm
+				fixed_dsp.asm
+				float_dsp.asm
+				imgutils.asm
+				lls.asm
+				tx_float.asm
+			)
+
+			@riscv64 riscv/(
+				asm.S
+				bswap_rvb.S
+				cpu.c
+				fixed_dsp_init.c
+				fixed_dsp_rvv.S
+				float_dsp_init.c
+				float_dsp_rvv.S
+			)
+
+			@aarch64 aarch64/(
+				asm.S
+				cpu.c
+				float_dsp_init.c
+				float_dsp_neon.S
+				tx_float_init.c
+				tx_float_neon.S
+			)
+		)
+	]],
 	sources.libavutil,
 })
 
 lib('libswresample.a', {
-	expand{'libswresample/', {
-		'audioconvert.c',
-		'dither.c',
-		'options.c',
-		'rematrix.c',
-		'resample.c',
-		'resample_dsp.c',
-		'swresample.c',
-		'swresample_frame.c',
-		'version.c',
-		'x86/audio_convert.asm',
-		'x86/rematrix.asm',
-		'x86/resample.asm',
-		'x86/audio_convert_init.c',
-		'x86/rematrix_init.c',
-		'x86/resample_init.c',
-	}},
+	paths[[
+		libswresample/(
+			audioconvert.c
+			dither.c
+			options.c
+			rematrix.c
+			resample.c
+			resample_dsp.c
+			swresample.c
+			swresample_frame.c
+			version.c
+
+			@x86_64 x86/(
+				audio_convert.asm
+				rematrix.asm
+				resample.asm
+				audio_convert_init.c
+				rematrix_init.c
+				resample_init.c
+			)
+
+			@aarch64 aarch64/(
+				audio_convert_init.c
+				audio_convert_neon.S
+				neontest.c
+				resample.S
+				resample_init.c
+			)
+		)
+	]],
 	sources.libswresample,
 	'libavutil.a',
 })
 
 lib('libswscale.a', {
-	expand{'libswscale/', {
-		'alphablend.c',
-		'hscale.c',
-		'hscale_fast_bilinear.c',
-		'gamma.c',
-		'half2float.c',
-		'input.c',
-		'options.c',
-		'output.c',
-		'rgb2rgb.c',
-		'slice.c',
-		'swscale.c',
-		'swscale_unscaled.c',
-		'utils.c',
-		'version.c',
-		'yuv2rgb.c',
-		'vscale.c',
-		'x86/rgb2rgb.c',
-		'x86/swscale.c',
-		'x86/yuv2rgb.c',
-		'x86/hscale_fast_bilinear_simd.c',
-		'x86/input.asm',
-		'x86/output.asm',
-		'x86/scale.asm',
-		'x86/scale_avx2.asm',
-		'x86/rgb_2_rgb.asm',
-		'x86/yuv_2_rgb.asm',
-		'x86/yuv2yuvX.asm',
-	}},
+	paths[[
+		libswscale/(
+			alphablend.c
+			hscale.c
+			hscale_fast_bilinear.c
+			gamma.c
+			half2float.c
+			input.c
+			options.c
+			output.c
+			rgb2rgb.c
+			slice.c
+			swscale.c
+			swscale_unscaled.c
+			utils.c
+			version.c
+			yuv2rgb.c
+			vscale.c
+
+			@x86_64 x86/(
+				rgb2rgb.c
+				swscale.c
+				yuv2rgb.c
+				hscale_fast_bilinear_simd.c
+				input.asm
+				output.asm
+				scale.asm
+				scale_avx2.asm
+				rgb_2_rgb.asm
+				yuv_2_rgb.asm
+				yuv2yuvX.asm
+			)
+
+			@riscv64 riscv/(
+				rgb2rgb.c
+				rgb2rgb_rvb.S
+				rgb2rgb_rvv.S
+			)
+
+			@aarch64 aarch64/(
+				hscale.S
+				output.S
+				rgb2rgb.c
+				rgb2rgb_neon.S
+				swscale.c
+				swscale_unscaled.c
+				yuv2rgb_neon.S
+			)
+		)
+	]],
 	sources.libswscale,
 	'libavutil.a',
 })
