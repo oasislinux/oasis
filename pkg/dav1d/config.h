e2e9fc0867af7ee2ae99e9382829e07018a1ecc6
Samuel Marín Soto <samarin@estudiantec.cr>
2026-05-27 04:17:12 -0600

    dav1d: fix build regression on x86
    the awk expression would make redefined macros due to the new arch guards in config.h. Pass config.h through unifdef first

--- pkg/dav1d/config.h
+++ pkg/dav1d/config.h
@@ -1,15 +1,15 @@
 #define ARCH_AARCH64 0
 #define ARCH_ARM 0
 #define ARCH_PPC64LE 0
+
+#ifdef __x86_64__
+#define ARCH_X86 1
+#define ARCH_X86_32 0
+#define ARCH_X86_64 1
+#else
 #define ARCH_X86 0
 #define ARCH_X86_32 0
 #define ARCH_X86_64 0
-
-#ifdef __x86_64__
-#undef ARCH_X86
-#undef ARCH_X86_64
-#define ARCH_X86 1
-#define ARCH_X86_64 1
 #endif
 
 #define CONFIG_16BPC 1
